bcd4e211f8cd287c6fb40a09c5831023addff319
MR-aalole <52821144+Temitope25@users.noreply.github.com>
2020-06-03 00:47:44 +0200

    Update aalole.js

--- scripts/aalole.js
+++ scripts/aalole.js
@@ -1,4 +1,4 @@
 const aaloleHngDetails = (name, id, lang) =>
   `Hello World.\n This is ${name},\n with HNGi7 ID ${id},\n using ${lang} for stage two task`;
 
-aaloleHngDetails("Rasheed Mikail Abiodun", "HNG-02286", "JavaScript");
+console.log(aaloleHngDetails("Rasheed Mikail Abiodun", "HNG-02286", "JavaScript"));

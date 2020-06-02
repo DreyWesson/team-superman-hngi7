17a0df8a3151bbcc0f33387c6ce5cd6e99ae87ec
iBoBoTi <iroegbucalistus@gmail.com>
2020-06-02 23:10:12 +0100

    task 2 commit

---
+++ scripts/iBoBoTi.py
@@ -0,0 +1,5 @@
+fullname = "Iroegbu Chukwuebuka"
+id = "HNG-03100"
+language = "Python Language"
+output = "Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task"
+print(output.format(fullname, id, language))

c607984a8c65c6cda97eb04d30e532205ee4f91b
Ajayi Michael <ajayimichael150@gmail.com>
2020-06-03 00:16:54 +0100

    This is my proposal :)

---
+++ scripts/Function.js
@@ -0,0 +1,11 @@
+function data(){
+    let fullName = 'Ajayi Michael';
+    let id = 'HNG-00384';
+    let language = 'JavaScript';
+
+    const profile = 'Hello World, this is ' + fullName + ' with ' + id + ' using ' + language + ' for stage 2 task';
+    
+    console.info(profile);
+}
+
+data();

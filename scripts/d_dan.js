7398d6185c31d6fc390d2f160dcac1c464f0f80d
damilaredaniel <danieldamilare18@gmail.com>
2020-06-02 23:19:46 +0100

    created d_daan.js

---
+++ scripts/d_dan.js
@@ -0,0 +1,8 @@
+var name='Damilare Solanke';
+var hngId='HNG-03896';
+var language='javascript';
+
+function myScript(fname,HngId,Language){
+console.log(`Hello world, this is ${fname} with HNGi7 ID:${HngId} using ${Language}`);
+}
+myScript(name,hngId,language);

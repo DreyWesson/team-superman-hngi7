69b77f0785d25a1022437446238fac1d0b649d31
abdulxs <gaftofi@gmail.com>
2020-06-02 19:39:19 +0100

    Create Abdulgafar.js
    Added a new script containing my name, HNGi ID and the language I used.

---
+++ scripts/Abdulgafar.js
@@ -0,0 +1,5 @@
+const introduce = (name, id, language) => {
+  
+return `Hello world, this is ${name} with HNGi ID of ${id} using ${language} language`;
+};
+introduce ('Abdulgafar', 'HNGI-01368', 'javascript');

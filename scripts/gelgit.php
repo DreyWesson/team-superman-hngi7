a38fa9d04134e5a37c7028bea0aa2d08c127b71d
sparkidea25 <michaeldaralola123@gmail.com>
2020-06-02 21:18:17 +0100

    Micheal

---
+++ scripts/gelgit.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$hello = "Hello world this is Micheal Daralola Oketunde with HNGI7 ID: HNG-04388 using PHP for Stage 2 task";
+
+echo json_encode($hello);
+?>
+
+</body>
+</html>

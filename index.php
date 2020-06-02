47ff0ba68d9b4464bdb1499d1ad531aef653f873
Emmanuel Menyaga <sparklinsparky@gmail.com>
2020-06-03 00:28:51 +0100

    Edit to make my script pass and making things look pretty on test output

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 $files = scandir('scripts');
 if($files) {
     foreach($files as $file) {
-        echo 'File: '.$file.'<br />';
+        echo '
+            File: '.$file.'';
 
         unset($output);
         if(preg_match('/.php$/i', $file)){
@@ -14,21 +15,29 @@ if($files) {
         }
 
         if(isset($output)) {
-            echo 'Output: '.$output.'<br />';
+            echo '
+            Output: '.$output.'';
             $result =  [];
             preg_match('/^Hello World, this is ([a-zA-Z -]*) with HNGi7 ID ((HNG-|)[0-9]{1,5}) using (Python|PHP|JavaScript|Node.js) for stage 2 task(.|)$/i', $output, $result);
             if(count($result) > 0) {
-                echo 'Name: '.$result[1].'<br />';
-                echo 'HNGi7 ID: '.$result[2].'<br />';
-                echo 'Language: '.$result[4].'<br />';
-                echo 'Result: Passed, congrats!!!';
+                echo '
+            Name: '.$result[1].'';
+                echo '
+            HNGi7 ID: '.$result[2].'';
+                echo '
+            Language: '.$result[4].'';
+                echo '
+            Result: Passed, congrats!!!';
             } else {
-                echo 'Result: Fail :(';
+                echo '
+            Result: Fail :(';
             }
         } else {
-            echo 'Result: Fail :(';
+            echo '
+            Result: Fail :(';
         }
 
-        echo '<br /><br />';
+        echo '
+        ';
     }
 }

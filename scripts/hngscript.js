e404adc3498ed224064d02fa9c867e029549e352
Omolola1997 <43821270+Omolola1997@users.noreply.github.com>
2020-06-03 00:03:57 +0100

    first commit

---
+++ scripts/hngscript.js
@@ -0,0 +1,6 @@
+//stage2
+const fullName = "Fele Omolola"
+const internHngID = "HNG-01729"
+const language = "JavaScript"
+
+console.log(`Hello World, this is ${fullName} with HNGi7 ID: ${internHngID} using ${language} for stage 2 task`)

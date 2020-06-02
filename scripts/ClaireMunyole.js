48576d143e5f1eb069a807b7c663a48346c8c65a
munyolec <munyolec@gmail.com>
2020-06-02 21:44:47 +0300

    task 2 script

---
+++ scripts/ClaireMunyole.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="demo"></p>
+
+<script>
+
+document.getElementById("demo").innerHTML = "Hello World, this is Claire Munyole with HNGi7 ID ID-00045 using JavaScript for stage 2 task";
+
+</script>
+
+</body>
+</html>

3b573909a848ecf5cab61d56fd8711cee1bae05e
damilaredaniel <danieldamilare18@gmail.com>
2020-06-02 23:35:35 +0100

    updated d_dan.js

--- scripts/d_dan.js
+++ scripts/d_dan.js
@@ -3,6 +3,6 @@ var hngId='HNG-03896';
 var language='javascript';
 
 function myScript(fname,HngId,Language){
-console.log(`Hello world, this is ${fname} with HNGi7 ID:${HngId} using ${Language}`);
+console.log(`Hello world, this is ${fname} with HNGi7 ID:${HngId} using ${Language} for stage 2 task`);
 }
 myScript(name,hngId,language);

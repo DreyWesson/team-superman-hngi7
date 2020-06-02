00b61be2860f85f3d767df9f55b327f813b69357
munyolec <munyolec@gmail.com>
2020-06-02 22:03:16 +0300

    remove file

--- scripts/ClaireMunyole.js
+++ scripts/ClaireMunyole.js
@@ -6,7 +6,7 @@
 
 <script>
 
-document.getElementById("demo").innerHTML = "Hello World, this is Claire Munyole with HNGi7 ID ID-00045 using JavaScript for stage 2 task";
+document.getElementById("demo").innerHTML = "Hello World, this is Claire Munyole with HNGi7 ID HNG-00045 using JavaScript for stage 2 task";
 
 </script>
 

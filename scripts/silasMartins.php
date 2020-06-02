3e3b5fe165c77a2a421676e9d05ce33620fcc31b
Silas Martins <63853942+silas07offcial@users.noreply.github.com>
2020-06-03 00:33:23 +0100

    Add files via upload

---
+++ scripts/silasMartins.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$message = array("Silas Aigbemekhe","HNG-03916","PHP");
+echo "Hello World, this is " . $message[0] ." ". "with HNG-ID " . $message[1] . " using " . $message[2] . " "."for stage 2 task";
+?>
+</body>
+</html>

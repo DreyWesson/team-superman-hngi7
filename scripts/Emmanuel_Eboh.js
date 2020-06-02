a7449e0a038d68319547c6cfb3a796b2cb91a8e8
Emmanuel Eboh <ecolejnr007@gmail.com>
2020-06-03 00:15:53 +0100

    Final commit for stage 2

--- scripts/Emmanuel_Eboh.js
+++ scripts/Emmanuel_Eboh.js
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html>
-<title>Emmanuel.Eboh</title>
-<script>
+
     const name = "Emmanuel Eboh";
     const id = "HNG-01574";
     const lang = "JavaScript";
     document.write(
         `Hello World, this is ${name} with ${id} using ${lang} for stage 2 task.`
         );
-</script>
-</html>

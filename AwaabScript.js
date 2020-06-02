6675b5cd7bed1eb85f8491fb2596f9ae34ee86d4
AwaabSingh <adeoyin2828@gmail.com>
2020-06-02 22:18:19 +0100

    added my script

---
+++ AwaabScript.js
@@ -0,0 +1,14 @@
+//  const fullName = "Abdul-Kareem Abdul-Fattah";
+//  const id = "U014K1QE6FL";
+//  const language = "Javascript";
+
+    
+  function myInfomation (fullName = 'Abdul-Kareem Abdul-Fattah', id = 'U014K1QE6FL',language = 'Javascript' ) {
+            console.log (
+                `Hello World, this is ${fullName} with HNGI7 ${id} using ${language}`
+            );
+  }
+
+  myInfomation();
+
+  

9222fb2adfb12489f0966ecec0e0d43f88896e3a
Oshoke <goldbars.ng@gmal.com>
2020-06-02 23:59:26 +0100

    Oshoke Louis 0QE

---
+++ random_oshoke_louis.py
@@ -0,0 +1,12 @@
+
+# A function to accept 3 string arguments and return an expected string 
+# containing the HNGi7 Intern profile
+
+def say_hello(full_name, id:int, language):
+    print (f"Hello World, this is {full_name} with HNGi7 ID {id} \
+    using {language} for stage 2 task")
+
+
+
+say_hello("Oshoke Louis", "12345", "python")
+

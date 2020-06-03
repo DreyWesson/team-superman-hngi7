bc883196accac7ec5d959bf682d956c370138446
Adbeel Omiunu <45834662+Adbe-El@users.noreply.github.com>
2020-06-03 18:23:08 +0100

    Update adbe-El.js

--- scripts/adbe-El.js
+++ scripts/adbe-El.js
@@ -1,5 +1,6 @@
 const name = "Adbeel Omiunu";
 const HNGi7ID = "HNG-02582";
 const language = "JavaScript";
+const email = "adbeelomiunu@gmail.com";
 
 console.log("Hello World, this is " + name + " with HNGi7 ID " + HNGi7ID + " using " + language + " for stage 2 task.");

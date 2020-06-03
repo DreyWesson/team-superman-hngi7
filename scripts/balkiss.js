e3eeee54ef041a7fbe2411eec2cd60454d2cd067
Balkiss-hue <58786205+Balkiss-hue@users.noreply.github.com>
2020-06-03 23:46:16 +0100

    this is my task

---
+++ scripts/balkiss.js
@@ -0,0 +1,7 @@
+let intern = {
+    name: " Ogundotun Balkiss",
+    hngId: "HNG-00945",
+    Language: "Javascript",
+    Email: "ogundotunbalkiss@gmail.com"
+};
+console.log(`Hello World, this is ${intern.name} with HNGi7 ID ${intern.hngId} using ${intern.Language} for stage 2 task`);

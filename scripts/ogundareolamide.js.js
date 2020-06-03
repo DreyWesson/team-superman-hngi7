52d673e45652c220a9dc4a7abea5c482c0057366
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-06-03 06:16:21 +0100

    repo updated

---
+++ scripts/ogundareolamide.js.js
@@ -0,0 +1,10 @@
+function hngInfo(fullname, id, language) {
+ const info = `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task.`;
+ console.log(info);
+};
+
+const fullName = "Ogundare Olamide";
+const hngID = "HNG-04678";
+const language = "JavaScript";
+
+hngInfo(fullName, hngID, language);

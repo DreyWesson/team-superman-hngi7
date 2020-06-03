23ffe6f74d43512a881d8da3473187bd45e3745d
Shittu Promise <eopas4christ@gmail.com>
2020-06-03 04:37:50 +0100

    Completed task 2

---
+++ scripts/promiseShittu.py
@@ -0,0 +1,4 @@
+def task_two(name, hng_id, stack):
+    print("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task" .format(name, hng_id, stack))
+
+task_two("Promise Shittu", "HNG-06109", "Python")

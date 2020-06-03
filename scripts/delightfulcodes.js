2b8f61953074b4e160664f1a3be860e279061c40
Christian Ndu <christiannduh@gmail.comgit config --global user.email christiannduh@gmail.com>
2020-06-03 18:24:38 +0100

    added mail

--- scripts/delightfulcodes.js
+++ scripts/delightfulcodes.js
@@ -1,10 +1,10 @@
-const internName = "Christian Ndu";
+const myName = "Christian Ndu";
 const hngId = "01290";
 const preferredLang = "Javascript";
 const myEmail = "christiannduh@gmail.com";
 
 function namecheck(){
-    console.log(`Hello World, this is ${internName} with HNGi7 ID HNG-${0}${Number(hngId)} using ${preferredLang} for stage 2 task.${myEmail}`)
+    console.log(`Hello World, this is ${myName} with HNGi7 ID HNG-${0}${Number(hngId)} using ${preferredLang} for stage 2 task.${myEmail}`)
 }
 //Number
 namecheck()

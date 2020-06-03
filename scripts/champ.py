e91f619c32f1c67d8112a92323660afa510e022c
patrickishaf <peterndukwe96@gmail.com>
2020-06-03 11:16:30 +0100

    added a variable to store my email

--- scripts/champ.py
+++ scripts/champ.py
@@ -1,19 +1,22 @@
 # username: champ
 # name: peter onum
 # id: HNG-00133
+# email:
 
-full_name = "Peter Onum"
-hng_id = "HNG-00133"
+name = "Peter Onum"
+ID = "HNG-00133"
 language = "python"
+email = "onumdev@gmail.com"
 
-my_dict = {'name':full_name, 'identity':hng_id, 'dialect':language}
+my_dict = {'name': name, 'identity': ID, 'dialect': language}
 
 
 def show_my_details(name, hng_id, language_input):
     return f"Hello World, this is {name} with HNGi7 ID {hng_id} using {language_input} for stage 2 task"
 
 
-print(show_my_details(my_dict['name'], my_dict['identity'], my_dict['dialect']))
+print(show_my_details(my_dict['name'],
+                      my_dict['identity'], my_dict['dialect']))
 
 
-quitter = input('Press any key to quit')
+quitter = input('Press enter to quit')

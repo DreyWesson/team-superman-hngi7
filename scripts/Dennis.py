455a58e6d1986e5bcae1ec10e110b153b5d39b0d
th3nn3ss <chuksmcdennis@yahoo.com>
2020-06-03 02:13:57 +0100

    Dennis Submission

---
+++ scripts/Dennis.py
@@ -0,0 +1,8 @@
+
+def task(name, hnd_id, lang):
+    print("Hello World, this is " + name + " with HNGi7 ID " + hnd_id + " using " + lang + " for stage 2 task")
+
+
+if __name__ == "__main__":
+    task(name="Dennis Chukwunta", hnd_id="HNG-01734", lang="Python")
+    

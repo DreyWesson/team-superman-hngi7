8e7144d52d173141f24e08918542df30d2a99b25
Oogaga <godspower.agofure@gmail.com>
2020-06-03 21:22:51 +0100

    chores(task 2): modified my script for task 2
    - Modified `oogaga.js`

--- oogaga.js
+++ oogaga.js
@@ -1,7 +1,7 @@
-function HngIntern(fullname, id, language) {
-    console.log(
-      `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task`
-    );
-  }
-  HngIntern("Godspower Agofure", "HNG-06251", "Javascript");
-  
+function HngIntern(fullname, id, email, language) {
+  console.log(
+    `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`
+  );
+}
+
+HngIntern("Godspower Agofure","HNG-06251","godspower.agofure@gmail.com", "Javascript");

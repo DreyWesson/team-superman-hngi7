84ab6a637d41023538e2d59ce2c33a7235c16d26
unknown <ebukajohn11@gmail.com>
2020-06-03 17:14:39 +0100

    HNG-03514 Ebuka John Ede PHP

--- scripts/smarthechScript.php
+++ scripts/smarthechScript.php
@@ -1,9 +1,7 @@
 <?php
 function task2($id, $fullname,$language ){
 	
-	return "Hello World, this is $fullname with HNGi7 ID $id using $language for stage 2 task";
-//}
-return true;
+	return json_encode("Hello World, this is $fullname with HNGi7 ID $id using $language for stage 2 task");
 }
 
 echo task2("HNG-03514", "Ebuka John Ede", "PHP");

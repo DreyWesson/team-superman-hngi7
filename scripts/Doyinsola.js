1b91de98d5a59d66d0d282a6ebca9d2325fa90c9
egbewandedoyinsola <egbewandefolahanmi@gmail.com>
2020-06-03 16:55:03 +0100

    Doyinsola.js

--- scripts/Doyinsola.js
+++ scripts/Doyinsola.js
@@ -1,1 +1,9 @@
-
+let fullName= "Doyinsola Egbewande";
+let iD= "HNG-01670";
+let languageUsed= "JavaScript";
+let email="egbewandefolahanmi@gmail.com";
+console.log("Hello World, this is " +  fullName + "" + " with HNGi7 ID " + "" + iD + " " + " using " + "" +  languageUsed + " for stage 2 task. ");
+console.log(fullName);
+console.log(iD); 
+console.log(languageUsed);
+console.log(email);

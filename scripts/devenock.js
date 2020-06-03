b3907ec219a56ddef2bba239b8f628f7dc8a2be9
Trend20 <enockomondi305@gmail.com>
2020-06-03 15:59:00 -0400

    devenock commit

---
+++ scripts/devenock.js
@@ -0,0 +1,17 @@
+const myDetails = (myName, myId, myStackLanguage) =>{
+    this.myName = myName;
+    this.myId = myId;
+    this.myStackLanguage;
+}
+
+const greetings = (myName,myId, myStackLanguage) =>{
+   console.log(`Hello World, this is ${myName} with HNGi7 ID ${myId} using ${myStackLanguage} for stage 2 task `);
+}
+
+const data = new myDetails(userName, userId, userlanguage);
+sayHelloToHNG(intern.DevName, intern.hngId, intern.language);
+
+
+const  userName='Enock Omondi Otuoma';
+const  userId="HNG-03371";
+const  userLanguage="JavaScript";

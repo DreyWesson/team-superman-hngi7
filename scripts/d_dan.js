9852e588f1a109fd063ea107e3fad502101d0748
damilaredaniel <danieldamilare18@gmail.com>
2020-06-03 14:30:00 +0100

    update d_dan.js

--- scripts/d_dan.js
+++ scripts/d_dan.js
@@ -4,6 +4,6 @@ var language='Javascript';
 var email='danieldamilare18@gmail.com';
 
 function myScript(fullname,id,Language,email){
-console.log(`Hello World, this is ${fullname} with HNGi7 ID ${id} and ${email} using ${Language} for stage 2 task`);
+console.log(`Hello World, this is ${fullname} with HNGi7 ID ${id} using ${Language} for stage 2 task.${email}`);
 }
 myScript(name,hngId,language,email);

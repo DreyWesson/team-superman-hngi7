ce3bbc65c48c1ca2231e4838118079ce84d29de6
Modupe <igeoluwamodupe1@gmail.com>
2020-06-03 16:20:08 +0100

    modupe task two

---
+++ scripts/modupe.js
@@ -0,0 +1,5 @@
+function taskone(name,hngid,language,email){
+    console.log("Hello world, this is " + " " + name + " " + "HNGi7 ID " + " " + hngid + " " + "using " + " " + language + " " + " for stage 2 task." + email)
+
+}
+taskone("Ige Modupe ", "HNG-0410", "JavaScript" , "igeoluwamodupe1@gmail.com");

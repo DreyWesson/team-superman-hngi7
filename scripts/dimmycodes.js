7edb98fa13c8bd667d6f01ab195c51e5c250c003
devdimmy <danieloladimeji04@gmail.com>
2020-06-03 09:24:33 +0100

    email added for task update

--- scripts/dimmycodes.js
+++ scripts/dimmycodes.js
@@ -4,4 +4,4 @@ const introduceMyself = (fullName, id, language, email) => {
     console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} email:${email} using ${language} for stage 2 task.`)
 }
 
-introduceMyself('Daniel Oladimeji', 'HNG-00236', 'JavaScript', 'danieloladimeji')
+introduceMyself('Daniel Oladimeji', 'HNG-00236', 'JavaScript', 'danieloladimeji04@gmail.com')

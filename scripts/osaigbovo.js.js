64cca5c5f798006a44fd90dbc23c26d07a44fc8b
osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2020-06-03 01:03:06 +0100

    feat: new js file

--- scripts/osaigbovo.js.js
+++ scripts/osaigbovo.js.js
@@ -1,5 +1,5 @@
 const helloWorldHng = (fullName, id, language) => {
-    console.log(`HelloWorld, This is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`)
-}
+    console.log(`HelloWorld, This is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`);
+};
 
-helloWorldHng('Idiaghe osaigbovo', 'HNG-01879', 'JavaScript')
+helloWorldHng('Idiaghe osaigbovo', 'HNG-01879', 'JavaScript');

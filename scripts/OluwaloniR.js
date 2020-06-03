3db1ddae25a7ef156e2a75fcbd468d0628cfe4dc
Oluwaloni Richard Sodeinde <lonnyrichard77@gmail.com>
2020-06-03 16:07:14 +0100

    added my email

--- scripts/OluwaloniR.js
+++ scripts/OluwaloniR.js
@@ -1,6 +1,6 @@
-function helloHng7(fullname, id, language) {
+function helloHng7(fullname, id, language, email) {
     console.log(
-      `Hello World, this is ${fullname} with HNGi7 ${id} using ${language}`
+      `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`
     );
   }
-helloHng7("Sodeinde Olualoni Richard", "HNG-01180", "Javascript");
+helloHng7("Sodeinde Olualoni Richard", "HNG-01180", "Javascript", "lonnyrichard77@gmail.com");

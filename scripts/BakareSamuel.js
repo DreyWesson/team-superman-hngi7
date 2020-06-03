eeea8918b4d2692c5727306432454307f9c5d013
saamlegend <49197462+saamlegend@users.noreply.github.com>
2020-06-03 21:28:37 +0100

    Update BakareSamuel.js

--- scripts/BakareSamuel.js
+++ scripts/BakareSamuel.js
@@ -2,4 +2,4 @@
 var fullName = 'Bakare Samuel', ID = 'HNG-02316', language = 'javascript';
 var Email = 'bakareayomiku@gmail.com';
 
-console.log('Hello World, this is '+fullName+' with HNGi7 ID '+ID+' using '+language+' for stage 2 task '+Email+ '');
+console.log('Hello World, this is '+fullName+' with HNGi7 ID '+ID+' using '+language+' for stage 2 task '.+Email+ '');

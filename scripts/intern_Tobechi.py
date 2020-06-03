cb9ea667742d1ab3047a2257edfa3a3c9e2021af
Tobechi <tobechichukwuleta125@gmail.com>
2020-06-03 01:31:35 -0700

    Added intern_Tobechi for task two

---
+++ scripts/intern_Tobechi.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 19:50:37 2020
+
+@author: Tobechi Chukwuleta
+"""
+import sys
+def my_script():
+    print('Hello World, this is %s with HNG ID %s using %s language for stage 2 task' % ('Tobechi', 'HNG-03085', 'python'))
+    
+my_script()
+sys.stdout.flush()

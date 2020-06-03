f02a191b9c6cb110bdcd051048030a5840fca3b1
chidubem <enwankwo025@gmail.com>
2020-06-02 22:41:00 -0700

    my script

--- scripts/chydube.php
+++ scripts/chydube.php
@@ -1,13 +1,13 @@
 <?php
 
 //function to generate user infomation.
-function identity_generator($name, $id, $language)
-{
-    return "Hello World, this is ".$name." with HNGi7 ID ".$id." using ".$language." for stage 2 task.";
-}
+    function details_generator($name, $id, $language)
+    {
+        return "Hello World, this is ".$name." with HNGi7 ID ".$id." using ".$language." for stage 2 task.";
+    }
 
-$output = identity_generator("Chidubem Nwankwo","00366","PHP");
+    $output = details_generator("Chidubem Nwankwo","00366","PHP");
 
-echo $output;
+    echo $output;
 
 ?>

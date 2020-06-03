2ed91823bcc7e61096896f1018ebd04ce8ff9f91
Ajayi Michael <ajayimichael150@gmail.com>
2020-06-03 15:47:45 +0100

    Added the email variable

--- scripts/Function.js
+++ scripts/Function.js
@@ -2,6 +2,7 @@ function data(){
     let fullName = 'Ajayi Michael';
     let id = 'HNG-00384';
     let language = 'JavaScript';
+    let email = 'ajayimichael150@gmail.com'
 
     const profile = 'Hello World, this is ' + fullName + ' with ' + id + ' using ' + language + ' for stage 2 task';
     

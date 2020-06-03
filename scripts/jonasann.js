79641ab31653fbe75b2e100dc83b00e23d89648b
Jonah Ifeoma <ifyajoke@gmail.com>
2020-06-03 14:55:52 +0100

    Fix: added email to output

--- scripts/jonasann.js
+++ scripts/jonasann.js
@@ -4,7 +4,7 @@ function introduceSelf(){
   let email = "ifyajoke@gmail.com";
   let language = "JavaScript";
 
-  console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task`)
+  console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`)
   
   }
   introduceSelf();

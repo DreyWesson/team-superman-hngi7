39ff34133bcd5f68427e1d95d137f137eeade86a
Khalid <olaniyisal@gmail.com>
2020-06-03 12:09:12 +0100

    salskhal script

---
+++ scripts/salskhal.js
@@ -0,0 +1,6 @@
+function helloHng(fullname, id, language) {
+    console.log(
+      `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task`
+    );
+  }
+  helloHng("Khalid Salman-Yusuf", "HNG-00577", "Javascript");

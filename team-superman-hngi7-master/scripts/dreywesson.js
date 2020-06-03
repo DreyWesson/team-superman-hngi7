1dd785f740bdae028c9ef9417864cf28457a94c7
Georgemichaeldagogo <52969898+Georgemichaeldagogo@users.noreply.github.com>
2020-06-03 02:01:31 +0100

    Delete dreywesson.js

--- team-superman-hngi7-master/scripts/dreywesson.js
+++
@@ -1,6 +0,0 @@
-function helloHng(fullname, id, language) {
-  console.log(
-    `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task`
-  );
-}
-helloHng("Dare Oduwole", "HNG-00069", "Javascript");

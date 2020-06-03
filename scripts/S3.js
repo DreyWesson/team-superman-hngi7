fb5add1ac87413a1668ee478fc7ef4506978fb95
sey cole <seyicole>
2020-06-03 09:41:51 +0100

    added my script

---
+++ scripts/S3.js
@@ -0,0 +1,9 @@
+const myScript = () => {
+    let name = 'Oluseyi Imanah';
+   let  id = 'HNG-03003';
+    let language = 'javascript';
+   let  info =`Hello world, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+ console.log(info);
+     
+ }
+ myScript()

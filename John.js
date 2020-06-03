cc3ebf54a92490027bc609df9f0acce022144bdd
Johnmattee <42149205+Johnmattee@users.noreply.github.com>
2020-06-03 20:44:59 +0100

    Update John.js

--- John.js
+++ John.js
@@ -1,5 +1,12 @@
-let fullName = 'Ajayi John';
-let ID = 'HNG-00374';
-let language = 'javascript';
 
-console.log(`Hello World, this is ${fullName} with HNG ID ${ID} using ${language} for stage 2 task`);
+let name = "Ajayi John";
+let id = "HNG-00374";
+let language = "JavaScript";
+let email="ajayitaiwojohnmatt@gmail.com";
+
+
+function hngTask(name, id, language, email){
+
+  console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`);
+}
+hngTask(name, id, language, email);

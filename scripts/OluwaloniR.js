d006ef03d0f352fe4e49fec0f430d9eb663a103f
Oluwaloni Richard Sodeinde <lonnyrichard77@gmail.com>
2020-06-03 08:39:50 +0100

    created my script OluwaloniR

---
+++ scripts/OluwaloniR.js
@@ -0,0 +1,6 @@
+function helloHng7(fullname, id, language) {
+    console.log(
+      `Hello World, this is ${fullname} with HNGi7 ${id} using ${language}`
+    );
+  }
+helloHng7("Sodeinde Olualoni Richard", "HNG-01180", "Javascript");

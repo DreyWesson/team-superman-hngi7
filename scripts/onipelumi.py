9427d13f7f44ff9cdd3a941b0e865979745f72b7
lumpee <--global>
2020-06-03 14:57:49 +0100

    lumi repo

---
+++ scripts/onipelumi.py
@@ -0,0 +1,5 @@
+#this program will take input and format it
+def me(name,id,language):
+    return
+print ("Hello world,this is %s with %s using %s for stage task 2" %("oluwapelumi oni","HNG-04612","Python"))
+

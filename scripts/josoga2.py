c12cbc5ecae9acc7559d3b6d9b7a430bfa08e556
Wale Joseph <30349247+josoga2@users.noreply.github.com>
2020-06-04 01:10:15 +0300

    Update josoga2.py

--- scripts/josoga2.py
+++ scripts/josoga2.py
@@ -1,3 +1,3 @@
 print ("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task.{}"
        
-       .format('Adewale OGUNLEYE', 'HNG-00445', 'Python3' , 'josoga2@gmail.com'))
+       .format('Adewale OGUNLEYE', 'HNG-00445', 'Python' , 'josoga2@gmail.com'))

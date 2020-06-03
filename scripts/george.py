bd0f4a1fcd7af659ff7b27f0505d8b80ee4810b9
georgediepiriye <georgediepiriye4u@gmail.com>
2020-06-03 23:04:48 +0100

    george's Hng script

--- scripts/george.py
+++ scripts/george.py
@@ -3,7 +3,7 @@ id = "HNG-00088"
 language = "python"
 email = "georgediepiriye4u@gmail.com"
 
-print ("Hello World,this is " + full_name + " with HNGi7 ID" +
-ID + " using " + language + " for stage 2 task")
+print ("Hello World,this is " + full_name + " with HNGi7 ID " +
+id + " using " + language + " for stage 2 task")
 
 

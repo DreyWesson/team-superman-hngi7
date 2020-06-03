177a2337c74c592ade863566514e6e9c9f79706c
mark odhiambo okoth <markokoth96@gmail.com>
2020-06-03 14:15:58 +0300

    added email

--- scripts/markokoth.js
+++ scripts/markokoth.js
@@ -1,5 +1,5 @@
-const markIntroduction = (fullName, id, language) => {
-    console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`)
+const markIntroduction = (fullName, id, language, email) => {
+    console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task.`)
 }
 
-markIntroduction('Mark Okoth', 'HNG-01803', 'JavaScript')
+markIntroduction('Mark Okoth', 'HNG-01803', 'JavaScript', 'markokoth96@gmail.com')

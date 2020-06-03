1bb1582cbdf0d082b91b46faf77e093f4e6289f4
masterspencer <thecharlesjunior@gmail.com>
2020-06-03 12:49:27 +0100

    update that has the email

---
+++ scripts/charles_Belu.js
@@ -0,0 +1,13 @@
+//an Object was created to hold properties of the user
+let userDetails = {
+    userName: "",
+    id: "",
+    email: "nwachebeluanthony@gmail.com",
+    lang: "",
+    displayInfo: (userName, id, lang) => {
+
+        console.log (`Hello World, this is ${userName} with HNGi7 ID ${id} using ${lang} for stage 2 task`);
+    }//function to output the object property values. 
+};
+let { userName, id,lang } = userDetails; //destructuring of object 
+userDetails.displayInfo("Charles Nwachebelu", "HNG-04564","javaScript");

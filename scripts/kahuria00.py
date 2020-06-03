d16786c1aa9bff2ce0591d72682ef266645f0feb
kahuria00 <charitykahuria@gmail.com>
2020-06-03 22:26:35 +0300

    changed format

--- scripts/kahuria00.py
+++ scripts/kahuria00.py
@@ -1,1 +1,4 @@
-print("Hello World, this is Charity wanjiru of email charitykahuria@gmail.com  with HNGi7 ID 00621 using Python for stage 2 task")
+print("Hello World, this is {0} of email {1}  with HNGi7 {2} using {3} for stage 2 task".format('Charity Wanjiru','charitykahuria@gmail.com','ID 00621','Python'))
+
+
+

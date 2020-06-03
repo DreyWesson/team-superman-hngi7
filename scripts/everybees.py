1b677d41946fb615d0b6119ef508570b5ef6598a
everybees <everybees@gmail.com>
2020-06-03 19:09:44 +0100

    removed white spaces causing test to fail

--- scripts/everybees.py
+++ scripts/everybees.py
@@ -6,6 +6,6 @@ email = "everybees@gmail.com"
 
 
 def stage_one_task(name, id, language):
-    print("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task. {}".format(name, id, language, email))
+    print("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task.{}".format(name, id, language, email))
 
 stage_one_task("Jibola Bakare", "HNG-03475", "Python")

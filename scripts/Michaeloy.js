c39574b2d084f206195a56b691ef8d5238d4cb56
Michael John <johnmikeloy@gmail.com>
2020-06-03 12:31:11 +0100

    myadded script

---
+++ scripts/Michaeloy.js
@@ -0,0 +1,5 @@
+const myProfile = (name, id, email, lang) => 
+`"Hello World, this is ${name} with HNGi7 ID ${id}
+and email ${email} using ${lang} for stage 2 task"`;
+
+console.log(myProfile("Michael John", "HNG-00424", "johnmikeloy@gmail.com", "JavaScript"));

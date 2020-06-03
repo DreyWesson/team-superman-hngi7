0925ba5832c46524a64eb54b4d557bd30bdef4eb
Aluko Ayodele <51384137+Alukoayodele@users.noreply.github.com>
2020-06-04 00:05:48 +0100

    email added

--- scripts/alukoayodele.js
+++ scripts/alukoayodele.js
@@ -1,22 +1,22 @@
 
 class Person {
-  constructor( fullName, id, email, language) {
+  constructor( fullName, id, language, email) {
     
     this.fullName = fullName;
     this.id = id;
-    this.email = email;
     this.language = language;
+    this.email = email;
   }
   output() {
-    return `Hello World,This is ${this.fullName} with HNGi7 ID ${this.id} using ${this.language} for stage 2 task.`;
+    return `Hello World,This is ${this.fullName} with HNGi7 ID ${this.id} using ${this.language} for stage 2 task.${this.email}`;
   }
 }
 
 const blackDev = new Person(
   'Aluko Ayodele',
   'HNG-02258',
+  'JavaScript',
   'ayodelealuko009.aa@gmail.com',
-  'JavaScript'
 );
 
 console.log(blackDev.output());

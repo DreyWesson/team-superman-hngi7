ac84c094b745a97a30b9fdb10a52c8922024f075
iBoBoTi <iroegbucalistus@gmail.com>
2020-06-03 16:36:41 +0100

    iBoBoTi.py script

--- scripts/iBoBoTi.py
+++ scripts/iBoBoTi.py
@@ -2,5 +2,5 @@ name = "Iroegbu Chukwuebuka"
 id = "HNG-03100"
 language = "Python"
 email = "iroegbucalistus@gmail.com"
-output = "Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task"
-print(output.format(name, id, language))
+output = "Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task.{3}"
+print(output.format(name, id, language, email))

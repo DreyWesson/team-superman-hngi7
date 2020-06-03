de196557ddf75a0f2cff6226f759c019ff495071
damilaredaniel <danieldamilare18@gmail.com>
2020-06-03 10:04:42 +0100

    Update d_dan.js

--- scripts/d_dan.js
+++ scripts/d_dan.js
@@ -2,7 +2,7 @@ var name='Damilare Solanke';
 var hngId='HNG-03896';
 var language='javascript';
 
-function myScript(fname,HngId,Language){
-console.log(`Hello World, this is ${fname} with HNGi7 ID ${HngId} using ${Language} for stage 2 task`);
+function myScript(fullname,id,Language){
+console.log(`Hello World, this is ${fullname} with HNGi7 ID ${id} using ${Language} for stage 2 task`);
 }
 myScript(name,hngId,language);

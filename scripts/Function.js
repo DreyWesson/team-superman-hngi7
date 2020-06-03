19a6f17a1d1454462224cfd711cc467538c1ce23
Ajayi Michael <ajayimichael150@gmail.com>
2020-06-03 16:38:07 +0100

    Update Function.js

--- scripts/Function.js
+++ scripts/Function.js
@@ -6,7 +6,7 @@ function data(){
 
     const profile = 'Hello World, this is ' + fullName + ' with HNGi7 ID ' + id + ' using ' + language + ' for stage 2 task';
     
-    console.info(profile);
+    console.log(profile);
 }
 
 data();

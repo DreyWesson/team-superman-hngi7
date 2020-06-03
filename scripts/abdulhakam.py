0c9c279144b9663361445478e6932e7ef7b24eb9
FameC <35351094+FameC@users.noreply.github.com>
2020-06-03 20:55:36 +0100

    Create abdulhakam.py

---
+++ scripts/abdulhakam.py
@@ -0,0 +1,7 @@
+full_name = 'Abdul-hakam Abuda Oshiogwe'
+hng_id ='HNG-02429'
+email_adr = 'princefame1@gmail.com'
+lang = 'Python' 
+
+
+print("Hello World, this is", full_name, "with HNGi7 ID", hng_id, "using", lang, "for stage 2 task."+email_adr)

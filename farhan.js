be27709fef496d5e88716bf3db1723cd19bea05b
Sodiq Farihan <farhansodiq360@gmail.com>
2020-06-03 09:50:14 +0100

    added my js script file; Farhan

---
+++ farhan.js
@@ -0,0 +1,5 @@
+function myDetails(Fullname, ID, Language){
+    console.log(`Hello World, this is ${Fullname} with HNGi7 ID ${ID} using ${Language} for Stage 2 task`);
+}
+
+myDetails("Sodiq Farihan", "HNG-03238", "JavaScript");

9311483bcce97dddef1ea13afbabf28a4fb335ed
Okiemute Gold <goldokiemute@gmail.com>
2020-06-03 19:28:51 +0100

    OkiemuteGold-HNG-01033 script

---
+++ scripts/Gold.js
@@ -0,0 +1,9 @@
+let fullname = 'Okiemute Gold';
+let hngid = 'HNG-01033';
+let email = 'goldokiemute@gmail.com';
+let language = 'Javascript';
+
+function goldTask2(fullname, hngid, email, language) {
+console.log(`Hello World, this is ${fullname} with HNGi7 ID ${hngid} and email ${email} using ${language} for stage 2 task.`);
+}
+goldTask2('Okiemute Gold', 'HNG-01033', 'goldokiemute@gmail.com', 'Javascript')

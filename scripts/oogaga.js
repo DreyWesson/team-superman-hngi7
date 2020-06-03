fc2729337e8d785a0d1d61986288239c156b60fe
Oogaga <godspower.agofure@gmail.com>
2020-06-03 10:42:25 +0100

    chores(task 2): my script for Task 2
    - Added new file to scripts folder `oogaga.js`

---
+++ scripts/oogaga.js
@@ -0,0 +1,8 @@
+function HngIntern(fullname, id, email, language) {
+    console.log(
+      `Hello World, this is ${fullname} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`
+    );
+  }
+
+  HngIntern("Godspower Agofure","HNG-06251","godspower.agofure@gmail.com", "Javascript");
+  

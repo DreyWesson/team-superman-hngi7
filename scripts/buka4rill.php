bcd5935460f19786454bb5ce79b8c8db790350ab
Ebuka Abraham <45390219+buka4rill@users.noreply.github.com>
2020-06-03 20:18:11 +0100

    Updated PHP file to include email
    Email: buka4rill@gmail.com
    Language used: PHP

--- scripts/buka4rill.php
+++ scripts/buka4rill.php
@@ -4,12 +4,14 @@
 $fullName = "Ebuka Abraham";
 $id = "HNG-01238";
 
-$language = "PHP";
-
-$output = "Hello World, this is $fullName with HNGi7 ID $id using $language for stage 2 task";
-
 // EMAIL
 $email = "buka4rill@gmail.com";
+
+$language = "PHP";
+
+$output = "Hello World, this is $fullName with HNGi7 ID $id using $language for stage 2 task.$email";
+
+
 
 // Output 
 echo $output;

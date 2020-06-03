379d3d14cb02783477bcdf5e709524e7058d9135
soloBless <66275309+soloBless@users.noreply.github.com>
2020-06-03 21:18:34 +0100

    Update soloscript.php

--- soloscript.php
+++ soloscript.php
@@ -3,6 +3,6 @@ $name = 'Nnamani Solomon'
 $hngid = 'HNG-05483'
 $language = 'PHP'
 $email = 'nnamanisolomonblessing@gmail.com'
-$output = "Hello World, this is $name with HNGi7 ID $hngid using $language for stage 2 task. $email"
+$output = "Hello World, this is $name with HNGi7 ID $hngid using $language for stage 2 task.$email"
 echo $output
 ?>

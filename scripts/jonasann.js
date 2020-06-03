f77780ffa9d9c7856c1eff201c71fd9cc6c03b9d
Jonah Ifeoma <ifyajoke@gmail.com>
2020-06-03 03:24:41 +0100

    changes to output

--- scripts/jonasann.js
+++ scripts/jonasann.js
@@ -1,9 +1,9 @@
 function introduceSelf(){
-    let fullName = "Ifeoma Oluwapelumi Jonah"
-  let hngID = "HNG-04143"
+  let fullName = "Ifeoma Oluwapelumi Jonah"
+  let ID = "HNG-04143"
   let language = "JavaScript"
   
-  console.log(`Hello World, this is ${fullName} with HNGi7 ID: ${hngID} using ${language} for stage 2 task`)
+  console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task`)
   
   }
   introduceSelf();

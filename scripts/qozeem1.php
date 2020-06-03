0caabd392455c3f311136a8904e2a59f5b9f0436
Qozeem Odeniran <muhdqozeem@gmail.com>
2020-06-03 22:32:24 +0100

    Updated php script

--- scripts/qozeem1.php
+++ scripts/qozeem1.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello World, this is Qozeem Odeniran with HNGi7 ID 01681 using PHP for stage 2 task"."<br/>";
-echo "muhdqozeem@gmail.com";
+echo "Hello World, this is Qozeem Odeniran with HNGi7 ID 01681 using PHP for stage 2 task";
+
 
 ?>

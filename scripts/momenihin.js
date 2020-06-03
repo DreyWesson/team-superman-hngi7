289678577c538771177388735490450912306228
Nwankwo Henry <nameisifunanya@gmail.com>
2020-06-03 15:50:18 +0100

    more updates on momenihin script file

--- scripts/momenihin.js
+++ scripts/momenihin.js
@@ -4,17 +4,16 @@ the fullname, HNGi7 ID and language for HNG INTERNSHIP **/
 const myFullName = "Nwankwo Henry Ifunanya";
 const myHNGi7ID = "HNG-03667";
 const myLanguage = "Javascript";
-const email = "nwankwohenry29@gmail.com";
+const myEmail = "nwankwohenry29@gmail.com";
 
 
-function helloWorld(fullName, ID, language){
-
+function helloWorld(fullName, ID, language, email){
 
     const fullText = `Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task.${email}`;
     console.log(fullText);
 
 }
-helloWorld(myFullName, myHNGi7ID, myLanguage);
+helloWorld(myFullName, myHNGi7ID, myLanguage, myEmail);
 
 
 

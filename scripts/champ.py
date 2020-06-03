fb7a3acb036247743df52e74a097438b0fd075c6
onumengine <66191032+onumengine@users.noreply.github.com>
2020-06-03 23:09:06 +0100

    added an email

--- scripts/champ.py
+++ scripts/champ.py
@@ -12,9 +12,9 @@ email = "onumdev@gmail.com"
 my_dict = {'name': name, 'id': ID, 'language': language, 'email':email}
 
 
-def show_my_details(name, hng_id, language_input):
-    return f"Hello World, this is {name} with HNGi7 ID {hng_id} using {language_input} for stage 2 task"
+def show_my_details(name, hng_id, language_input, mail):
+    return f"Hello World, this is {name} with HNGi7 ID {hng_id} using {language_input} for stage 2 task.{mail}"
 
 
 print(show_my_details(my_dict['name'],
-                      my_dict['id'], my_dict['language']))
+                      my_dict['id'], my_dict['language'], my_dict['email']))

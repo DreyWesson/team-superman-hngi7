2b86984643cfe782e39b02408b7669599911e6bb
Ereyomi Oluwaseyi Samuel <32690414+ereyomi@users.noreply.github.com>
2020-06-03 08:47:02 +0100

    feat: ereyomi.js script

---
+++ scripts/Ereyomi.js
@@ -0,0 +1,11 @@
+const mydata = {
+  id: 'HNG-01457',
+  name: 'Ereyomi',
+  language: 'JavaScript',
+};
+
+const runHNG = ({id, name, language}) => { 
+   console.log( `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task` );
+};
+
+runHNG(mydata);

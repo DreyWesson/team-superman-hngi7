1abe7d8c8577f466cb50503c1ff687c7c2e09ce5
Ajaodolapo1301 <Ajaodlp@gmail.com>
2020-06-03 14:29:59 +0100

    AbdRaqeeb script

--- scripts/abdraqeeb.js
+++ scripts/abdraqeeb.js
@@ -6,7 +6,7 @@ const details = [
 ]
 
 const text = (name, id, lang, email) => {
-    console.log(`"Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task"`)
+    console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task.${email}`);
 };
 
 text(details[0], details[1], details[2], details[3]);

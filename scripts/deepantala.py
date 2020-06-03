0ff9236646e1ced1f2ca3044cc9dba772730c957
DeepAntala <deep.antala@yahoo.com>
2020-06-03 07:10:44 -0400

    Update deepantala.py

--- scripts/deepantala.py
+++ scripts/deepantala.py
@@ -1,5 +1,4 @@
-
 def stage1_intro(ID, name, lang):
     print("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task.".format(name, ID, lang))
-    
+
 stage1_intro("HNG-00064", "Deep Antala", "Python")

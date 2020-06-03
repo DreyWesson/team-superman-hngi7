55c1dfd331c1b27c8a8bc48b4d333f73a3cfa83b
Aluko Ayodele <51384137+Alukoayodele@users.noreply.github.com>
2020-06-03 03:58:42 +0100

    Update alukoayodele.js

--- scripts/alukoayodele.js
+++ scripts/alukoayodele.js
@@ -1,16 +1,21 @@
 
 class Person {
-  constructor(fullName, id, language, greetings) {
+  constructor(greetings, fullName, id, language) {
     this.greetings = greetings;
     this.fullName = fullName;
     this.id = id;
     this.language = language;
   }
   introduction() {
-    return `${this.greetings}!!I am ${this.fullName} with HNG-${this.id} id and i write ${this.language}.`;
+    return `${this.greetings}!!I am ${this.fullName} with ${this.id}  and i write ${this.language} for stage 2 task.`;
   }
 }
 
-const blackDev = new Person('Hello World','Aluko Ayodele', '02258', 'JavaScript');
+const blackDev = new Person(
+  'Hello World',
+  'Aluko Ayodele',
+  'HNG-02258',
+  'JavaScript'
+);
 
 console.log(blackDev.introduction());

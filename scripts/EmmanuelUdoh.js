917ecfd47c7cb89191e5c94bb22929ee40a1c483
Emmanuel <eudoh940@gmail.com>
2020-06-03 00:26:32 -0700

    remove commit from master

--- scripts/EmmanuelUdoh.js
+++
@@ -1,6 +0,0 @@
-var info = function(fullname, id, language) {
-    return `Hello World, this is ${fullname} with HNGi7-ID ${id} using ${language} for stage 2 task`
-    
-}
-let newInfo = info("Emmanuel Udoh", "HNG-02600", "Javascript");
-console.log(newInfo)

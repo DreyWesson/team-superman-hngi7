9ee79360912564c3074884593483df3145485aff
Chimaobi Prince <60024033+royadeveloper01@users.noreply.github.com>
2020-06-03 13:28:56 +0000

    Update royadeveloper.js

--- scripts/royadeveloper.js
+++ scripts/royadeveloper.js
@@ -4,6 +4,6 @@ const email = "royadeveloper01@gmail.com";
 const language = "JavaScript";
 
 const myInfo = (name, id, language) => {
-    console.log(`Hello World, This is ${name} with HNGi7 ID ${id} using ${language} for stage two task`);
+    console.log(`Hello World, This is ${name} with HNGi7 ID ${id} using ${language} for stage two task.${email}`);
 }
 myInfo(name,id,language);

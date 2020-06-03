cdff76caae3801800de6831e2d752949921cd8a2
Kennygrace <62079374+Kennygrace@users.noreply.github.com>
2020-06-03 14:32:55 +0100

    Update kennygrace.js

--- scripts/kennygrace.js
+++ scripts/kennygrace.js
@@ -5,5 +5,5 @@ let person = {
     email : "igekehinde90@gmail.com"
 };
 
-let output = "Hello World, this is" + " " +  person.name + " " + "with HNGi7 ID" + " " + person.hngid + " " + "using" + " " + person.language + " " + "for stage 2 task";
+let output = "Hello World, this is" + " " +  person.name + " " + "with HNGi7 ID" + " " + person.hngid + " " + "using" + " " + person.language + " " + "for stage 2 task. + person.email";
 console.log(output);

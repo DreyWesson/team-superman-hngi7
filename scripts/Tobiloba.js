a06cc6f6f065dfce1e5653dca66659fd68ab82ad
Tobiloba <tobiloba.ft@gmail.com>
2020-06-03 17:08:14 +0000

    added my email

--- scripts/Tobiloba.js
+++ scripts/Tobiloba.js
@@ -1,7 +1,12 @@
-const intro = ['Fakorede Tobiloba', 'HNG-02655', 'JavaScript'];
+const intro = [
+  'Fakorede Tobiloba',
+  'HNG-02655',
+  'JavaScript',
+  'fakoredetobiloba.ft@gmail.com',
+];
 
 console.log(
   `Hello World, this is ${intro[0]} with HNGi7 ID ${intro[1]} using ${
     intro[2]
-  } for stage 2 task`
+  } for stage 2 task.${intro[3]}`
 );

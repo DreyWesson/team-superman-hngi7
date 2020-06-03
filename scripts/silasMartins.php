5cf67f0e2faac0322a9f5794a1b05b733b911bf7
Silas Martins <63853942+silas07offcial@users.noreply.github.com>
2020-06-03 16:08:09 +0100

    Update silasMartins.php

--- scripts/silasMartins.php
+++ scripts/silasMartins.php
@@ -1,4 +1,5 @@
 <?php
-$message = array("Silas Aigbemekhe","HNG-03916","silas07official@gmail.com","PHP");
-echo "Hello World, this is " . $message[0] ." ". "with HNGi7 ID " . $message[1] ." "."and email"." ".$message[2]." "."using" ." ". $message[3] . " "."for stage 2 task";
+$myArray = array("Silas Aigbemekhe","HNG-03916","silas07official@gmail.com","PHP");
+$message ="Hello World, this is " . $myArray[0] ." ". "with HNGi7 ID " . $myArray[1] ." "."using" ." ". $myArray[3] . " "."for stage 2 task.silas07official@gmail.com";
+echo $message;
 ?>

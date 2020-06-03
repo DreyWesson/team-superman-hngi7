d82aef7535034c587871c64c04aa3519e6f848be
Johnmattee <42149205+Johnmattee@users.noreply.github.com>
2020-06-03 21:46:41 +0100

    Delete John.js
    I deleted this because it's not running, I have another file that I've pushed go the repo with the same name

--- scripts/John.js
+++
@@ -1,5 +0,0 @@
-let fullName = 'Ajayi John';
-let ID = 'HNG-00374';
-let language = 'javascript';
-
-console.log(`Hello World, this is ${fullName} with HNG ID ${ID} using ${language} for stage 2 task`);

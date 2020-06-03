16683e54ee5b446390d8f39b1865c898fd527de7
Tikare Oluwatomiloba <54294689+sirtomi12@users.noreply.github.com>
2020-06-03 21:02:37 +0100

    Delete _SirTomi.py

--- scripts/_SirTomi.py
+++
@@ -1,3 +0,0 @@
-def secondTask(name, ID, language):
-    print("Hello World, this is {} with HNGi7 ID {} using {} for Stage 2 Task.".format(name, ID, language))
-secondTask("Tikare Oluwatomiloba", "HNG-02041", "Python")

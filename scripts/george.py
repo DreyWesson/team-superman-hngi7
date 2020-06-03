c0e443aef0c8eea2601bc4769fa48a099a8c059e
georgediepiriye <georgediepiriye4u@gmail.com>
2020-06-03 22:48:00 +0100

    george's phython script

---
+++ scripts/george.py
@@ -0,0 +1,9 @@
+full_name = "Diepiriye George"
+id = "HNG-00088"
+language = "python"
+email = "georgediepiriye4u@gmail.com"
+
+print ("Hello World,this is " + full_name + " with HNGi7 ID" +
+ID + " using " + language + " for stage 2 task")
+
+

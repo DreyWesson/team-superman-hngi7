aa7b508b9dd8a747e35c9922bbb25b0bcc704b79
Nwankwo Henry <nameisifunanya@gmail.com>
2020-06-03 12:25:46 +0100

    momenihin script commit

---
+++ scripts/momenihin.js
@@ -0,0 +1,19 @@
+/** A javascript program for HNGi7 task that outputs a string containing 
+the fullname, HNGi7 ID and language for HNG INTERNSHIP **/
+
+const myFullName = "Nwankwo Henry Ifunanya";
+const myHNGi7ID = "HNG-03667";
+const myLanguage = "Javascript";
+
+
+function helloWorld(fullName, ID, language){
+
+
+    const fullText = `Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task`;
+    console.log(fullText);
+
+}
+helloWorld(myFullName, myHNGi7ID, myLanguage);
+
+
+

ce3eee7b078c82b4abd7bcfdd95b44e7deaae518
Obamaolu <muyiwa_david36@yahoo.com>
2020-06-03 15:50:05 +0100

    Fix: Modify the output to the required result for the task

--- scripts/nightcrawler.js
+++ scripts/nightcrawler.js
@@ -3,4 +3,4 @@ let id = "HNG-03038"
 let language = "JavaScript"
 let email = "muyiwa_david36@yahoo.com"
 
-console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task`)
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`)

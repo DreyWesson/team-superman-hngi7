6cd0e05fbaae2df86a4835ff7f7af858d9afac78
Annysah <anisatakinbani13@gmail.com>
2020-06-03 11:39:55 +0100

    Create annysah.js

---
+++ scripts/annysah.js
@@ -0,0 +1,4 @@
+function myDetails(name, id, language) {
+    console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`)
+}
+myDetails('Anisat Akinbani', 'HNG-01710', 'javascript')

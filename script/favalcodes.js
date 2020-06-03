52e96d95c2da6cc4632ce2036ed3ab53c2b1d09e
User <luqmanlajani@gmail.com>
2020-06-03 18:26:30 +0100

    lawalluqman.py

---
+++ script/favalcodes.js
@@ -0,0 +1,7 @@
+const fullName = "Favour Ezinne Arua";
+const id = "HNG-04184";
+let lang = "Javascript";
+
+console.log(
+    "Hello World, This is " + fullName + ", "+"with HNGi7 ID as " + id + " using "+ lang +" for stage 2 task."
+    );
--- scripts/mowumi.py
+++
@@ -1,5 +0,0 @@
-def introduction(name, ID, language):
-    print("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task.".format(name, ID, language))
-
-
-introduction("Boluwatife Oduyemi", "HNG-00152", "Python")

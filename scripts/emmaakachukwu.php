d7ee9a9fade7e710a4b5de4ca67f958c8b6e60de
= <=>
2020-06-03 08:06:14 +0100

    emmaakachukwu's script for task 2

--- scripts/emmaakachukwu.php
+++ scripts/emmaakachukwu.php
@@ -1,6 +1,6 @@
 <?php
 $full_name = "Emmanuel Akachukwu";
-$id = "HNG-00326";
+$ id = "HNG-00326";
 $language = "PHP";
 
 $result = "Hello World, this is ${full_name} with HNGi7 ID ${id} using ${language} for stage 2 task";

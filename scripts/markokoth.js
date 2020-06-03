55773379aeb8f2db13079a3948d655c2f7870588
mark odhiambo okoth <markokoth96@gmail.com>
2020-06-03 16:08:45 +0300

    the email console

--- scripts/markokoth.js
+++ scripts/markokoth.js
@@ -1,5 +1,5 @@
 const markIntroduction = (fullName, id, language, email) => {
-    console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task.`)
+    console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`)
 }
 
 markIntroduction('Mark Okoth', 'HNG-01803', 'JavaScript', 'markokoth96@gmail.com')

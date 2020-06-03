f6c12fa1e322fc7f4015ca9e64ddd5f422642b20
AwaabSingh <56194701+AwaabSingh@users.noreply.github.com>
2020-06-03 16:20:09 +0100

    added email

--- scripts/AwaabScript.js
+++ scripts/AwaabScript.js
@@ -1,9 +1,10 @@
       function myInfomation (
                       fullName = 'Abdul-Kareem Abdul-Fattah',
                        id = "HNG-01706" ,
-                      language = 'JavaScript') {
+                      language = 'JavaScript',
+                      email = "adeoyin2828@gmail.com") {
           console.log (
-                `Hello World, this is ${fullName} with HNGI7 ID ${id}  using ${language} for stage 2 task`
+                `Hello World, this is ${fullName} with HNGI7 ID ${id}  using ${language} for stage 2 task. ${email}`
             );
   };
 

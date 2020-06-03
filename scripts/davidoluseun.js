cb5d9f05a19c3a4e1e419d6eea6389e87e4bc93c
David Oluseun <a.davidoluseun@gmail.com>
2020-06-03 14:18:59 +0100

    Update my script file to include my email

--- scripts/davidoluseun.js
+++ scripts/davidoluseun.js
@@ -3,8 +3,9 @@ function greetings() {
     let name = `Oluwaseun David Anifowose`;
     let id = `HNG-01609`;
     let lang = `JavaScript`;
+    let email = `a.davidoluseun@gmail.com`;
 
-    let message = `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task`;
+    let message = `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task.${email}`;
     console.log(message);
 }
 

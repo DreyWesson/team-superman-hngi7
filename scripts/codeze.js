ee70dc369fa5394b109673e304a55f0198e9d376
Eze Ochonogor <sendezeamail@gmail.com>
2020-06-03 14:03:19 +0400

    Added Email

--- scripts/codeze.js
+++ scripts/codeze.js
@@ -2,5 +2,5 @@
 let name = "Ochonogor Eze";
 let id = "HNG-00949";
 let language = "JavaScript";
-
-console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`);
+let email="sendezeamail@gmail.com";
+console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`);

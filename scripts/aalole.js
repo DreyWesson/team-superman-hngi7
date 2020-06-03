2949ff4882497cb82a6b10e64a37a3fbe442afbd
MR-aalole <52821144+Temitope25@users.noreply.github.com>
2020-06-03 09:45:31 +0200

    Removed new line character
    This is ther only character in that differs the instructions...

--- scripts/aalole.js
+++ scripts/aalole.js
@@ -1,5 +1,5 @@
 const aaloleHngDetails = (name, id, lang) =>
-  `Hello World.\n This is ${name},\n with HNGi7 ID ${id},\n using ${lang} for stage two task`;
+  `Hello World, this is ${name}, with HNGi7 ID ${id}, using ${lang} for stage two task`;
 
 aaloleHngDetails("Rasheed Mikail Abiodun", "HNG-02286", "JavaScript");
 console.log(aaloleHngDetails("Rasheed Mikail Abiodun", "HNG-02286", "JavaScript"));

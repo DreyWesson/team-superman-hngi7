91f57ea628545d5402d5aec756f6222fff1d974a
Osemeke Onyekachukwu <52927146+Osemeke89@users.noreply.github.com>
2020-06-02 17:44:42 -0700

    Add files via upload

---
+++ scripts/onyeka.php
@@ -0,0 +1,8 @@
+<?php 
+$FullName = "Osemeke Onyekachukwu";
+$HNGi7Id = "HNG-00924";
+$Lang = "PHP";
+
+
+echo "Hello World, this is " .$FullName. " with HNGi7 ID " .$HNGi7Id. " using " .$Lang. " for stage 2 task";
+?>

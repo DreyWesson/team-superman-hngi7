7fcd0093d78372b45f84b4cd9949abaa4b676026
Maheswar147 <preddy@ec.iitr.ac.in>
2020-06-03 20:09:53 +0530

    Updated email for my repo

--- scripts/Mahesh.js
+++ scripts/Mahesh.js
@@ -3,3 +3,4 @@ const id = "04099";
 const language = "Javascript";
 const emailId = "preddy@ec.iitr.ac.in"
 console.log("Hello World, this is " + userName + " with HNGi7 ID HNG-" + id + " using " + language + " for stage 2 task." + emailId);
+//Modified email for completion of task2

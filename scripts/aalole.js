dc7c9cb5912f0295f8e32fd8cbaadc24a7f4e6f5
MR-aalole <52821144+Temitope25@users.noreply.github.com>
2020-06-03 15:53:07 +0200

    Updated aalole script
     removed all the commas and added email in the specified order

--- scripts/aalole.js
+++ scripts/aalole.js
@@ -1,5 +1,6 @@
+
 const aaloleHngDetails = (name, id, lang, email) =>
-  `Hello World, this is ${name}, with HNGi7 ID ${id}, using ${lang} for stage two task. My Email is ${email}`;
+  `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task.${email}`;
 
 aaloleHngDetails("Rasheed Mikail Abiodun", "HNG-02286", "JavaScript", "Rasheedabiodun556@yahoo.com");
 console.log(aaloleHngDetails("Rasheed Mikail Abiodun", "HNG-02286", "JavaScript", "Rasheedabiodun556@yahoo.com"));

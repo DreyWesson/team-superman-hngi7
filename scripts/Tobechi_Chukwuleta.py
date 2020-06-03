909c10271c81678311f2e6bc4597415ebfadd54a
Tobechi <tobechichukwuleta125@gmail.com>
2020-06-03 16:04:12 -0700

    Tobechi scrript for task 2

--- scripts/Tobechi_Chukwuleta.py
+++ scripts/Tobechi_Chukwuleta.py
@@ -7,7 +7,7 @@ Created on Wed Jun  3 01:48:52 2020
 
 import sys
 def my_script():
-    print('Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task' % ('Chukwuleta Tobechi', 'HNG-03085', 'Python'))
+    print('Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task.%s' % ('Chukwuleta Tobechi', 'HNG-03085', 'Python', 'tobechichukwuleta125@gmail.com'))
     
 my_script()
 sys.stdout.flush()

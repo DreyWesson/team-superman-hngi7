af20ea9afe7ef847047b19fbc688b7ec723491e6
Favour Godwin Arua <52804141+Favalcodes@users.noreply.github.com>
2020-06-03 09:33:27 +0100

    Update favalcodes.js

--- scripts/favalcodes.js
+++ scripts/favalcodes.js
@@ -3,5 +3,5 @@ const id = "HNG-04184";
 let lang = "Javascript";
 
 console.log(
-    "Hello World, This is " + fullName + ", "+"with HNGi7 ID as " + id + " using "+ lang +" for stage 2 task."
+    "Hello World, this is " + fullName + " with HNGi7 ID " + id + " using "+ lang +" for stage 2 task."
     );

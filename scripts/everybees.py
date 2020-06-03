e84a4984851007e7457f01ada81c002b08f999af
everybees <everybees@gmail.com>
2020-06-03 13:38:41 +0100

    added my script - Jibola

--- scripts/everybees.py
+++ scripts/everybees.py
@@ -3,8 +3,8 @@ This is a script to print my details
 
 """
 
-def stage_one_task(name, email, id, language):
-    print("Hello World, this is {} with email {} and HNGi7 ID {} using {} for stage 2 task.".format(name, email, id, language))
+def stage_one_task(name, id, language):
+    print("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task.".format(name, id, language))
 
-
-stage_one_task("Jibola Bakare", "everybees@gmail.com", "HNG-03475", "Python")
+email = "everybees@gmail.com"
+stage_one_task("Jibola Bakare", "HNG-03475", "Python")

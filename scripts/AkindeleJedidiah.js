835a270b24069b95de9efa26828005df7de458f6
akinjed5 <62756856+akinjed5@users.noreply.github.com>
2020-06-03 09:27:49 +0100

    Create AkindeleJedidiah.js

---
+++ scripts/AkindeleJedidiah.js
@@ -0,0 +1,6 @@
+function helloHng(fullname, id, language) {
+  console.log(
+    `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task`
+  );
+}
+helloHng("Akindele Jedidiah", "HNG-01217", "Javascript");

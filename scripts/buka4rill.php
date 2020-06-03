8d034578e1f5e24446e2dd667334bd9d733ae644
WEB DEVELOPER <buka4rill@gmail.com>
2020-06-03 12:46:06 +0100

    Task 2 for Ebuka Abraham

---
+++ scripts/buka4rill.php
@@ -0,0 +1,15 @@
+<?php
+
+// Variables
+$fullName = "Ebuka Abraham";
+$id = "HNG-01238";
+
+$language = "PHP";
+
+$output = "Hello World, this is $fullName with HNGi7 ID $id using $language for stage 2 task";
+
+// EMAIL
+$email = "buka4rill@gmail.com";
+
+// Output 
+echo $output;

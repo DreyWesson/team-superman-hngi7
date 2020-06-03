81a8a588c6129a3b51502cce9373a2f55683e46d
daphycisist <daphycisist@gmail.com>
2020-06-03 09:19:15 +0100

    add agiamah to script

--- scripts/agiamah.js
+++ scripts/agiamah.js
@@ -2,4 +2,4 @@ const name = 'Agiamah Precious';
 const hngId = 'HNG-04421';
 const language = 'javascript';
 
-console.log(`Hello world, this is ${name} with HNGi7 ID:${hngId} using ${language} for stage 2 task`);
+console.log(`Hello world, this is ${name} with HNGi7 ID ${hngId} using ${language} for stage 2 task`);

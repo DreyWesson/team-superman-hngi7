ba5d4cb7750937f76531bc5ac029dcb0972ea17f
berniescribblings <65433039+berniescribblings@users.noreply.github.com>
2020-06-03 21:07:22 +0100

    Add files via upload
    bernard script

---
+++ Bernard.js
@@ -0,0 +1,7 @@
+ function firsttask (fullname, HNGid, language, email)
+ {
+     console.log(
+         "hello World, this is $ {fullname} with HNGi7 ID ${ HNGid} using ${language} for stage 2 task.${email}"
+         );
+ }
+ firsttask ("Obinna Bernard Ofoegbu", "HNG-01203", "javascript", "obinnaofoegbu71@gmail.com");

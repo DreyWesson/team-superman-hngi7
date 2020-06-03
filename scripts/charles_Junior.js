ee6478e9fe19b63a8d10e3a7c293bd2637218cbd
Masterspencer <45554831+Masterspencer@users.noreply.github.com>
2020-06-03 12:06:40 +0100

    update to the script to output email information

--- scripts/charles_Junior.js
+++ scripts/charles_Junior.js
@@ -1,12 +1,13 @@
 let userDetails = {
     userName: "",
     id: "",
+    email: "",
     lang: "",
-    displayInfo: (userName, id, lang) => {
+    displayInfo: (userName, id, email,lang) => {
 
-        console.log (`Hello World, this is ${userName} with HNGi7 ID ${id} using ${lang} for stage 2 task`);
+        console.log (`Hello World, this is ${userName} with HNGi7 ID ${id} and email ${email} using ${lang} for stage 2 task`);
 
     }
 };
-let { userName, id, lang } = userDetails;
-userDetails.displayInfo("Charles Nwachebelu", "HNG-04564", "javaScript");
+let { userName, id, email, lang } = userDetails;
+userDetails.displayInfo("Charles Nwachebelu", "HNG-04564","nwachebeluanthony@gmail.com" "javaScript");

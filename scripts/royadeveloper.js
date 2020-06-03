a301f1245d8ac3557f820e0f87ede544c893e62f
Chimaobi Prince <60024033+royadeveloper01@users.noreply.github.com>
2020-06-03 12:02:08 +0000

    Update royadeveloper.js

--- scripts/royadeveloper.js
+++ scripts/royadeveloper.js
@@ -3,7 +3,7 @@ const id = "HNG-00996";
 const email = "royadeveloper01@gmail.com";
 const language = "JavaScript";
 
-const myInfo = (name, id, email, language) => {
-    console.log(`Hello World, This is ${name} with HNGi7 ID ${id} and ${email} using ${language} for stage two task`);
+const myInfo = (name, id, language) => {
+    console.log(`Hello World, This is ${name} with HNGi7 ID ${id} using ${language} for stage two task`);
 }
-myInfo(name,id,email,language);
+myInfo(name,id,language);

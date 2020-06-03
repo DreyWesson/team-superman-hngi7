fc0b545c8a5d88aef16a335a256a595f6cc4a537
nkemjikaobi <nkemjikaobi@gmail.com>
2020-06-03 10:34:37 +0100

    Nkemjika Obi Stage 2 Task

---
+++ scripts/nkemjikaobi.php
@@ -0,0 +1,13 @@
+<?php
+
+//STAGE 2 TASK
+
+//Declare the variables
+$name = 'Nkemjika Obi';
+$id = 'HNG-01283';
+$language = 'PHP';
+
+//Display the output
+echo 'Hello World, this is '.$name.' with HNGi7 ID ' .$id.' using '.$language.' for stage 2 task';
+
+?>

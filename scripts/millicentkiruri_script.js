7493572d0b6dbe28a9f05eb7d03010a2af87d4d1
Millicent Kiruri <40162588+MissKiruri@users.noreply.github.com>
2020-06-03 14:37:54 +0300

    Update millicentkiruri_script.js

--- scripts/millicentkiruri_script.js
+++ scripts/millicentkiruri_script.js
@@ -1,7 +1,7 @@
 function myScript(fullname,ID,language){
 console.log(
-'Hello World, this is ${fullname} with HNGi7 ID ${ID} using
-${language} for stage 2 task'
+`Hello World, this is ${fullname} with HNGi7 ID ${ID} using
+${language} for stage 2 task`
 );
 }
 myScript("Millicent Kiruri", "HNG-03381", "JavaScript");

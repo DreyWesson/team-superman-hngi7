aa299bef35c4be49641063be01a08dac61302697
Emmanuel Eboh <63825997+EOEboh@users.noreply.github.com>
2020-06-03 02:28:06 +0100

    Update Emmanuel_Eboh.js

--- scripts/Emmanuel_Eboh.js
+++ scripts/Emmanuel_Eboh.js
@@ -3,6 +3,6 @@
     const id = "HNG-01574";
     const lang = "JavaScript";
     console.log(
-        `Hello World, this is ${name} with HNGi7 ${id} using ${lang} for stage 2 task.`
+        `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task.`
         );
 

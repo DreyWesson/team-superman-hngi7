e4e21c457a5f041e7b79372f90e36f80d670cabc
mutiu.adeleye <mutiu.adeleye@automataassociates.com>
2020-06-03 14:14:18 +0100

    Added Email Address

---
+++ scripts/ArinzeAlekeHNGTask2 (2).php
@@ -0,0 +1,20 @@
+<?php
+
+$name = "Arinzechukwu Aleke";
+$id = "HNG-03073";
+$language = "php";
+$email = "emmanuelaleke@gmail.com";
+
+
+$welcome='Hello World, this is '.$name .' with HNGi7 ID'. $id.'using '.$language . ' for stage 2 task. Email address: '. $email;
+
+
+
+
+
+echo $welcome;
+
+
+
+
+?>

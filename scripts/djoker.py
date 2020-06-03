ee7e7380f5461cdfdd83b902cfa7174f37b8e262
chidiarua <chidiarua99@gmail.com>
2020-06-03 15:19:50 +0100

    task 2 commit

--- scripts/djoker.py
+++ scripts/djoker.py
@@ -1,7 +1,6 @@
 name = 'Chidi Arua'
-email = 'chidiarua99@gmail.com'
 hng_id = 'HNG-05000'
+email = 'chidiarua99@gmail.com'
 language = 'Python'
 
-print(f"Hello world, this is {name} with HNG ID {hng_id} and email {email}, using {language} for stage 2 task")
-
+print(f"Hello World, this is {name} with HNGi7 ID {hng_id} using {language} for stage 2 task.{email}")

f83c73817ac708ee63262b7ac95acddbb371a44c
DevAdedoyin <59482569+DevAdedoyin@users.noreply.github.com>
2020-06-03 16:17:29 +0100

    Update idris_adedoyin_03148.py

--- scripts/idris_adedoyin_03148.py
+++ scripts/idris_adedoyin_03148.py
@@ -1,2 +1,1 @@
-email = "dreezybines@gmail.com"
-print("Hello World, this is Idris Adedoyin with HNGi7 ID HNG-03148 using Python for stage 2 task")
+print("Hello World, this is Idris Adedoyin with HNGi7 ID HNG-03148 using Python for stage 2 task.dreezybines@gmail.com")

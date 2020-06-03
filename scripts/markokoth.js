e0e30a22fde72cc9bbe6167f68378d3061a08268
mark odhiambo okoth <markokoth96@gmail.com>
2020-06-03 11:14:08 +0300

    mark okoth tasks

---
+++ scripts/markokoth.js
@@ -0,0 +1,5 @@
+const markIntroduction = (fullName, id, language) => {
+    console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`)
+}
+
+markIntroduction('Mark Okoth', 'HNG-01803', 'JavaScript')

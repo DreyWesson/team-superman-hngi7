31134b4b645cdbbd780a9d0fa2e5599d0154505e
Michael John <johnmikeloy@gmail.com>
2020-06-04 00:20:29 +0100

    javascript

---
+++ scripts/michaeloy.js
@@ -0,0 +1,5 @@
+const myInfo = (fullname, id, lang, email) => {
+    console.log(`Hello World, this is ${fullname} with HNGi7 ID ${id} using ${lang} for stage 2 task.${email}`);
+}
+
+myInfo("Michael John", "HNG-00424", "JavaScript", "johnmikeloy@gmail.com");

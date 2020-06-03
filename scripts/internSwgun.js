fb1ec4738a4c5dfefcabfef80a0614f59b4fc351
Vik2ry <41373128+Vik2ry@users.noreply.github.com>
2020-06-03 07:21:07 +0100

    Update internSwgun.js
    My attempt

--- scripts/internSwgun.js
+++ scripts/internSwgun.js
@@ -2,4 +2,4 @@ let fullName = "BANJI Olusegun Kolawole"
 let hngID = "HNG-01595"
 let language = "JavaScript"
 
-console.log(`Hello World, this is ${fullName} with HNGi7 ID: ${hngID} using ${language} for stage 2 task`)
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hngID} using ${language} for stage 2 task`)

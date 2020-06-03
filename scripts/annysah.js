45f15c058fa3b0d8788e2fadc9fd026631d0b90f
Anisat Akinbani <52763841+Annysah@users.noreply.github.com>
2020-06-03 13:25:25 +0100

    Delete annysah.js

--- scripts/annysah.js
+++
@@ -1,9 +0,0 @@
-const name = 'Anisat Akinbani'
-const id = 'HNG-01710'
-const email = 'anisatakinbani13@gmail.com'
-const language = 'javascript'
-
-function myDetails(name, id, language) {
-    console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`)
-}
-myDetails('Anisat Akinbani', 'HNG-01710', 'javascript')

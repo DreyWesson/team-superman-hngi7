f93da14cad49772f41efbc5f40bb42c297c14aa9
Emmanuel Egbo <52846684+Emmanuelhills@users.noreply.github.com>
2020-06-03 14:12:44 +0100

    Added my email to the JS File

--- scripts/EgboEmmanuel.js
+++ scripts/EgboEmmanuel.js
@@ -1,6 +1,6 @@
-function taskHng(fullname, id, language) {
+function taskHng(fullname, id, language, email) {
   console.log(
-    `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task`
+    `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task.{email}`
   );
 }
-taskHng("Egbo Emmanuel", "HNG-04843", "Javascript");
+taskHng("Egbo Emmanuel", "HNG-04843", "Javascript", "emmanuelhills255@gmail.com");

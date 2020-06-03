5bf6c86c5e3472344379735a4a4fef3c0e2d2e00
Fatimah <daviesfatimah97@gmail.com>
2020-06-03 16:32:18 -0700

    concatenated email

--- scripts/davies.js
+++ scripts/davies.js
@@ -1,5 +1,4 @@
-let daviesEmail="daviesfatimah97@gmail.com";
-const helloHng=(hngName, hngId, language)=>{
-    console.log(`Hello World, this is ${hngName} with HNGi7 ID ${hngId} using ${language} for stage 2 task`);
+const helloHng=(hngName, hngId, language, email)=>{
+    console.log(`Hello World, this is ${hngName} with HNGi7 ID ${hngId} using ${language} for stage 2 task.${email}`);
 }
-helloHng("Fatimah Davies", "HNG-04778", "Javascript");
+helloHng("Fatimah Davies", "HNG-04778", "Javascript", "daviesfatimah97@gmail.com");

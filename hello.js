ee7fb3276563ccb2e6a9395c9b59bc42c8147076
Teepath <tundeadebanjo246@gmail.com>
2020-06-03 11:33:07 +0100

    new task2

---
+++ hello.js
@@ -0,0 +1,6 @@
+let fullName = "Tunde";
+let id = "HNG-02925";
+let language = "javascript";
+
+
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task`)

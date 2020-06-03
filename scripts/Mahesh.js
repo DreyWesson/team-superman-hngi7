e34ac8da7cd43079766839b0b42acb6c0698c2ce
Maheswar147 <preddy@ec.iitr.ac.in>
2020-06-03 19:57:18 +0530

    used Javscript and commited to task-2

--- scripts/Mahesh.js
+++ scripts/Mahesh.js
@@ -1,6 +1,5 @@
 const userName = "Maheswar reddy";
 const id = "04099";
 const language = "Javascript";
-console.log("Hello World, this is " + userName + " with HNGi7 ID HNG-" + id + " using " + language + " for stage 2 task");
-//email id for slack -- "preddy@ec.iitr.ac.in"
-
+const emailId = "preddy@ec.iitr.ac.in"
+console.log("Hello World, this is " + userName + " with HNGi7 ID HNG-" + id + " using " + language + " for stage 2 task." + emailId);

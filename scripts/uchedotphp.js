9b045eca26a2095847da05c3e51eaba73e08c5e4
uchedotphp <uchedotphp@gmail.com>
2020-06-03 13:13:09 +0100

    added email address

--- scripts/uchedotphp.js
+++ scripts/uchedotphp.js
@@ -2,6 +2,10 @@
   "use strict";
   const email = "uchedotphp@gmail.com";
   const statement =
-    "Hello World, this is Nwulu Uchechukwu Prince with HNGi7 ID HNG-03931 using JavaScript for stage 2 task";
+    "Hello World, this is Nwulu Uchechukwu Prince with HNGi7 ID HNG-03931 and email" +
+    " " +
+    email +
+    " " +
+    "using JavaScript for stage 2 task";
   console.log(statement);
 })();

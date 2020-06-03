1506ac1371b3d8fb5abf77cb9bc5ece0bac435f4
oluwaseyifunmitan Muyiwa <49420652+Seyifunmitan@users.noreply.github.com>
2020-06-03 08:20:00 +0100

    Rename task.js to seyifunmitan.js

--- scripts/task.js
+++
@@ -1,5 +0,0 @@
-let fullName = "Ademujimi oluwaseyi"
-let hngId = "HNG-00685"
-let language = "JavaScript"
-
-console.log(`Hello World, this is ${fullName} with HNGi7 ID: ${hngId} using ${language} for stage 2 task`)

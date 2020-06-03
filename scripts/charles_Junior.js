3f4ddb112146fa60ae60322c1043eae48898622f
masterspencer <thecharlesjunior@gmail.com>
2020-06-03 12:22:46 +0100

    final upload for my project

--- scripts/charles_Junior.js
+++ scripts/charles_Junior.js
@@ -2,11 +2,12 @@ let userDetails = {
     userName: "",
     id: "",
     lang: "",
+    email: "",
     displayInfo: (userName, id, lang) => {
 
         console.log (`Hello World, this is ${userName} with HNGi7 ID ${id} using ${lang} for stage 2 task`);
 
     }
 };
-let { userName, id, lang } = userDetails;
-userDetails.displayInfo("Charles Nwachebelu", "HNG-04564", "javaScript");
+let { userName, id, lang, email } = userDetails;
+userDetails.displayInfo("Charles Nwachebelu", "HNG-04564", "javaScript", "nwachebeluanthony@gmail.com");

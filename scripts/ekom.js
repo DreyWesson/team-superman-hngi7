5db5e760f4267d0f4660ddfca2a8198445a58473
ekpoekom <54278950+ekpoekom@users.noreply.github.com>
2020-06-03 10:51:15 -0700

    second commit:with updated email instructions
    Signed-off-by: ekpoekom <54278950+ekpoekom@users.noreply.github.com>

--- scripts/ekom.js
+++ scripts/ekom.js
@@ -1,1 +1,19 @@
-console.log('Hello World, this is Ekpo Ekom with HNGi7 ID HNG-05281 using Javascript for stage 2 task');
+function Info(name, id, email, language){
+    this.yourName=name;
+    this.yourId=id;
+    this.yourEmail=email;
+    this.yourLanguage=language;
+}
+
+
+function output(){
+    console.log(`Hello World, this is Ekpo Ekom with HNGi7 ID HNG-05281 using Javascript for stage 2 task`);
+}
+
+let myName="Ekpo Ekom";
+let myId="HNG-05281";
+let myEmail = "ekpoekom@gmail.com";
+let myLanguage="JavaScript";
+
+let info=new Info(myName, myId, myEmail, myLanguage);
+output();

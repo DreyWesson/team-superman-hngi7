1326013555de26bea9d03627683cd608fa979b9b
Foresight #X <sundayamuke7@gmail.com>
2020-06-03 09:52:32 +0100

    Update: Corrected some matching errors.

--- scripts/Foresight.php
+++ scripts/Foresight.php
@@ -3,5 +3,5 @@ $full_name = "Sunday Amuke";
 $id = "02082";
 $lang = "PHP";
 
-echo "Hello World, this is ".$full_name." with HNGi7 ID ".$id." using ".$lang." for stage 2 task";
+echo "Hello World, this is ".$full_name." with HNGi7 ID-".$id." using ".$lang." for stage 2 task";
 ?>

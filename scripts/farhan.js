f582c47e8a37f27dcbc057f7b647e54e6d3f94cc
Sodiq Farhan <farhansodiq360@gmail.com>
2020-06-03 20:54:18 +0100

    latest updated script with email; farhan.js

---
+++ scripts/farhan.js
@@ -0,0 +1,11 @@
+function myInfo() {
+
+    let fullname = "Sodiq Farihan";
+    let id = "HNG-03238";
+    let language = "JavaScript";
+    let email = "farhansodiq360@gmail.com";
+
+    console.log(`Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`);
+}
+
+myInfo();

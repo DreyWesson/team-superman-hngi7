800b9874b4db9cb8a69033fb018f91ec2f3ea8b3
abdulsalamcodes <59220264+abdulsalamcodes@users.noreply.github.com>
2020-06-03 15:31:09 +0100

    Update abdulsalam.js
    An email address is added.

--- scripts/abdulsalam.js
+++ scripts/abdulsalam.js
@@ -2,8 +2,9 @@ let intern = {
     devName : 'Abodunrin Abdulsalam',
     id : 'HNG-00361',
     language : 'Javscript',
+    email : 'abdulsalamabodunrin369@gmail.com',
     sayHello() {
-        return `Hello World, this ${this.devName} with HNGi7 ID ${this.id} using ${this.language} for stage 2 task`;
+        return `Hello World, this ${this.devName} with HNGi7 ID ${this.id} using ${this.language} for stage 2 task.${this.email}`;
     }
 }
 console.log(intern.sayHello())

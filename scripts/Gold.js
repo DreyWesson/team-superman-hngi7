c6a38c856dc002e431e8529cdab163d1c08b5f2a
Okiemute Gold <goldokiemute@gmail.com>
2020-06-04 00:48:49 +0100

    Update Gold.js

--- scripts/Gold.js
+++ scripts/Gold.js
@@ -1,9 +1,9 @@
 let fullname = 'Okiemute Gold';
 let hngid = 'HNG-01033';
-let email = 'goldokiemute@gmail.com';
 let language = 'Javascript';
+let email = 'goldokiemute@gmail.com';
 
-function goldTask2(fullname, hngid, email, language) {
-console.log(`Hello World, this is ${fullname} with HNGi7 ID ${hngid} and email ${email} using ${language} for stage 2 task.`);
+function goldTask2(fullname, hngid, language, email) {
+    console.log(`Hello World, this is ${fullname} with HNGi7 ID ${hngid} and using ${language} for stage 2 task.${email}`);
 }
-goldTask2('Okiemute Gold', 'HNG-01033', 'goldokiemute@gmail.com', 'Javascript')
+goldTask2('Okiemute Gold', 'HNG-01033', 'Javascript', 'goldokiemute@gmail.com')

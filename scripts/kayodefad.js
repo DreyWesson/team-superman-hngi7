40d20390eaa4e466743e61a1307946a7c6ad1c08
Oluwakayode <fadojuk@gmail.com>
2020-06-03 13:34:38 +0100

    Added email parameter

--- scripts/kayodefad.js
+++ scripts/kayodefad.js
@@ -1,5 +1,12 @@
-const introduceYou = (fullName, id, language) => {
-    console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`)
-}
+const introduceYou = (fullName, id, language, email) => {
+	console.log(
+		`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`
+	);
+};
 
-introduceYou('Oluwakayode Fadoju', 'HNG-01390', 'JavaScript')
+introduceYou(
+	'Oluwakayode Fadoju',
+	'HNG-01390',
+	'JavaScript',
+	'fadojuk@gmail.com'
+);

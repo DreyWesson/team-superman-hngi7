fb28760a7bbb374e756743e361040afba4f57d14
Maheswar147 <preddy@ec.iitr.ac.in>
2020-06-03 14:38:01 +0530

    Commited to task-2

---
+++ scripts/Mahesh.js
@@ -0,0 +1,6 @@
+const userName = "Maheswar reddy";
+const id = "04099";
+const language = "Javascript";
+console.log("Hello World, this is " + userName + " with HNGi7 ID HNG-" + id + " using " + language + " for stage 2 task");
+//email id for slack -- "preddy@ec.iitr.ac.in"
+

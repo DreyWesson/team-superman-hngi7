bb0ca464eea9029e5885a095e68e2f02d653d506
sey cole <seyicole>
2020-06-03 21:50:39 +0100

    second commit

--- scripts/S3.js
+++ scripts/S3.js
@@ -1,8 +1,10 @@
+let name = 'Oluseyi Imanah';
+let  id = 'HNG-03003';
+let language = 'javascript';
+let email = 'seyicole01@gmail.com';
+
 const myScript = () => {
-    let name = 'Oluseyi Imanah';
-   let  id = 'HNG-03003';
-    let language = 'javascript';
-   let  info =`Hello world, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+   let  info =`Hello world, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`;
  console.log(info);
      
  }

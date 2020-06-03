03d7c261b7d18ab5611f6cfda77aed9ad25a0726
Fatimah <daviesfatimah97@gmail.com>
2020-06-03 14:38:08 -0700

    print davies fatimah's info

---
+++ scripts/davies.js
@@ -0,0 +1,5 @@
+let daviesEmail="daviesfatimah97@gmail.com";
+const helloHng=(hngName, hngId, language)=>{
+    console.log(`Hello World, this is ${hngName} with HNGi7 ID ${hngId} using ${language} for stage 2 task`);
+}
+helloHng("Fatimah Davies", "HNG-04778", "Javascript");

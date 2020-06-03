a72080f8ef02f3be2c92fab6966ee4526bc2d7a6
Pablo <royadeveloper01@gmail.com>
2020-06-03 11:09:29 +0000

    added my Id

--- scripts/royadeveloper.js
+++ scripts/royadeveloper.js
@@ -1,4 +1,4 @@
 const myInfo = (fullName, ID, language) => {
-    console.log(`Hello World, This is ${fullName} with HNGi7 ID-${ID} using ${language} for stage two task`);
+    console.log(`Hello World, This is ${fullName} with HNGi7 ID ${ID} using ${language} for stage two task`);
 }
-myInfo("Prince Chimaobi", "00996", "JavaScript");
+myInfo("Prince Chimaobi", "HNG-00996", "JavaScript");

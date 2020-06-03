000472abb8904d15f51d0a629d72451e40e1ad43
AwaabSingh <56194701+AwaabSingh@users.noreply.github.com>
2020-06-03 13:25:47 +0100

    uploded my script

---
+++ scripts/AwaabScript.js
@@ -0,0 +1,12 @@
+      function myInfomation (
+                      fullName = 'Abdul-Kareem Abdul-Fattah',
+                       id = "HNG-01706" ,
+                      language = 'JavaScript') {
+          console.log (
+                `Hello World, this is ${fullName} with HNGI7 ID ${id}  using ${language} for stage 2 task`
+            );
+  };
+
+  myInfomation();
+
+  

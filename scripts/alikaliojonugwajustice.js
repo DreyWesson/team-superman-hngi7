12d96339033a020447ae93e6e2b4e0cdffebd01d
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-06-03 14:36:21 +0100

    Update alikaliojonugwajustice.js with email parameter

--- scripts/alikaliojonugwajustice.js
+++ scripts/alikaliojonugwajustice.js
@@ -1,13 +1,14 @@
-const ALIKALI_OJONUGWA_JUSTICE_HNG_Details = ({ name, id, lang }) =>
-  `Hello World.\nThis is ${name},\nwith HNGi7 ID ${id},\nusing ${lang} for stage two task
-  `;
+const ALIKALI_OJONUGWA_JUSTICE_HNG_Details = ({ name, id, lang, email }) =>
+  `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task.${email}`;
 
 console.log(
   ALIKALI_OJONUGWA_JUSTICE_HNG_Details(
     {
       name: "Alikali Ojonugwa Justice",
       id: "HNG-03822",
-      lang: "JavaScript"
+      lang: "JavaScript",
+      email: "alikali.ojonugwajustice@gmail.com"
     }
   )
 );
+

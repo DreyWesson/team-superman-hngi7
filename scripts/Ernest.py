8182e95c527705e2e5692de5d886a642e7b7f78f
E-STAT <owojori.tolulope@gmail.com>
2020-06-04 00:45:54 +0100

    Ernest Script editted

--- scripts/Ernest.py
+++ scripts/Ernest.py
@@ -3,12 +3,12 @@ hng_id_user = "HNG-01824"
 email = "owojori.tolulope@gmail.com"
 language = "Python"
 
-def task(Name, hng_id, language):
-    output = "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task".format(Name, hng_id,language)
+def task(Name, hng_id, language, email):
+    output = "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task. {}".format(Name, hng_id,language, email)
     return output
 
 
-print(task(Name = Name_user, hng_id = hng_id_user, language=language))
+print(task(Name = Name_user, hng_id = hng_id_user, language=language, email = email))
 
 
     

45d880cf9f4e6392ace6b7f7690eab820f4ecdcc
Hasneh <66261479+Hasneh@users.noreply.github.com>
2020-06-03 22:11:40 +0100

    My script file
    Script file with JavaScript

---
+++ scripts/hasneh.js
@@ -0,0 +1,9 @@
+var name = "Hassan Oladeji";
+var id = "HNG-01102";
+var lang = "JavaScript";
+var email = "hassanoladeji0@gmail.com";
+
+function hasneh(name,id,lang,email){
+    console.log("Hello World, this is "+name+" with HNGi7 ID "+id+" using "+lang+" for stage 2 task."+email);
+}
+hasneh(name,id,lang,email);

d3441113b3af18e56fa51b0e609a3e6c756d897a
atiloladave <60699516+atiloladave@users.noreply.github.com>
2020-06-03 00:18:54 +0000

    Update atilola.js

--- scripts/atilola.js
+++ scripts/atilola.js
@@ -5,6 +5,6 @@ function davidAtilola(){
     let iD = 'HNG-01583'
     let language = 'JavaScript';
 
-    console.log (`${welcome}, this is ${fullName} with the HNGi7 ID: ${iD} using ${language} for stage 2 task.`)
+    console.log (`${welcome}, this is ${fullName} with the HNGi7 ID ${iD} using ${language} for stage 2 task.`)
 }
 davidAtilola()

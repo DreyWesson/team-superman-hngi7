412e349f74412c4bb5c4e07f6d3caf4ba38f5bf9
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-06-03 16:39:17 +0100

    index.php modified

--- index.php
+++ index.php
@@ -31,14 +31,6 @@ if ($files) {
 
     echo '<br /><br />';
   }
-}
-
-if (!isset($_GET['json'])) {
-    ?>
-    <!DOCTYPE html>
-    <html lang="en">
-
-    <head>
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">

8e858bcf48e3ba887f1fa3f0358c12d72a384929
uchedotphp <uchedotphp@gmail.com>
2020-06-03 12:59:08 +0100

    First initial commit to Team Superman

---
+++ scripts/uchedotphp.js
@@ -0,0 +1,7 @@
+(function () {
+  "use strict";
+  const email = "uchedotphp@gmail.com";
+  const statement =
+    "Hello World, this is Nwulu Uchechukwu Prince with HNGi7 ID HNG-03931 using JavaScript for stage 2 task";
+  console.log(statement);
+})();

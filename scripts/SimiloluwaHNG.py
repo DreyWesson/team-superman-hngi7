b00b625350f58c3078842ae18f1548d59d55c867
rexsimiloluwah <adetoyosi2001@gmail.com>
2020-06-03 02:20:32 +0100

    New Changes

--- scripts/SimiloluwaHNG.py
+++ scripts/SimiloluwaHNG.py
@@ -1,1 +1,2 @@
-print("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task" %("Similoluwa Okunowo","HNG02950","Python"))
+print("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task" %("Similoluwa Okunowo","HNG-02950","Python"))
+

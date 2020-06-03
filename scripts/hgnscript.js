00b8e1c2ad2f3fa7ecc648ab323e4d2a2730104b
Marvellous <fmarvelsk@gmail.com>
2020-06-03 16:20:42 +0100

    Javascipt

--- scripts/hgnscript.js
+++ scripts/hgnscript.js
@@ -1,5 +1,5 @@
-const hgn = (name, id, lang) => {
-  console.log('Hello world, this is ' + name + ' with HNGi7 ID '  + id + ' using ' + lang +' for stage 2 task ')
+const hgn = (fullname, id, email, lang) => {
+  console.log(`Hello world, this is ${fullname} with HNGi7 ID ${id} using ${lang} for stage 2 task. ${email}`)
 
 }
-hgn('Jide-Filani Marvellous', 'HNG-02447', 'Javascript')
+hgn('Jide-Filani Marvellous', 'HNG-02447', 'fmarvelsk@gmail.com', 'Javascript')
--- scripts/myscript.js
+++
@@ -1,6 +0,0 @@
-function sayHello(fullname, id, language) {
-  console.log(
-    `Hello World, this is ${fullname} with HNGi7 ${id} using ${language}`
-  );
-}
-sayHello(fullname, id, language);

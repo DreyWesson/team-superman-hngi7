d61d34ff6301de7e41804c13679ed51ec3f50c02
Shittu Promise <eopas4christ@gmail.com>
2020-06-03 15:22:10 +0100

    Added email address

--- scripts/promiseShittu.py
+++ scripts/promiseShittu.py
@@ -1,4 +1,5 @@
-def task_two(name, hng_id, stack):
-    print("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task" .format(name, hng_id, stack))
+def task_two(name, hng_id, stack, email_address):
+    print("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task.{3}" .format(name, hng_id, stack, email_address))
 
-task_two("Promise Shittu", "HNG-06109", "Python")
+
+task_two("Promise Shittu", "HNG-06109", "Python", "promise.aeos@gmail.com")

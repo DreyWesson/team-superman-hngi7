3982bdc121668573dbb759cc77ee6faf97ad86b7
newton <chiakajunior@yahoo.com>
2020-06-03 15:50:32 +0100

    added my email

--- scripts/cj.js
+++ scripts/cj.js
@@ -1,9 +1,9 @@
-const myInfo = (name, ID, Language) => {
+const myInfo = (name, ID, Language, Email) => {
 
  console.log (
-    `Hello World, this is ${name} with HNGi7 ID ${ID} using ${Language} for stage 2 task`
+    `Hello World, this is ${name} with HNGi7 ID ${ID} using ${Language} for stage 2 task. ${Email}`
   );
 
 }
 
-myInfo("Osuji Chiaka Daniel", "HNG-03090", "Javascript");
+myInfo("Osuji Chiaka Daniel", "HNG-03090", "Javascript", "chikajunior!9@gmail.com");

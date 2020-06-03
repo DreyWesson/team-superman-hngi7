49e593ff5078a9dd78a1c1235b70919b1826a480
Marvellous <fmarvelsk@gmail.com>
2020-06-03 17:19:13 +0100

    Javascript

--- scripts/hgnscript.js
+++ scripts/hgnscript.js
@@ -1,5 +1,6 @@
-const hgn = (fullname, id, email, lang) => {
-  console.log(`Hello world, this is ${fullname} with HNGi7 ID ${id} using ${lang} for stage 2 task. ${email}`)
+const hgn = (fullname, id, lang) => {
+  let email =  'fmarvelsk@gmail.com'
+  console.log(`Hello world, this is ${fullname} with HNGi7 ID ${id} using ${lang} for stage 2 task.${email}`)
 
 }
-hgn('Jide-Filani Marvellous', 'HNG-02447', 'fmarvelsk@gmail.com', 'Javascript')
+hgn('Jide-Filani Marvellous','HNG-02447','Javascript')

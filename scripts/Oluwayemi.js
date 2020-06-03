f5aabdea5243a9f8af130c96980a706c3ca3da94
oluwagiga <55166096+oluwagiga@users.noreply.github.com>
2020-06-03 23:20:40 +0100

    team task
    This is my first commit on task 2

---
+++ scripts/Oluwayemi.js
@@ -0,0 +1,8 @@
+let script = {
+    text: "Hello World",
+    fullname: "Oluwayemi",
+    ID: 'HNG-0' + 4156,
+    language: "JavaScript"
+};
+
+console.log(script.text + ", this is " + script.fullname + " with HNGi7 ID " + script.ID + " using " + script.language + " for stage 2 task. ");

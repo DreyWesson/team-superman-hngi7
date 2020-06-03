5ce5b8c9d15cbbd5eed5e377d46c2b88295ce099
Crystal-Techworld <Johnowolabi41@yahoo.com>
2020-06-03 10:47:19 +0100

    initial commit for my javascript file to team task

---
+++ scripts/crystal.js
@@ -0,0 +1,12 @@
+var fullName = "Opeyemi Owolabi";
+var id = "HNG-03599";
+var email = "Johnowolabi41@yahoo.com"
+var language = "Javascript";
+
+var ouput = "Hello World, this is " + fullName +  " with HNGi7 ID " + id + " using " + language + " for stage 2 task";
+
+console.log(ouput);
+console.log(fullName);
+console.log(id);
+console.log(email);
+console.log(language);

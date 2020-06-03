fb3ddfd37b0aa70a9a6516c1a46567fdfe09ec2a
= <=>
2020-06-03 08:03:35 +0100

    first commit

---
+++ scripts/emmaakachukwu.php
@@ -0,0 +1,8 @@
+<?php
+$full_name = "Emmanuel Akachukwu";
+$id = "HNG-00326";
+$language = "PHP";
+
+$result = "Hello World, this is ${full_name} with HNGi7 ID ${id} using ${language} for stage 2 task";
+
+echo $result;

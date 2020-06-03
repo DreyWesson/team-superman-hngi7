1df0dc3d919b89a2d7d5c381aab2a0f4bfc4229a
Chidubem Nwankwo <63216507+chydube@users.noreply.github.com>
2020-06-03 12:41:48 -0700

    Update chydube.php

--- scripts/chydube.php
+++ scripts/chydube.php
@@ -3,7 +3,7 @@
  //function to desplay intern detials
  function intern($name, $id, $pro_lang, $email)
  {
-     return "Hello World, this is ".$name. " with HNGi7 id ".$id. " using ".$pro_lang." for stage 2 task..".$email;
+     return "Hello World, this is ".$name. " with HNGi7 id ".$id. " using ".$pro_lang." for stage 2 task.".$email;
  }
 
  $output = intern("Chidubem Nwankwo", "HNG-00366","PHP","Dubem360@gmail.com");

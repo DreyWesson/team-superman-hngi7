11ba14e491fd5a68dd48895cc3a9af9cdcc7ff73
abdulsalamcodes <59220264+abdulsalamcodes@users.noreply.github.com>
2020-06-03 04:29:59 +0100

    delete abdulsalam.js file from the root

--- abdulsalam.js
+++
@@ -1,9 +0,0 @@
-let intern = {
-    devName : 'Abodunrin Abdulsalam',
-    id : 'HNG-00361',
-    language : 'Javscript',
-    sayHello() {
-        return `Hello World, this ${this.devName} with HNGi7 ID ${this.id} using ${this.language} for stage 2 task`;
-    }
-}
-console.log(intern.sayHello())

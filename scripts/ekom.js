14990c67b194792411b00f5d6706cdaa8847b4c1
ekpoekom <54278950+ekpoekom@users.noreply.github.com>
2020-06-03 14:55:41 -0700

    update on email

--- scripts/ekom.js
+++ scripts/ekom.js
@@ -6,15 +6,17 @@ function Info(name, id, email, language){
     this.yourLanguage=language;
 }
 
-
-function output(){
-    console.log(`Hello World, this is Ekpo Ekom with HNGi7 ID HNG-05281 using Javascript for stage 2 task`);
-}
-
 let myName="Ekpo Ekom";
 let myId="HNG-05281";
 let myEmail = "ekpoekom@gmail.com";
 let myLanguage="JavaScript";
+
+
+function output(){
+    console.log('Hello World, this is ' + myName + ' with HNGi7 ID ' + myId + ' using ' + myLanguage + ' for stage 2 task.' +myEmail);
+}
+
+
 
 let info=new Info(myName, myId, myEmail, myLanguage);
 output();

fb31af4bc1818e0c1c7d0b3621e031a9bc8a4f0d
munyolec <33727407+munyolec@users.noreply.github.com>
2020-06-03 15:09:25 +0300

    Update ClaireMunyole_00045.js
    declared email in the script

--- scripts/ClaireMunyole_00045.js
+++ scripts/ClaireMunyole_00045.js
@@ -1,11 +1,11 @@
 
-
-function task_two_helloworld(name, identification, scripting_language) {
+//this is task 2 for HNG
+function task_two_hng_helloworld(name, identification, scripting_language, email) {
   console.log(
     `Hello World, this is ${name} with HNGi7 ID ${identification} using ${scripting_language} for stage 2 task`
   );
 }
-task_two_helloworld("Claire Munyole", "HNG-00045", "Javascript");
+task_two_hng_helloworld("Claire Munyole", "HNG-00045", "Javascript", "munyolec@gmail.com");
 
 
 

ef0903b7fe43bf96f90af651a8d789cb68986b92
Victor Famade <40117665+Famzy96@users.noreply.github.com>
2020-06-03 13:12:55 +0100

    Update Famzy.py

--- scripts/Famzy.py
+++ scripts/Famzy.py
@@ -1,5 +1,5 @@
 def task(name, ID, language):
     print("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task.".format(name, ID, language))
 
-
+email = 'famadevictor@gmail.com'
 task("Victor Famade", "HNG-01863", "Python")

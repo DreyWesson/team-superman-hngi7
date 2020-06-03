d7acebd8547307924352e6d1de56e76af218dabd
abdulxs <gaftofi@gmail.com>
2020-06-03 12:23:56 +0100

    Update Abdulgafar.js

--- scripts/Abdulgafar.js
+++ scripts/Abdulgafar.js
@@ -3,4 +3,4 @@
 console.log(`Hello world, this is Yusuf Abdulgafar with HNGi7 ID of HNG-01368 using JavaScript for stage 2 task`);
 
 
-const email = 'gaftofi@gmail.com'
+const email = 'gaftofi@gmail.com';

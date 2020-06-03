6529e5be47cc7a6c878d0ca9ee1446bddceb34e8
atiloladave <atiloladavid1@gmail.com>
2020-06-03 09:53:40 -1200

    updated JavaScript

--- scripts/atilola.js
+++ scripts/atilola.js
@@ -1,10 +1,10 @@
 function davidAtilola(){
 
     const fullName = 'David Atilola';
-    const hngID = 'HNG-01583'
+    const id = 'HNG-01583'
     const language = 'JavaScript';
     const email = 'atiloladavid1@gmail.com'
 
-    console.log (`Hello World, this is ${fullName} with the HNGi7 ID ${hngID} using ${language} for stage 2 task.${email}`)
+    console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`)
 }
 davidAtilola()

fb802b01cb1ea8214006d83e536d73309ac5bfe8
CodeitSoft-Tech <45128276+CodeitSoft-Tech@users.noreply.github.com>
2020-06-03 06:23:22 -0700

    Update FrancisTawiahHNG-01722

--- scripts/FrancisTawiahHNG-01722.php
+++ scripts/FrancisTawiahHNG-01722.php
@@ -7,7 +7,7 @@
 
 	if($username == 'FRANCIS TAWIAH' && $intern_id == 'HNG-01722' && $language == 'PHP')
 	{
-		$message = "Hello World, this is ".$username." with HNi7 ID,  ".$intern_id." using ".$language;
+		$message = "Hello World, this is ".$username." with HNi7 ID ".$intern_id." using ".$language." for stage 2 task";
 		$read_message = json_encode($message);
 
 		echo $read_message;

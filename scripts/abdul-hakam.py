63780f67df4449ff4676190e5f510762084a3ddd
FameC <35351094+FameC@users.noreply.github.com>
2020-06-03 17:54:30 +0100

    added full-stop

--- scripts/abdul-hakam.py
+++ scripts/abdul-hakam.py
@@ -5,4 +5,4 @@ hng_id ='HNG-02429'
 lang = 'Python' 
 email_adr = 'princefame1@gmail.com'
 
-print(f"Hello World, this is {full_name} with HNGi7 ID {hng_id} using {lang} for stage 2 task")
+print(f"Hello World, this is {full_name} with HNGi7 ID {hng_id} using {lang} for stage 2 task.")

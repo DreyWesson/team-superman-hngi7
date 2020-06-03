02abb1864cccbb93cd2e2a7aa1cf18140fbdfa20
Foresight #X <sundayamuke7@gmail.com>
2020-06-03 13:40:00 +0100

    Fixed the echo message

--- scripts/Foresight.php
+++ scripts/Foresight.php
@@ -1,7 +1,7 @@
 <?php
 $full_name = "Sunday Amuke";
-$id = "02082";
+$id = "HNG-02082";
 $lang = "PHP";
 
-echo "Hello World, this is ".$full_name." with HNGi7 ID-".$id." using ".$lang." for stage 2 task";
+echo "Hello World, this is ".$full_name." with HNGi7 ID ".$id." using ".$lang." for stage 2 task";
 ?>

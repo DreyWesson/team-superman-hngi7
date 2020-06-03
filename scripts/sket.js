6d3bf714d0292f0d3bba88cff6691fc9269f7d70
Sketodee <66252348+Sketodee@users.noreply.github.com>
2020-06-03 14:07:08 +0100

    Add files via upload
    this is the updated version of my script

--- scripts/sket.js
+++ scripts/sket.js
@@ -1,7 +1,7 @@
 function firstTask(FullName, HNGId, Language, Email)
  {
     console.log(
-      `Hello World, This is ${FullName} with HNGi7 ID ${HNGId} using ${Language} for stage 2 task ${Email}`
+      `Hello World, This is ${FullName} with HNGi7 ID ${HNGId} using ${Language} for stage 2 task. ${Email}`
     );
   }
   firstTask("Shobande James", "HNG-01145", "Javascript", "shobandejames@gmail.com");

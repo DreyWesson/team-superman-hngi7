0f995793daa91ce05f1cbcd698e3c4ffa965da0b
uchedotphp <uchedotphp@gmail.com>
2020-06-03 06:38:19 +0100

    made changes to the type of language used. Had to pascal format it from javascript to JavaScript in order to fit perfectly into created RegExp

--- scripts/uchedotphp.js
+++ scripts/uchedotphp.js
@@ -1,6 +1,6 @@
 (function () {
   "use strict";
   const statement =
-    "Hello World, this is Nwulu Uchechukwu Prince with HNGi7 ID HNG-03931 using javascript for stage 2 task";
+    "Hello World, this is Nwulu Uchechukwu Prince with HNGi7 ID HNG-03931 using JavaScript for stage 2 task";
   console.log(statement);
 })();

355b3c31799ec75b2b5aaaf57c3b4c960536f690
Christian Ndu <christiannduh@gmail.comgit config --global user.email christiannduh@gmail.com>
2020-06-03 02:07:38 +0100

    more changes

--- scripts/delightfulcodes.js
+++ scripts/delightfulcodes.js
@@ -3,7 +3,7 @@ const hngId = "01290";
 const preferredLang = "Javascript";
 
 function namecheck(){
-    console.log(`Hello World, this is ${internName} with HNGi7 ID HNG-${0}${parseInt(hngId,10)} using ${preferredLang} for stage 2 task`)
+    console.log(`Hello World, this is ${internName} with HNGi7 ID HNG-${0}${Number(hngId)} using ${preferredLang} for stage 2 task`)
 }
 //Number
 namecheck()

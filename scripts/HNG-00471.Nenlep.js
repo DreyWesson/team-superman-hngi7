1db7e2179932224f4cd28adab161066da5c2b9c0
nenlep <nenlep12345@gmail.com>
2020-06-03 20:07:34 +0100

    Update HNG-00471.Nenlep.js

--- scripts/HNG-00471.Nenlep.js
+++ scripts/HNG-00471.Nenlep.js
@@ -2,7 +2,7 @@ function task(){
   var fname = "Nenlep Ishaku";
   var hng_ID = "HNG-00471";
   var email = "nenlep12345@gmail.com";
-  var text = `Hello World, this is ${fname} with HNGi7 ID ${hng_ID} using JavaScript for stage 2 task.${email}`;
-  console.log(text);
+  var lang = "JavaScript";
+  console.log(`Hello World, this is ${fname} with HNGi7 ID ${hng_ID} using ${lang} for stage 2 task.${email}`);
 }
 task();

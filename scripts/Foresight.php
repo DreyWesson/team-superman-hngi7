c8b34eea37dc68d4c0b268a6a4a3c6fa6cbb537d
Foresight #X <sundayamuke7@gmail.com>
2020-06-03 15:33:40 +0100

    Added email.

--- scripts/Foresight.php
+++ scripts/Foresight.php
@@ -2,6 +2,7 @@
 $full_name = "Sunday Amuke";
 $id = "HNG-02082";
 $lang = "PHP";
+$email = "sundayamuke7@gmail.com";
 
-echo "Hello World, this is ".$full_name." with HNGi7 ID ".$id." using ".$lang." for stage 2 task";
+echo "Hello World, this is $full_name with HNGi7 ID $id using $lang for stage 2 task.$email";
 ?>

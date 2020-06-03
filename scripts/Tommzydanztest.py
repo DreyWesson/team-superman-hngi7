94605ab5e131d97fc0c376c5b50221f1822f2bee
Daniel Tomiwa <daniela.stomiwa@yahoo.com>
2020-06-03 15:41:57 +0100

    Updated script for task 2

--- scripts/Tommzydanztest.py
+++ scripts/Tommzydanztest.py
@@ -8,8 +8,7 @@ email = "daniela.stomiwa@yahoo.com"
 language = "Python" 
 
 
-w = "Hello World, this is %s with HNGi7 ID %s"%	(name, id)
-e = " using %s for stage 2 task"% (language)
+w = "Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task.%s"% (name, id, language, email)
 
-print (w + e)
+print (w)
 sys.stdout.flush()

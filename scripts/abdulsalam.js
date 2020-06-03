fef07383accfaf75d4f7a8ba6445541d40792fd3
abdulsalamcodes <59220264+abdulsalamcodes@users.noreply.github.com>
2020-06-03 21:37:40 +0100

    Update abdulsalam.js

--- scripts/abdulsalam.js
+++ scripts/abdulsalam.js
@@ -4,7 +4,7 @@ let intern = {
     language : 'Javscript',
     email : 'abdulsalamabodunrin369@gmail.com',
     sayHello() {
-        return `Hello World, this ${this.devName} with HNGi7 ID ${this.id} using ${this.language} for stage 2 task.${this.email}`;
+        return `Hello World, this is ${this.devName} with HNGi7 ID ${this.id} using ${this.language} for stage 2 task.${this.email}`;
     }
 }
 console.log(intern.sayHello())

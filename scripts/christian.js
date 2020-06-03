ed1a29eebbdde2c52b550a67a57e0521af04aff6
chrisunday2013 <potentialsunny47@gmail.com>
2020-06-03 22:06:36 +0100

    javascript

---
+++ scripts/christian.js
@@ -0,0 +1,7 @@
+const result = function(name, id, email,language){
+  return `Hello World, this is ${name} with HNGi7 ID ${id}
+  using ${language} for stage 2 task. ${email}`
+
+}
+
+  console.log(result(" Christian Sunday", "HNG-03696", "Javascript", "chris4christ25@yahoo.com"))

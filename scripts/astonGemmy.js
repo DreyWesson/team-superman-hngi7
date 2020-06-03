089f054b7d72b89b7409b0c8350a9dd71f9e7630
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2020-06-03 19:41:33 +0100

    Update astonGemmy.js

--- scripts/astonGemmy.js
+++ scripts/astonGemmy.js
@@ -3,3 +3,13 @@ function logHNG(dataName, dataID, dataLanguage, dataEmail){
 }
 
 logHNG(dataName, dataID, dataLanguage, dataEmail);
+function logHNG(dataName, dataID, dataLanguage, dataEmail){
+    console.log("Hello World, this is " + dataName + " with HNGi7 ID " + dataID + " using " + dataLanguage + " for stage 2 task " + dataEmail);
+}
+
+let dataName = 'Akpan, Uwakmfon Sunday';
+let dataID = 'HNG-06122';
+let dataLanguage = 'Javascript';
+let dataEmail = 'astongemmy@gmail.com';
+
+logHNG(dataName, dataID, dataLanguage, dataEmail);

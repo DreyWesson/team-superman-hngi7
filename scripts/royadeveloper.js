e005859e1d9be1d457f6ce30cbb611ffda55c7fa
Pablo <royadeveloper01@gmail.com>
2020-06-03 18:26:35 +0000

    change

--- scripts/royadeveloper.js
+++ scripts/royadeveloper.js
@@ -1,7 +1,8 @@
 const name = "Prince Chimaobi";
 const id = "HNG-00996";
-const email = "royadeveloper01@gmail.com";
 const language = "JavaScript";
+const email = "royadeveloper01@gmail.com";
+
 
 const myInfo = (name, id,language,email) => {
     console.log(`Hello World, This is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`);

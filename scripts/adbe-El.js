29c42808152e24e18ac5a7865d2f8fb3cdec9388
Adbeel Omiunu <45834662+Adbe-El@users.noreply.github.com>
2020-06-03 02:41:09 +0100

    adbe-El.js

---
+++ scripts/adbe-El.js
@@ -0,0 +1,5 @@
+const name = "Adbeel Omiunu";
+const HNGi7_ID: "HNG-02583";
+const language= "JavaScript";
+
+console.log("Hello World, this is " + name + " with HNGi7 ID " + HNGi7_ID + " using " + language + " for stage 2 task.");

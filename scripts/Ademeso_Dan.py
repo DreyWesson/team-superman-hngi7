ebf532d83098f08995bda1bc53dd5ce2a8159e61
Daniel Ademeso <31941870+danade002@users.noreply.github.com>
2020-06-02 19:20:26 -0700

    Second commit

--- scripts/Ademeso_Dan.py
+++ scripts/Ademeso_Dan.py
@@ -1,11 +1,11 @@
 
 # Script created by Daniel ADEMESO for HNGi7 Intern 
 
-def My_input(name, HNGID, Language_Used):
-    return ("Hello World, this is " + name +" with HNGi7 ID " +HNGID+ " using "+ Language_Used + " for stage 2 task")
+def input(name, HNGID, Language):
+    return ("Hello World, this is " + name +" with HNGi7 ID " +HNGID+ " using "+ Language + " for stage 2 task")
 
 
-print(My_input("Daniel Ademeso", "HNG-03169", "python"))
+print(input("Daniel Ademeso", "HNG-03169", "python"))
 
 
 

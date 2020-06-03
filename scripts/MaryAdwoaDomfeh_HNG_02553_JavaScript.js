fee6a89437f637923bbf5dc02009a4f64ae16d4f
Domfeh <41950595+Domfeh@users.noreply.github.com>
2020-06-03 04:11:29 +0000

    Task 2

---
+++ scripts/MaryAdwoaDomfeh_HNG_02553_JavaScript.js
@@ -0,0 +1,12 @@
+
+function taskTwo(){
+const name = 'Mary Adwoa Domfeh';
+const id = 'HNG-02553';
+const language = 'JavaScript';
+
+console.log(`Hello World, this is ${name} with\nHNGi7 ID ${id} using ${language} for stage 2 task`);
+
+
+}
+
+taskTwo();

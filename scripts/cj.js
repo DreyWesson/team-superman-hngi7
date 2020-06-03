74003ad02387d6e764c446378b0d16503d925fa6
newton <chiakajunior@yahoo.com>
2020-06-03 01:47:01 +0100

    added cj file

---
+++ scripts/cj.js
@@ -0,0 +1,9 @@
+const myInfo = (name, ID, Language) => {
+
+ console.log (
+    `Hello World, this is ${name} with HNGi7 ID ${ID} using ${Language} for stage 2 task`
+  );
+
+}
+
+myInfo("Osuji Chiaka Daniel", "HNG-03090", "Javascript");

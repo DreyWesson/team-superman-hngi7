7ec66d18526a200cc3d80a1caf8c7f36c52be5b7
FameC <35351094+FameC@users.noreply.github.com>
2020-06-03 16:25:08 +0100

    abdul-hakam.py
    Created using python

---
+++ scripts/abdul-hakam.py
@@ -0,0 +1,8 @@
+#A script to display my personal details for HNG internship
+
+full_name = 'Abdul-hakam Abuda Oshiogwe'
+hng_id ='HNG-02429'
+lang = 'Python' 
+email_adr = 'princefame1@gmail.com'
+
+print(f"Hello World, this is {full_name} with HNGi7 ID {hng_id} using {lang} for stage 2 task")

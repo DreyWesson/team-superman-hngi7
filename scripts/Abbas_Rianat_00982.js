8afb6298a5e0f8c147a78e996327b6921820b3de
rihanat <rihanatoluwatosin@gmail.com>
2020-06-03 18:54:15 +0100

    my first task

--- scripts/Abbas_Rianat_00982.js
+++ scripts/Abbas_Rianat_00982.js
@@ -1,6 +1,6 @@
 function riri(fullname="Abbas Rianat", id ="00982", email ="rihanatoluwatosin@gmail.com", language ="Javascript"){
     console.log(
-     `hello world, this is ${fullname} with HNGi7 ${id} and email ${email} using ${language} for stage 2 task`
+     `hello world, this is ${fullname} with HNGi7 ${id} using ${language} for stage 2 task.${email}`
     );
 }
 riri();

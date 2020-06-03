6e9ce0954b157101854560961a749c4836a5b49d
Abdulbaasit-Seriki <baasisek01@gmail.com>
2020-06-03 10:51:07 +0100

    Created abdulbaasit script

---
+++ scripts/abdulbaasit.js
@@ -0,0 +1,5 @@
+const hngDetails = (name, id, lang) => {
+    console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task`);
+}
+ 
+hngDetails("Abdulbaasit Seriki", "HNG-05764" , "JavaScript");

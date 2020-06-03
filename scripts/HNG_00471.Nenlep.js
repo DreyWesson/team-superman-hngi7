7e0739f83edac4d28c802c53faf203b5144f56a1
Nenlep Ishaku <47663123+nenlep@users.noreply.github.com>
2020-06-03 15:00:32 +0100

    Update HNG_00471.Nenlep.js

--- scripts/HNG_00471.Nenlep.js
+++ scripts/HNG_00471.Nenlep.js
@@ -1,5 +1,5 @@
 function hello(){
-  var text = "Hello World, this is Nenlep Ishaku with HNGi7 ID HNG-00471 using JavaScript for stage 2 task.";
+  var text = "Hello World, this is Nenlep Ishaku with HNGi7 ID HNG-00471 using JavaScript for stage 2 task.nenlep12345@gmail.com";
   console.log(text);
 }
 hello();

bad624d0cea9d6766f98554c660e13611f3a965d
Modupe <igeoluwamodupe1@gmail.com>
2020-06-03 17:37:14 +0100

    HNG Task Two

--- scripts/modupe.js
+++ scripts/modupe.js
@@ -1,5 +1,6 @@
-function taskone(name,hngid,language,email){
-    console.log("Hello world, this is " + " " + name + " " + "HNGi7 ID " + " " + hngid + " " + "using " + " " + language + " " + " for stage 2 task." + email)
+// HNG Task two
+function tasktwo(name,hngid,language,mail){
+    console.log("Hello world, this is " + " " + name + " " + "HNGi7 ID " + " " + hngid + " " + "using " + " " + language + " " + " for stage 2 task."+ mail)
 
 }
-taskone("Ige Modupe ", "HNG-0410", "JavaScript" , "igeoluwamodupe1@gmail.com");
+tasktwo("Ige Modupe", "HNG-0410", "JavaScript" , "igeoluwamodupe1@gmail.com");

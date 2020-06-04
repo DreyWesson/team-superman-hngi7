4558d347090309fcafc6bf08b2bc6348e390861a
Ajayi Michael <ajayimichael150@gmail.com>
2020-06-04 10:10:43 +0100

    Update Functake.js

--- scripts/Functake.js
+++ scripts/Functake.js
@@ -1,12 +1,8 @@
-function data(){
-    let fullName = 'Ajayi Michael';
-    let id = 'HNG-00384';
-    let language = 'JavaScript';
-    let email = 'ajayimichael150@gmail.com';
+ let fullName = 'Ajayi Michael';
+ let id = 'HNG-00384';
+ let language = 'JavaScript';
+ let email = 'ajayimichael150@gmail.com';
     
-    const profile = 'Hello World, this is ' + fullName + ' with HNGi7 ID' + id + ' using ' + language + ' for stage 2 task' + email;
+ let profile = 'Hello World, this is $(fullName) with HNGi7 ID $(id) using $(language) for stage 2 task $(email);
     
-    console.log(profile);
-}
-
-data();
+ console.log(profile);

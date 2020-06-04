3b6543edaf2f372ab9c489c221634fdaddfce69a
luqman5555 <66163385+luqman5555@users.noreply.github.com>
2020-06-04 23:54:51 +0100

    Add files via upload

--- lawalluqman copy.py
+++ lawalluqman copy.py
@@ -1,9 +1,8 @@
-def  introduction(fullname ,my_iD,language):
-    print("hello world this is {} with HNGi7  ID {} using  {} for stage 2 task ." .format(name,ID, language))
+# A function to accept 4 string arguments and return an expected string 
+# containing the HNGi7 Intern profile
+
+def say_hello(full_name, id:int, language, email):
+    return (f"Hello World, this is {full_name} with HNGi7 ID {id} and email{adress} using  {language},format for stage 2 task")
 
 
-introduction("lawal luqman", "HNG-2322", "Python")
-
-def  introduction(fullname ,my_iD,language):
-   print("hello world this is {} with HNGi7  ID {} using  {} for stage 2 task .{}" .format(name,ID, language,email))
-    introduction("lawal luqman", "HNG-2322", "Python","luqmanlajani@gmail.com")
+print(say_hello("Lawal luqman", "HNG-2322", "python"  "luqmanlajani@gmail.com")

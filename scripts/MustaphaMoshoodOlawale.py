2eb4fc724fb16f07312c2336fa131a12c43d9f5c
Mustapha Moshood <olawale3415@gmail.com>
2020-06-04 06:50:36 +0100

    submission

--- scripts/MustaphaMoshoodOlawale.py
+++ scripts/MustaphaMoshoodOlawale.py
@@ -2,3 +2,7 @@ def task_2 (name, id, language):
 	print(f"Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task")
 
 task_2("Mustapha Moshood Olawale", "HNG-05611", "Python" )
+
+
+
+

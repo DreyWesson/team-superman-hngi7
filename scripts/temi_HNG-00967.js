66d157b62b1ceac199f17ad36f93eed2ddc7af95
Temiloluwa-Onaleye <65330353+Temiloluwa-Onaleye@users.noreply.github.com>
2020-06-04 14:26:00 +0100

    Correction

--- scripts/temi_HNG-00967.js
+++ scripts/temi_HNG-00967.js
@@ -1,3 +1,3 @@
-const shs = "Hello World, this is ONALEYE Temiloluwa with HNGi7 ID HNG-00967 using Javscript for stage 2 task.onaleyetemmy@outlook.com";
+const shs = "Hello World, this is ONALEYE Temiloluwa with HNGi7 ID HNG-00967 using Javascript for stage 2 task.onaleyetemmy@outlook.com";
 
 console.log(shs);

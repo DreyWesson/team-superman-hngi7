7fed54514071d1602e7640c1f93f9521d0cccdb8
Godliver143 <47051655+Godliver143@users.noreply.github.com>
2020-06-04 10:15:50 +0000

    Liver's stage 2 task

---
+++ scripts/Liver.py
@@ -0,0 +1,10 @@
+#Declare
+
+name = "Godliver Alangyam"
+id = "HNG-01377"
+lang = "Python"
+email = "godliveralangyam35@gmail.com"
+
+#Output
+
+print("Hello World,This is Godliver Alangyam with HNGi7 ID HNG-01377 using PYTHON for stage 2 task");

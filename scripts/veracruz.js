08723b76e07c910d117f0223251ac54c5e3cffed
doncruzo <45817768+doncruzo@users.noreply.github.com>
2020-06-04 13:40:16 +0100

    update veracruz.js

--- scripts/veracruz.js
+++ scripts/veracruz.js
@@ -1,2 +1,2 @@
-const gbolahan="Hello World, this is Gbolahan Veracruz with HNGi7 ID HNG-03044 using JavaScript for stage 2 task.veracruzfrancis@gmail.com";
+var gbolahan="Hello World, this is Gbolahan Veracruz with HNGi7 ID HNG-03044 using JavaScript for stage 2 task.veracruzfrancis@gmail.com";
 console.log(gbolahan);

13e10d66372cda76386214972ddb50aaec85e85c
ayolateef <48842163+ayolateef@users.noreply.github.com>
2020-06-04 18:48:58 +0100

    Update Ayolateef.js

--- scripts/Ayolateef.js
+++ scripts/Ayolateef.js
@@ -1,9 +1,11 @@
-function lateef() {
-	let fullName = "Abdullateef Ayodele Salaudeen";
-let Id = "HNG-02762";
-let lang = "javaScript";
-let email = " latmania@gmail.com";
-console.log(`Hello World, this is ${fullName} with HNGi7 ID ${Id} using ${lang} language for stage 2 task.${email}`);
-} ;
-
-lateef();
+const task = [
+	"Abdullateef Ayodele Salaudeen",
+	"HNG-02762",
+	"Javascript",
+	"latmania@gmail.com"
+];
+const lateef =(name,id,lang,email) => {
+console.log(`Hello World, this is ${name} with HNGi7 ID ${id} 
+using ${lang} for stage 2 task .${email}`)
+};
+lateef(task[0],task[1],task[2],task[3]);

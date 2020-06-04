4d5735cf7cf87d0f5e133df1174aaad319047989
Elljay <32459954+hayomide@users.noreply.github.com>
2020-06-04 23:28:11 +0100

    Bami Task 2

---
+++ scripts/Bami_task2.py
@@ -0,0 +1,14 @@
+HNGi7_text = 'Hello World!'
+HNGi7_ID = "HG-00563"
+language = 'Python'
+Full_name = {
+    "first_name": "Elijah",
+    "middle_name": "Ayobami",
+    "last_name": "Bello",
+}
+
+x = Full_name["first_name"]
+y = Full_name["middle_name"]
+z = Full_name["last_name"]    
+
+print(HNGi7_text +'\t This is \t' + x + '\t' + y + '\t' + z + '\t with HNGI7 ID ->>\t' + HNGi7_ID + '\t using->>\t' + language + '\t for stage2 task')

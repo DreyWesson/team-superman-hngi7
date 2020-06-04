b168ad03e8f27e4c5b2be78110d475a088a88f22
Ajayi Michael <ajayimichael150@gmail.com>
2020-06-04 10:22:34 +0100

    Update Functake.js

--- scripts/Functake.js
+++ scripts/Functake.js
@@ -3,7 +3,7 @@
   id : 'HNG-00384',
   language : 'JavaScript',
   email : 'ajayimichael150@gmail.com'
- ]   
+ ];
 
  let profile = 'Hello World, this is $(info.fullName) with HNGi7 ID $(info.id) using $(info.language) for stage 2 task $(info.email);
     

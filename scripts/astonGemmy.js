7cd0d18e3913a7cd066f7396f32a726df75bb457
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2020-06-04 14:13:18 +0100

    Update astonGemmy.js

--- scripts/astonGemmy.js
+++ scripts/astonGemmy.js
@@ -1,4 +1,4 @@
-function logHNG(name, id, language, email){
+function logHNG(name, id, language,email){
     console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`);
 }
 
@@ -9,4 +9,4 @@ data = {
     "email":"astongemmy@gmail.com",
 };
 
-logHNG(data.name, data.id, data.language, data.email);
+logHNG(data.name, data.id, data.language,data.email);

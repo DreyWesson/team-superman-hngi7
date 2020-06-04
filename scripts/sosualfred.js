848f1d059086e8b3d8b0251884b71d96c6936829
sosualfred <sosualfred@gmail.com>
2020-06-04 21:00:15 +0000

    Changed my failed script.

--- scripts/sosualfred.js
+++
@@ -1,6 +0,0 @@
-let name = "Sosu Alfred";
-let hngId = "HNG-02745";
-let language = "JavaScript";
-let email = "sosualfred@gmail.com";
-
-console.log("Hello World, this is "+ name +" with HNGi7 ID "+ hngId +" using "+ language +" for stage 2 task."+ email +"");

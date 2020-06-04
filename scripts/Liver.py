be521e5527d7fb6c73fd1264879a9f8783c0e0c6
Godliver143 <godliveralangyam35@gmail.com>
2020-06-04 11:54:44 +0000

    Update Liver.py
    Changes made

--- scripts/Liver.py
+++ scripts/Liver.py
@@ -3,8 +3,8 @@
 name = "Godliver Alangyam"
 id = "HNG-01377"
 lang = "Python"
-email = "godliveralangyam35@gmail.com"
+
 
 #Output
 
-print("Hello World,This is Godliver Alangyam with HNGi7 ID HNG-01377 using PYTHON for stage 2 task");
+print("Hello World,This is Godliver Alangyam with HNGi7 ID HNG-01377 using PYTHON for stage 2 task")

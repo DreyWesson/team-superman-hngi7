8f1d472713cd69bdc4897197708b7e79960ff7e9
feli <chesangfelister@gmil.com>
2020-06-04 10:12:32 +0300

    my script

---
+++ scripts/ChesangFelister.js
@@ -0,0 +1,11 @@
+let MyName = "Chesang Felister";
+let IdNo = "INTERNSHIP ID: HNG-01293";
+let Email = "chesangfelister@gmail.com";
+let Language = "JavaScript";
+
+function displayMessage() {
+let Message = "Hello World, this is " + "  "+ MyName +" "+ id +"  "+ "using " + language + " "+"for stage 2 task. " + "My  email"+" "+  email;
+console.log(Message);
+}
+
+displayMessage();

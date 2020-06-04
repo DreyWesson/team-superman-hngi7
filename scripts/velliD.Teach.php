77b4153fa78b61f452902fa763ee8fe219f447f7
Anthony Onyejesi <anthonyonyejesi@Anthonys-MacBook-Pro.local>
2020-06-04 22:46:57 +0100

    First and last

---
+++ scripts/velliD.Teach.php
@@ -0,0 +1,7 @@
+<?php
+    $myname = "Emeka Onyejesi";
+    $myid = "HNG-01971";
+    $mylang = "Javascript";
+    $mymail = "emekaveli@gmail.com";
+
+    echo "Hello World, this is $myname with HNGi7 ID $myid using $mylang for stage 2 task.$mymail";

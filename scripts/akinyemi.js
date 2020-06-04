c33540fa94006cb268ea07a49caa80d61432f3bc
YemiYemz <akinyemiodebunmi@gmail.com>
2020-06-04 16:09:24 +0100

    YemiYemz-HNG-03885 script

---
+++ scripts/akinyemi.js
@@ -0,0 +1,6 @@
+let fullname = "Akinyemi Odebunmi";
+let hngi7Id = "HNG-03885";
+let language = "Javascript";
+let myEmail = "akinyemiodebunmi@gmail.com";
+
+    console.log(`Hello World, this is ${fullname} with HNGi7 ID ${hngi7Id} using ${language} for stage 2 task.${myEmail}`);

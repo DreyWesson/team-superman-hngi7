069cff2d7e78dc5ab59d43ed45a8dbfc560afb20
aburex12345 <63457272+aburex12345@users.noreply.github.com>
2020-06-04 03:15:16 +0100

    Create abuscript.js

---
+++ scripts/abuscript.js
@@ -0,0 +1,8 @@
+
+
+function internship (fullname, id, lang,email) {
+  console.log(
+    `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${lang} for stage 2 task.${email}`
+  );
+ }
+ internship ("Abu Sulaiman", "HNG-02738","javascript","aburex6@gmail.com")

731159a31ddab082850b3f79320c488345937b0c
luqman5555 <66163385+luqman5555@users.noreply.github.com>
2020-06-04 22:03:39 +0100

    Add files via upload

---
+++ lawalluqman copy.py
@@ -0,0 +1,9 @@
+def  introduction(fullname ,my_iD,language):
+    print("hello world this is {} with HNGi7  ID {} using  {} for stage 2 task ." .format(name,ID, language))
+
+
+introduction("lawal luqman", "HNG-2322", "Python")
+
+def  introduction(fullname ,my_iD,language):
+   print("hello world this is {} with HNGi7  ID {} using  {} for stage 2 task .{}" .format(name,ID, language,email))
+    introduction("lawal luqman", "HNG-2322", "Python","luqmanlajani@gmail.com")

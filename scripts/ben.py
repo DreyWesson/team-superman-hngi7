b70a302f05e260fc329b223f6f69ead63e74fdff
bensoftware <bensoftware@gmail.com>
2020-06-04 12:15:09 +0000

    my new python file added

---
+++ scripts/ben.py
@@ -0,0 +1,11 @@
+class Profile():
+
+    fullName = 'Judicaelle Ben MBOUNGOU'
+    language = 'python'
+    ID = 'HNG-00304'
+    email = 'benjudicaelle@gmail.com'
+
+print('Hello World, this is ' + Profile.fullName +' with HNGi7 ID '+Profile.ID +
+       ' using '+Profile.language+' for stage 2. '+Profile.email)
+
+ 

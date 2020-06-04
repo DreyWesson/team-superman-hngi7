ec2daa76a097ed80886b55346d36e0d0bba045c7
kahuria00 <charitykahuria@gmail.com>
2020-06-04 13:38:32 +0300

    last trial

--- scripts/kahuria00.py
+++ scripts/kahuria00.py
@@ -1,4 +1,10 @@
-print("Hello World, this is {name}  with HNGi7 {id} using {ln} for stage 2 task".format(name='Charity Wanjiru',email='charitykahuria@gmail.com',id='ID 00621',ln='Python'))
+name='Charity Wanjiru'
+email='charitykahuria@gmail.com'
+ID='00621'
+language='Python'
+
+
+print("Hello World, this is {name}  with HNGi7 ID {ID} using {ln} for stage 2 task.{email}".format(name,ID,language,email))
 
 
 

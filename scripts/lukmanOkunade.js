37f4223fdcfcd790f7070dd2f0be47cb7f7bdae7
LukmanOkunade <okunadelukmon@gmail.com>
2020-06-04 09:11:41 +0100

    task rework

--- scripts/lukmanOkunade.js
+++ scripts/lukmanOkunade.js
@@ -1,5 +1,5 @@
-var myIntroduction = function(fullName, id, language, Email) {
-    return (`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage two task.${Email}`)
+var myIntroduction = function(fullName, id, language, email) {
+    return (`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage two task.${email}`)
 }
 
 var newMyIntroduction = intro("Lukman Okunade", "HNG-00960", "JavaScript", "okunadelukmon@gmail.com");

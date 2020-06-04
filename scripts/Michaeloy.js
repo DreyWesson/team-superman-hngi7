2a162198b261899dac3197e84d65dc54fc922bfd
oluwagiga <55166096+oluwagiga@users.noreply.github.com>
2020-06-04 14:28:39 +0100

    Oluwgiga
    This is my commit on task 2

--- scripts/Michaeloy.js
+++ scripts/Michaeloy.js
@@ -1,4 +1,5 @@
-const myProfile = (name, id, lang, email) => 
-`"Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task.${email} "`;
+const myInfo = (fullname, id, lang, email) => {
+    console.log(`Hello World, this is ${fullname} with HNGi7 ID ${id} using ${lang} for stage 2 task.${email}`);
+}
 
-console.log(myProfile("Michael John", "HNG-00424", "JavaScript", "johnmikeloy@gmail.com"));
+myInfo("Michael John", "HNG-00424", "JavaScript", "johnmikeloy@gmail.com");
---
+++ scripts/Oluwayemi.js
@@ -0,0 +1,9 @@
+let script = {
+    text: "Hello World",
+    fullname: "Oluwayemi",
+    ID: 'HNG-0' + 4156,
+    language: "JavaScript",
+    email: "oluwayemiiyiola@gmail.com"
+};
+
+console.log(script.text + ", this is " + script.fullname + " with HNGi7 ID " + script.ID + " using " + script.language + " for stage 2 task. " + script.email);

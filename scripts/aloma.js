5493adaf46ef28c1c015e58ce8339cf792e939db
drizlad <drizlad@gmail.com>
2020-06-04 16:08:43 +1300

    aloma-HNG-03260 script

--- scripts/aloma.js
+++ scripts/aloma.js
@@ -2,7 +2,8 @@
 
 function introduction(myName, myHngId, myProgrammingLanguage, myEmail) {
 
-    console.log(`Hello World, my name is ${myName} with HNGi7 ID ${myHngId} using ${myProgrammingLanguage} for the stage 2 task .${myEmail}`);
+    console.log(`Hello World, this is ${myName} with HNGi7 ID ${myHngId} using ${myProgrammingLanguage} for the stage 2 task.${myEmail}`);
 }
 
-introduction("Adeyeni idris oladayo", "HNG-03260", "Javascript", "drizlad@gmail.com");
+introduction("Adeyemi idris oladayo", "HNG-03260", "Javascript", "drizlad@gmail.com");
+console.log(introduction)

8ce5ae05d38e5a374f43cdccf7aa7500f1cfe40e
Adbeel Omiunu <45834662+Adbe-El@users.noreply.github.com>
2020-06-04 19:01:15 +0100

    Delete Adbe-El.js

--- scripts/Adbe-El.js
+++
@@ -1,7 +0,0 @@
-const name = "Adbeel Omiunu";
-const Id = "HNG-02582";
-const language = "JavaScript";
-const email = "adbeelomiunu@gmail.com";
-
-
-console.log("Hello World, this is " + name + " with HNGi7 ID " + Id + " using " + language + " for stage 2 task. " + email);

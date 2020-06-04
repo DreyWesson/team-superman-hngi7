d4201c46f0e21a63eaab14be7e35d05fc23eeb70
laitandada <michaeldada09@gmail.com>
2020-06-04 19:41:13 +0100

    commit message

--- scripts/lytan.js
+++ scripts/lytan.js
@@ -1,6 +1,6 @@
 
-function HNGi7(fullname, id, language, email){
+function helloHng(fullname, id, language, email){
 	console.log(
-		`Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`);
+		`Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`);
 }
-HNGi7("Olaitan dada", "HNG-00667", "javascript", "michaeldada09@gmail.com");
+helloHng("Olaitan dada", "HNG-00641", "javascript", "michaeldada09@gmail.com");

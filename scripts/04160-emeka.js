7fa54f14a87faa7045be41938c2e7d783491d44b
emekauja <60346432+emekauja@users.noreply.github.com>
2020-06-04 17:10:22 +0100

    Update 04160-emeka.js

--- scripts/04160-emeka.js
+++ scripts/04160-emeka.js
@@ -1,17 +1,15 @@
 class HNGinterns{
-    constructor (firstName, lastName, id, language, email){
-        this.firstName = firstName
-        this.lastName = lastName
+    constructor (name, id, language, email){
+        this.name = name
         this.id = id
         this.language = language
         this.email = email
     }
     getBio() {
-        return `Hello World, this is ${this.firstName} ${this.lastName} with HNGi7 ID ${this.id} using ${this.language} for stage 2 task. ${this.email}`;
+        return `Hello World, this is ${this.name} with HNGi7 ID ${this.id} using ${this.language} for stage 2 task. ${this.email}`;
     }
 }
 
-const newTntern = new HNGinterns('Emeka', 'Uja', '04160', 'javascript', 'ujadnith@gmail.com' );
+const newTntern = new HNGinterns('Emeka Uja', '04160', 'javascript', 'ujadnith@gmail.com' );
 const emeka = newTntern.getBio();
-console.log(emeka);
-
+console.log(emeka)

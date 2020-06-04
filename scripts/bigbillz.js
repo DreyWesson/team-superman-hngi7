688eb1c7748abcee1397e2f30305b8f2e1cbc254
thebigbillz <adenugbafavour@gmail.com>
2020-06-04 20:05:54 +0100

    Changed String Format

--- scripts/bigbillz.js
+++ scripts/bigbillz.js
@@ -2,10 +2,11 @@ const favour ={
     name: "Adenugba Favour",
     id:'HNG-03382',
     language: 'Javascript',
+    email: 'adenugbafavour@gmail.com'
 }
 
-function personInfo({id, name, language}){
-    console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.`);
+function personInfo({id, name, language, email}){
+    console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`);
 }
 
 personInfo(favour)

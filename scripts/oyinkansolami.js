fc4187abfc1e62ad6131d3f9ed85f2e4bf50b350
Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-06-04 14:53:47 +0100

    Oyinkansolami commit file

---
+++ scripts/oyinkansolami.js
@@ -0,0 +1,12 @@
+/** @format */
+
+const myDetails = [
+	(FULLNAME = 'Oluwatomi Oyinkansolami Atansuyi'),
+	(ID = 'HNG-01218'),
+	(LANGUAGE = 'Javascript'),
+	(EMAIL = 'tomisinatansuyi@gmail.com'),
+];
+
+const feedback = `Hello World, this is ${FULLNAME} with HNGi7 ID ${ID} using ${LANGUAGE} for stage 2 task.${EMAIL}`;
+
+console.log(feedback);

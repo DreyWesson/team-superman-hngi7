dbe75572278a6634613836873166ea3e741e1c85
Godliver143 <godliveralangyam35@gmail.com>
2020-06-04 22:21:22 +0000

    Update Liver.py

--- scripts/Liver.py
+++ scripts/Liver.py
@@ -8,4 +8,4 @@ email=".godliveralangyam35@gmail.com"
 
 #Output
 
-print("Hello World,This is Godliver Alangyam with HNGi7 ID HNG-01377 using PYTHON for stage 2 task" +) godliveralangyangyam35@gmail.com
+print("Hello World,this is Godliver Alangyam with HNGi7 ID HNG-01377 using PYTHON for stage 2 task" +) godliveralangyam35@gmail.com

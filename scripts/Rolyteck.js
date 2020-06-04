bbf17e9c197721aaaa3a24aa681bf67421692757
Blessing Roland <30534732+Rolyteck@users.noreply.github.com>
2020-06-04 17:43:58 +0100

    Stage 2 task
    This my Script for stage 2 task

---
+++ scripts/Rolyteck.js
@@ -0,0 +1,8 @@
+function task(){
+  var name = "Roland Blessing";
+  var Hng_ID = "HNG-02628";
+  var Email = "Rolyconsult@gmail.com";
+  var Lang = "JavaScript";
+  console.log(`Hello World, this is ${name} with HNGi7 ID ${Hng_ID} using ${Lang} for stage 2 task.${Email}`);
+}
+task();

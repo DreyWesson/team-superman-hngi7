a69726aeca94796d3e8c4c363d5da19aadc7e39f
achietech <66231913+achietech@users.noreply.github.com>
2020-06-04 21:26:06 +0100

    Create achietech.js
    this is my task 2

---
+++ achietech.js
@@ -0,0 +1,10 @@
+var user = {
+    firstName: "Emmanuel",
+    middleName: "Mohammed",
+    lastName: "Ishaku",
+    email: "meetemmanuel247@gmail.com",
+    language: "JavaScript",
+    id: "HNG-03207",
+    };
+    var myInfo = `Hello world, this is [${user.firstName}] [${user.middleName}] [${user.lastName}] with HNGi7 ID [${user.id}] using [${user.language}] for Stage 2 task. [${user.email}]`;
+    console.log(myInfo);

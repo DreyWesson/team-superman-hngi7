58b3be84976921d362cacb652178a19bc3934e08
bensoftware <bensoftware@gmail.com>
2020-06-04 17:41:19 +0000

    Update python file format

--- scripts/ben.py
+++ scripts/ben.py
@@ -1,4 +1,1 @@
-
-
-
-print("Hello World, this is Judicaelle Ben MBOUNGOU with HNGi7 ID HNG-00304 using python for stage 2 task. benjudicaelle@gmail.com")
+print("Hello World, this is Judicaelle Ben MBOUNGOU with HNGi7 ID HNG-00304 using python for stage 2 task.")

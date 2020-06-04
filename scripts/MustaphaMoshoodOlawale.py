2b50afef55edc00b8452cca138bd4c006344e5ca
Mustapha Moshood <olawale3415@gmail.com>
2020-06-04 19:34:50 +0100

    corrected a mistake

--- scripts/MustaphaMoshoodOlawale.py
+++ scripts/MustaphaMoshoodOlawale.py
@@ -1,8 +1,2 @@
-def task_2 (name, id, language, email):
-	print("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task. {3}".format(name, id, language, email))
-
-task_2("Mustapha Moshood Olawale", "HNG-05611", "Python", "folarinolawale3415@gmail.com" )
-
-
-
+print("Hello World, this is Mustapha Moshood Olawale with HNGi7 ID HNG-05611 using Python for stage 2 task.folarinolawale3415@gmail.com")
 

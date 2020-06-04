e53fa9af919d97ea656b3b6c3e9cfcf673bf77e7
alialaba <60770911+alialaba@users.noreply.github.com>
2020-06-04 08:57:51 -0700

    Update ali.js

--- scripts/ali.js
+++ scripts/ali.js
@@ -1,4 +1,4 @@
-function introduce(name="Ali Abdul Ganiy", hngi7Id="HNG-01101",lang="javascript", email="alialaba079@gmail.com"){
-return `Hello World, this is ${name}, with HNGi7 ID ${hngi7Id} using ${lang} for stage 2 task.${email} `
+function introduce(name="Ali Abdul Ganiy", ID="HNG-01101",lang="javascript", email="alialaba079@gmail.com"){
+return `Hello World, this is ${name} with HNGi7 ID ${ID} using ${lang} for stage 2 task.${email} `
 }
 console.log(introduce())

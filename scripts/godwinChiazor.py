e90a6a9e31095b64c185dbe771049afdd63c9d6c
pathogneumonic <godwinchiazor@gmail.com>
2020-06-04 17:11:17 +0100

    godwinChiazor.py
    fixed email

---
+++ scripts/godwinChiazor.py
@@ -0,0 +1,6 @@
+full_name = 'godwin chiazor'
+hng_id = 'HNG-00373'
+email_adr = 'chiazorgodwin@gmail.com'
+lang = 'Python' 
+
+print("Hello World, this is", full_name, "with HNGi7 ID", hng_id, "using", lang, "for stage 2 task.{}".format(email_adr))

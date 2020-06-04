82287476fcc388518211555d94e10319ac949937
Blessedpsalm <43576157+Blessedpsalm@users.noreply.github.com>
2020-06-04 16:14:14 +0100

    Update blessedpsalm.py

--- scripts/blessedpsalm.py
+++ scripts/blessedpsalm.py
@@ -8,6 +8,6 @@ Created on Thu Jun  4 16:00:26 2020
 
 def profile_format(first_name, last_name, stack, hng_id, email):
     full_name = first_name + ' ' + last_name
-    return print(f"Hello world, I am {full_name} with HNGi7 ID {hng_id} a beginner in {stack} for stage 2 task.{email}")
+    return print(f"Hello world, I am {full_name} with HNGi7 ID {hng_id} using {stack} for stage 2 task.{email}")
 
 profile_format('Samuel', 'Ogunsola', 'Python, HTML and CSS', 'HNG-00800','sammy4excellence97@gmail.com')

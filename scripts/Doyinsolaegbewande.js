d8771588a341af458d896d1f4a47f9fbe6ecce7d
egbewandedoyinsola <egbewandefolahanmi@gmail.com>
2020-06-04 14:03:27 +0100

    Doyinsolaegbewande.js

--- scripts/Doyinsolaegbewande.js
+++ scripts/Doyinsolaegbewande.js
@@ -1,5 +1,11 @@
-let fullName= "Doyinsola Egbewande";
-let iD= "HNG-01670";
-let languageUsed= "JavaScript";
-let email="egbewandefolahanmi@gmail.com";
-console.log("Hello World, this is " +  fullName + "" + " with HNGi7 ID " + "" + iD + " " + " using " + "" +  languageUsed + " for stage 2 task. "  + email);
+//this is stage 2 task by Doyinsola 
+//Declare variables
+var fullName="Doyinsola Egbewande";
+var idHNG="HNG-01670";
+var usedLanguage="JavaScript";
+var email="egbewandefolahanmi@gmail.com";
+function doyinsola_helloworld(name, iD, lang, mail){
+    //input data
+    console.log("Hello World, this is " + name + " with HNGi7 ID " + iD + " using " + lang +" for stage 2 task."+ mail);  
+}
+doyinsola_helloworld(fullName, idHNG, usedLanguage, email);

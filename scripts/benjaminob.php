41b9ac828428dd2b059210e17a643d5ed518770e
BenjaminOB <57020492+benjaminob@users.noreply.github.com>
2020-06-04 20:31:47 +0100

    Add files via upload

---
+++ scripts/benjaminob.php
@@ -0,0 +1,8 @@
+<?php
+$name = "BABATOLA BENJAMIN OLUWASEYI";
+$ID = "HNG-04711";
+$language = "PHP";
+$email = "benjaminob001@gmail.com";
+
+echo "Hello World, this is $name with with HNGi7 ID $ID using $language for stage 2 task. $email";
+?>

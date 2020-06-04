71f649b5a60df0303e0958afd147abc9e33023fc
Obusor Ezekiel <obusorezekiel@gmail.com>
2020-06-04 09:42:49 +0100

    Ezekielumesi added his script

---
+++ scripts/Ezekielumesi.js
@@ -0,0 +1,11 @@
+let devName = "Ezekiel Umesi";
+let hngID = "HNG-00356";
+let language = "JavaScript";
+let mail = "obusorezekiel@gmail.com";
+
+// Function for the necessary data
+function HNG_Data(name, id, lang, mail){
+  console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + lang + " for stage 2 task."+ mail);
+}
+
+HNG_Data(devName, hngID, language, mail);

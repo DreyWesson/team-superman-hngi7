49ad01d7786f14dd12863ed6845f3ae6d722016a
Godson <adeshina.godwin@gmail.com>
2020-06-04 20:30:00 +0100

    Update Godsonn.js

--- scripts/Godsonn.js
+++ scripts/Godsonn.js
@@ -8,7 +8,7 @@ const myPerson = {
         console.log ( "Hello World, My name is " + this.fullName + " with HNGi7 INTERNSHIP ID " + this.ID + " using " + this.language + " for stage 2 task. Email:" + this.Email + "")
     }
     
-};
+}
 myPerson.myIntroduction()
 
 

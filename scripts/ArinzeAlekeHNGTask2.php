15db65407eba047adadc938dba5f9a45acb45c24
EmmanuelSeyi <emmanuelaleke@gmail.com>
2020-06-04 05:54:50 +0100

    ArinzeAlekeHNGTask2 with php

---
+++ scripts/ArinzeAlekeHNGTask2.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = "Arinzechukwu Aleke";
+$id = "HNG-03073";
+$language = "php";
+$email = "emmanuelaleke@gmail.com";
+
+
+welcome='Hello World, this is '.$name .' with HNGi7 ID'. $id.'using '.$language . ' for stage 2 task.'.$email;
+
+echo $welcome;
+
+?>

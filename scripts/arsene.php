7e03c9479e2cc39bbac75cb8ce6c4f5c871bbd4e
merlino <abideklove@gmail.com>
2020-06-04 18:38:18 +0100

    Update my php script for HNGI stage 1 Task

--- scripts/arsene.php
+++ scripts/arsene.php
@@ -2,8 +2,8 @@
 $name = "Bolanle Ayodeji";
 $id = "HNG-04600";
 $language = "php";
-$email = "abideklove@gmail.com"
-$text = "Hello World, this is {$name} with HNGi7 ID {$id} using {$language} for stage 2 task.{email}";
+$email = "abideklove@gmail.com";
+$text = "Hello World, this is {$name} with HNGi7 ID {$id} using {$language} for stage 2 task.{$email}";
 $result = json_encode($text);
 echo $result;
 ?>

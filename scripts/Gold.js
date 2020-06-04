e8accd9b2e1e128db44718f13127a4c7872a058e
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2020-06-04 18:26:54 +0100

    cleaning files

--- scripts/Gold.js
+++ scripts/Gold.js
@@ -4,6 +4,6 @@ let language = 'Javascript';
 let email = 'goldokiemute@gmail.com';
 
 function goldTask2(fullname, hngid, language, email) {
-    console.log(`Hello World, this is ${fullname} with HNGi7 ID ${hngid} and using ${language} for stage 2 task.${email}`);
+    console.log(`Hello World, this is ${fullname} with HNGi7 ID ${hngid} using ${language} for stage 2 task.${email}`);
 }
 goldTask2('Okiemute Gold', 'HNG-01033', 'Javascript', 'goldokiemute@gmail.com')

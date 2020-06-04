0bbf14589e18419c8bc5dc0ec89489d19b8e70c7
victor200-tech <55100684+victor200-tech@users.noreply.github.com>
2020-06-04 18:33:45 +0100

    Update segun33.js

--- scripts/segun33.js
+++ scripts/segun33.js
@@ -8,5 +8,5 @@ const email = "ogunekun@gmail.com";
 const language = "javascript";
 
 const text = () => {
-    return "Hello world. This is ${name} with HNGI7 ID ${id} using ${language} for stage 2 task.${email}"};
+    return "Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}"};
 console.log(text());

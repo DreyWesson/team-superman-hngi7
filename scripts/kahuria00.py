7691b23958292696d5470cf80d2ef7a433fb047b
kahuria00 <charitykahuria@gmail.com>
2020-06-04 09:19:30 +0300

    email added again

--- scripts/kahuria00.py
+++ scripts/kahuria00.py
@@ -1,4 +1,4 @@
-print("Hello World, this is {0} of email {1}  with HNGi7 {2} using {3} for stage 2 task".format('Charity Wanjiru','charitykahuria@gmail.com','ID 00621','Python'))
+print("Hello World, this is {name}  with HNGi7 {id} using {ln} for stage 2 task".format(name='Charity Wanjiru',email='charitykahuria@gmail.com',id='ID 00621',ln='Python'))
 
 
 

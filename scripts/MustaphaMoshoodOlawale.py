a7f1525c467a20063577894f8bc92079c481ef5f
Mustapha Moshood <olawale3415@gmail.com>
2020-06-04 14:45:23 +0100

    change to format method

--- scripts/MustaphaMoshoodOlawale.py
+++ scripts/MustaphaMoshoodOlawale.py
@@ -1,7 +1,7 @@
-def task_2 (name, id, language):
-	print("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task".format(name, id, language))
+def task_2 (name, id, language, email):
+	print("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task. {3}".format(name, id, language, email))
 
-task_2("Mustapha Moshood Olawale", "HNG-05611", "Python" )
+task_2("Mustapha Moshood Olawale", "HNG-05611", "Python", "folarinolawale3415@gmail.com" )
 
 
 

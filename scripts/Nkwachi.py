925beadfcb3e47ebd88b2535cdbf20c02ecb0785
Nkwachi <nkwachinwamaghinna@gmail.com>
2020-06-04 09:58:25 +0100

    Nkwachi Nwamaghinna: Added email

--- scripts/Nkwachi.py
+++ scripts/Nkwachi.py
@@ -1,4 +1,4 @@
 name = 'Nkwachi Nwamaghinna'
 hngID = 'HNG-02656'
 
-print('Hello World, this is '+name+' with HNGi7 ID '+hngID+' using python for stage 2 task')
+print('Hello World, this is '+name+' with HNGi7 ID '+hngID+' using python for stage 2 task.nkwachinwamaghinna@gmail.com')

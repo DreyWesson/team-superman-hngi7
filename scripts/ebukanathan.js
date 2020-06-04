e0294a593bce33955d8a6fb86fa45f14a2cacdf5
unknown <ebukanathan@gmail.com>
2020-06-04 02:53:44 +0100

    On branch master  Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	new file:   scripts/ebukanathan.js

---
+++ scripts/ebukanathan.js
@@ -0,0 +1,11 @@
+function intern(name,no,language,email) {
+    this.fullname = name;
+    this.id = no;
+    this.language = language;
+    this.email = email;
+    console.log(`Hello World, this is ${this.fullname} with HNGi7 ID ${this.id} using ${this.language} for stage 2 task`);
+    
+};
+
+intern("ebuka Nathan","HNG-02719","Javascript")
+

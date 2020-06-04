dff8b068b4ef2d925df7ab0efd45a5d1595e2bba
babyabdul <aral-hassan001@st.ug.edu.gh>
2020-06-04 03:34:48 +0100

    adding my email

--- scripts/abdulrafik.py
+++ scripts/abdulrafik.py
@@ -1,5 +1,5 @@
-def hng_stage2_task(name, id, lang):
-    print(f" Hello World, this is {name} with HNGi7 ID:  {id} using {lang} for stage 2 task")
+def hng_stage2_task(name, id, lang, email):
+    print(f" Hello World, this is {name} with HNGi7 ID:  {id} using {lang} for stage 2 task.{email}")
 
 
-hng_stage2_task("Abdul Rafik Al-hassan", "HNG-05318", "Python")
+hng_stage2_task("Abdul Rafik Al-hassan", "HNG-05318", "Python","aral-hassan001@st.ug.edu.gh")

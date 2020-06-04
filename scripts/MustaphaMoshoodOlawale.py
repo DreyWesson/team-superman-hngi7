d3d41443550f2663aa3e4592754a029cc36b5f89
Mustapha Moshood <olawale3415@gmail.com>
2020-06-04 14:24:25 +0100

    change to format method

--- scripts/MustaphaMoshoodOlawale.py
+++ scripts/MustaphaMoshoodOlawale.py
@@ -1,5 +1,5 @@
 def task_2 (name, id, language):
-	print(f"Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task")
+	print("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task".format(name, id, language))
 
 task_2("Mustapha Moshood Olawale", "HNG-05611", "Python" )
 

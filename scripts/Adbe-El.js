310f13ccf7a1602dd7ce1e183f01cd9493b99d2d
Adbeel Omiunu <45834662+Adbe-El@users.noreply.github.com>
2020-06-04 17:47:26 +0100

    Adbe-El.js

--- scripts/Adbe-El.js
+++ scripts/Adbe-El.js
@@ -3,4 +3,5 @@ const Id = "HNG-02582";
 const language = "JavaScript";
 const email = "adbeelomiunu@gmail.com";
 
+
 console.log("Hello World, this is " + name + " with HNGi7 ID " + Id + " using " + language + " for stage 2 task. " + email);

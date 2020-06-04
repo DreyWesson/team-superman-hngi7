a6ca92f613817649c63a843add54bb672069734d
Johnmattee <42149205+Johnmattee@users.noreply.github.com>
2020-06-04 16:21:12 +0100

    pls delete HNG-00374.js in the script folder
    I have another file of the same details, help me delete this one

--- scripts/HNG-00374.js
+++
@@ -1,6 +0,0 @@
-let name = "Ajayi John";
-let id = "HNG-00374";
-let language = "Javascript";
-let email="ajayitaiwojohnmatt@gmail.com";
-
-console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`);

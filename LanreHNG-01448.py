08b19ef46d67c1694a7f32ef8dafcd1c032a353a
Lanre <39558521+Talphlanre@users.noreply.github.com>
2020-06-04 09:03:05 +0100

    Create LanreHNG-01448.py
    my task 2 project

---
+++ LanreHNG-01448.py
@@ -0,0 +1,7 @@
+def hng(name, ID, language, email):
+    print("Hello World, this is {} with HNGi7 ID {} using {} for Stage 2 Task.{}".format(
+        name, ID, language, email, ))
+
+
+hng("Fatoki Lanre", "HNG-01448",
+    "Python", "phattalph@gmail.com")

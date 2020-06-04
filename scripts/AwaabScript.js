c6b9941c046923dadccf42466afc5ab72f954afe
AwaabSingh <adeoyin2828@gmail.com>
2020-06-04 18:50:41 +0100

    correction made pls merge

--- scripts/AwaabScript.js
+++ scripts/AwaabScript.js
@@ -4,7 +4,7 @@
                       language = 'JavaScript',
                       email = "adeoyin2828@gmail.com") {
           console.log (
-                `Hello World, this is ${fullName} with HNGI7 ID ${id}  using ${language} for stage 2 task. ${email}`
+                `Hello World, this is ${fullName} with HNGI7 ID ${id}  using ${language} for stage 2 task.${email}`
             );
   };
 

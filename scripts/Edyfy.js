12595d8d515b8033f8148839d98ee4de4beddf34
Oyoh edmond <chiwunmba@gmail.com>
2020-06-04 19:50:44 +0100

    oyoh edmond

--- scripts/Edyfy.js
+++ scripts/Edyfy.js
@@ -3,5 +3,5 @@ let Name = "Oyoh Edmond",
     Lang = "Javascript",
     Email = "Chiwunmba@gmail.com";
 
-console.log(`Hello world,this is ${Name} with HNGi7 ${Id} using ${Lang} for stage 2 task. ${Email}`);
+console.log(`Hello World, this is ${Name} with HNGi7 ${Id} using ${Lang} for stage 2 task.${Email}`);
 

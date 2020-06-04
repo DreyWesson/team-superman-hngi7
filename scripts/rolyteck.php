6a91fcda3fa9d321d1512e7004a67f1ab3765318
Blessing Roland <30534732+Rolyteck@users.noreply.github.com>
2020-06-05 00:14:25 +0100

    My Stage 2 task

---
+++ scripts/rolyteck.php
@@ -0,0 +1,13 @@
+ 
+
+<?php
+
+ //function to display intern details
+ function intern($name, $id, $pro_lang, $email)
+ {
+     return "Hello World, this is ".$name. " with HNGi7 id ".$id. " using ".$pro_lang." for stage 2 task.".$email;
+ }
+
+ $output = intern("Roland Blessing", "HNG-02628","PHP","Rolyconsult@gmail.com");
+
+ echo $output;

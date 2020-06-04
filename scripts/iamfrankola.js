fe2d161fda6a9de4ada561128a545e7e4506f793
Francis Uthman kolapo <44780222+iamfrankola@users.noreply.github.com>
2020-06-04 19:54:24 +0100

    Add files via upload

---
+++ scripts/iamfrankola.js
@@ -0,0 +1,6 @@
+var fullName = 'Kolapo Francis Uthman';
+var id = 'HNG-03952';
+var language = 'Javascript';
+var email = 'iamfrankola@mail.com';
+
+console.log(`Hello world, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`);

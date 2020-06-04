7b4a4415fd2a7de6a6af2e95b15bab403a319b19
Alextola98 <66447434+Alextola98@users.noreply.github.com>
2020-06-04 12:32:56 +0100

    deleted wrong scripts
    deleted wrong scripts

--- scripts/Okewusi Olutola.js
+++
@@ -1,8 +0,0 @@
-let container = [
-    fullName = 'Okewusi Olutola',
-    id = 'HNG-06621',
-    email = 'alextola98@gmail.com',
-    language = 'JavaScript'
-];
-
-console.log( 'Hello World, this is ' + container["0"] + ' with HNGi7 ID ' + container["1"] + ' using ' + container["3"] + ' for stage 2 task. ' + container["2"] );

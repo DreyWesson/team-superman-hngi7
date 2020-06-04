16610a2dfba1c1d5464358687fa9ab10d0fe0df4
Balogun dolapo <lanrebalogun46@gmail.com>
2020-06-04 03:56:17 +0100

    xyzcoderfd script

---
+++ scripts/xyzcoderfd.js
@@ -0,0 +1,7 @@
+let myIntro = {
+    fullName: "Dolapo Balogun",
+    id: "HNG-04619",
+    lang: "JavaScript",
+    email: "lanrechuks@yahoo.com"
+}
+console.log("Hello world, this is " + fullName + " with HNGi7 ID " + id + " using " + language + " for stage 2 task." + Email)

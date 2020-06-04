0f1d228b587236ab80279e8619604bf6a5b57455
abideen305 <59992054+abideen305@users.noreply.github.com>
2020-06-04 16:13:06 -0600

    Update AbideenMuh.py

--- scripts/AbideenMuh.py
+++ scripts/AbideenMuh.py
@@ -2,7 +2,5 @@ myName = 'Abideen Muhammed'
 myID = 'HNG-02878'
 lang = 'Python'
 myEmail ='abideenmuhammed2018@gmail.com'
-def myself(myName,myID,lang,myEmail):
-    print(f"Hello World, this is {myName} with HNGi7 ID {myID} using {lang} for stage 2 task.{myEmail}")
-    return
-myself(myName,myID,lang,myEmail)
+print("Hello World, this is" +' '+ myName +" "+ "with HNGi7 ID" +" "+ myID +" "+ "using" +' '+ lang +" "+ "for stage 2 task."+""+myEmail)
+    

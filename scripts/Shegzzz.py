56211be04c61d435531ae804a399ca91846b9897
shegzzz <52917975+shegzzz@users.noreply.github.com>
2020-06-05 02:18:31 +0100

    Submission By Adeyeye Olusegun

---
+++ scripts/Shegzzz.py
@@ -0,0 +1,10 @@
+Name= "Olusegun Adeyeye"
+Id = "HNG-04289"
+Language = "Python"
+Email = "adeyeye.timilehin1@gmail.com"
+
+def introduction():
+    print (f"Hello World, this is {Name} with HNGi7 ID {Id} using {Language} for Stage 2 task.{Email}")
+    return
+
+introduction()

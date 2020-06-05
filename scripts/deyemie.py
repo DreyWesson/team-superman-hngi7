f0e52b3cbc909c067b87323c1b7497edda2f913d
Adeyemi <30883466+atoyegbe@users.noreply.github.com>
2020-06-05 16:52:06 -0700

    Update deyemie.py

--- scripts/deyemie.py
+++ scripts/deyemie.py
@@ -3,4 +3,4 @@ hngid = 'HNG-04780'
 email = 'herdeyemie@gmail.com'
 language = 'Python' 
 
-print("Hello World, this is Atoyegbe Adeyemi with HNGi7 ID HNG-04780, using Python for stage 2 task.herdeyemie@gmail.com")
+print("Hello World, this is Atoyegbe Adeyemi with HNGi7 ID HNG-04780 using Python for stage 2 task.herdeyemie@gmail.com")

f250b142c2a1e3fba62744fedce6ded006601f37
Emmanuel Menyaga <sparklinsparky@gmail.com>
2020-06-05 20:30:17 +0100

    making things work

--- scripts/tobby78.py
+++ scripts/tobby78.py
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-def my_profile(first_name, last_name, language, hng_id, email):
-    full_name = first_name + ' ' + last_name
-    print ("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task.{3}".format(full_name, hng_id, language, email))
-=======
 name = 'Kazeem Omoloja'
 hng_id = 'HNG-03159'
 email = 'omolojakazeem@gmail.com'
 language = 'Python'
->>>>>>> 76e0d68d619296c4c96dc01e4d33d342b8cabea9
 
-print("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task.{}".format(name, hng_id, language, email)
+print("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task.{3}".format(name, hng_id, language, email)
       )

421deabc7c4914b0af5074b5e4540ad6995a9cdf
nckappdev <thatdeveloperguy1301@gmail.com>
2020-06-05 06:52:31 +0100

    hng

--- scripts/hng-00260.js
+++ scripts/hng-00260.js
@@ -1,4 +1,4 @@
-const details = [
+const items = [
     "Ajao Ataulmujeeb",
     "HNG-00260",
     "Javascript",
@@ -8,11 +8,11 @@ const details = [
 
 
 
-const text = (name, id, lang, email) => {
+const start = (name, id, lang, email) => {
     console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task.${email}`)
 };
 
-text(details[0], details[1], details[2], details[3]);
+start(items[0], items[1], items[2], items[3]);
 
 
 

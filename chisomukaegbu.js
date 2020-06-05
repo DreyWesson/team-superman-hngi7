a01cc2a8d1ac3bc89141da694aad949ef136660c
Chisom <54583060+Chisom06@users.noreply.github.com>
2020-06-05 00:04:49 +0000

    Add files via upload

---
+++ chisomukaegbu.js
@@ -0,0 +1,12 @@
+//"Hello World, this is [full name] with HNGi7 ID [ID] usig [language] for stage 2 task.[Email]"
+
+let fullName = 'Chisom Ukaegbu';
+let idNumber = 'HNG-04485';
+let language = 'Javascript';
+let email = 'chisom@telladigital.com.ng';
+
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${idNumber} using ${language} for stage 2 task.${email}`);
+
+
+
+

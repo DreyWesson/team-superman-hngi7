785accced8a95a2385efaea52af6ae4823399d96
sosualfred <sosualfred@gmail.com>
2020-06-05 15:27:54 +0000

    clean up

--- Bernard.js
+++
@@ -1,7 +0,0 @@
- function firsttask (fullname, HNGid, language, email)
- {
-     console.log(
-         "hello World, this is $ {fullname} with HNGi7 ID ${ HNGid} using ${language} for stage 2 task.${email}"
-         );
- }
- firsttask ("Obinna Bernard Ofoegbu", "HNG-01203", "javascript", "obinnaofoegbu71@gmail.com");

d40749bca93873797ce0f3ef720683332a8138ab
Oshoke <goldbars.ng@gmal.com>
2020-06-05 06:01:26 +0100

    made corrections email

--- scripts/random_oshoke_louis.py
+++ scripts/random_oshoke_louis.py
@@ -3,8 +3,12 @@
 # containing the HNGi7 Intern profile
 
 def greet_hello(full_name, id, language,email):
-    return ("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task {3}".format(full_name,id,language,email))
+    return ("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task.{3}".format(full_name,id,language,email))
 
+fname="Oshoke Louis"
+id= "HNG-01673"
+lang = "python"
+email= "goldbars.ng@gmail.com"
 
-print(greet_hello("Oshoke Louis", "HNG-01673","python", "goldbars.ng@gmail.com"))
+print(greet_hello("fname", id, lang, email))
 

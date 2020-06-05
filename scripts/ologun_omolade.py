d3abb6c097fcbb311f610cfad9ef888f915ccdc0
delade <ologunomolade23@gmail.com>
2020-06-05 03:46:45 +0100

    this is my second task

---
+++ scripts/ologun_omolade.py
@@ -0,0 +1,10 @@
+"""
+This is a script to print my details
+"""
+email = "ologunomolade23@gmail.com"
+
+
+def hng_task_two(name, id, language):
+    print("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task.{}".format(name, id, language, email))
+
+hng_task_two("ologun omolade", "HNG-00393", "Python")

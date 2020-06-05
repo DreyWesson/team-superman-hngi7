3f818c373dcb26b27074158e6ab85c2dd8bc44fa
shinaayomi <lateef718@gmail.com>
2020-06-05 17:45:37 +0100

    Update shinaayomi.py

--- scripts/shinaayomi.py
+++ scripts/shinaayomi.py
@@ -1,5 +1,11 @@
-name = "Adebisi Abdullateef"
-id = "HNGi7 HNG-04601"
-language = "python"
-email = "lateef718@gmail.com"
-print("Hello World, this is " + name + " with " + id + " using " + language + " for stage 2 task." + email)
+def main_details(name, hng_id, email, language):
+	print("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " using "+ language +" for stage 2 task.{}".format(email))
+	
+
+name = "Adebisi Abdullateef" 
+hng_id = "HNG-04601"
+language = "Python" 
+email = "lateef718@gmail.com"	
+
+if __name__ == "__main__":
+	main_details(name, hng_id, email, language)

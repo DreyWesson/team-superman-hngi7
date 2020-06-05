c357a8ec81f45614dffe90b0eeb40dbd3075ee48
intelligentzombie1 <50877510+intelligentzombie1@users.noreply.github.com>
2020-06-05 18:06:54 +0100

    Add files via upload

--- scripts/kayodeoshodi.js
+++ scripts/kayodeoshodi.js
@@ -4,9 +4,9 @@ const language = 'Javascript';
 const email = 'kayodeoshodi400@gmail.com'
 
 const script = () => {
-	let output = `Hello world this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`;
+	let output = `Hello world, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`;
 
 	console.log(output);
 }
 
-script()
+script(name, id, language, email);

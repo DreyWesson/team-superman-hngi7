98a7009532e729b8c1b131e1e5867d5c22cae8c6
sophiekalu <sophiekalu.developer@gmail.com>
2020-06-05 17:39:17 +0100

    Updating my my script

--- scripts/sophie-kalu.js
+++ scripts/sophie-kalu.js
@@ -1,8 +1,6 @@
 const sayHello = (fullName, id, language, email) => {
-	return (`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`);
+	console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`);
 };
 
 // Output
-const output = sayHello('Sophie Kalu', 'HNG-03921', 'JavaScript', 'sophiekalu.developer@gmail.com');
-
-console.log(output);
+sayHello('Sophie Kalu', 'HNG-03921', 'JavaScript', 'sophiekalu.developer@gmail.com');

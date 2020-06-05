9472775eacbb3119b581c1250c4097bf8bafad40
dutchkay1 <ductchkay7@gmail.com>
2020-06-05 09:25:45 +0100

    Js updated

--- scripts/dutchkay.js
+++ scripts/dutchkay.js
@@ -1,5 +1,5 @@
 // Using the arrow function to declare my intro
-const functionKay = (fullName, myId, languageStack, email) => {
+function functionKay(fullName, myId, languageStack, email) {
 	console.log(`Hello World, this is ${fullName} with HNGi7 ID ${myId} using  ${languageStack} for stage 2 task.${email}`)
 }
 functionKay("Chukwuebuka Okafor", "HNG-00102", "JavaScript", "ductchkay7@gmail.com");

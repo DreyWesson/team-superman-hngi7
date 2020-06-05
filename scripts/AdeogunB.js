eb8e99614d7fd844e8cef7748b156ad9ee4bf020
Babafemi Adeogun <54693061+adejesu@users.noreply.github.com>
2020-06-05 11:30:50 +0100

    AdeogunB.js

--- scripts/AdeogunB.js
+++ scripts/AdeogunB.js
@@ -1,16 +1,18 @@
-function Intern(name, hngId, devLanguage) {
+function Intern(name, hngId, devLanguage, email) {
     this.DevName = name;
     this.hngId = hngId;
     this.language = devLanguage;
+    this.email = email;
 }
 
-function sayHelloToHNG (name, Id, language) {
-    console.log ('Hello World, this is ${name} with HNGi7 ID ${Id} using ${language} for stage 2 task ')
+function sayHelloToHNG (name, Id, language, email) {
+    console.log ('Hello World, this is ${name} with HNGi7 ID ${Id} using ${language} for stage 2 task.${email}')
 }
 
 let devName = "Adeogun Babafemi";
 let devId = "HNG-03777";
 let devLanguage = "JavaScript";
+let devEmail = "babafem.adeogun@gmail.com"
 
-let Intern = new Intern (devName, devId, devLanguage);
-sayHelloToHNG (Intern.DevName, Intern.hngId, Intern.language);
+let Intern = new Intern (devName, devId, devLanguage, devEmail);
+sayHelloToHNG (Intern.DevName, Intern.hngId, Intern.language, Intern.devEmail);

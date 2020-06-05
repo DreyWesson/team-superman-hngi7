ebce51dc1af23028165dd01c600818accfcbd745
Sunkanmi Otitolaye <60078601+leosuky@users.noreply.github.com>
2020-06-05 20:27:49 +0100

    new

---
+++ scripts/SunkanmiOtitolaye.py
@@ -0,0 +1,7 @@
+# username: Sunkanmi
+# name: Sunkanmi Otitolaye
+# id: HNG-03049
+# email: leosuky99@gmail.com
+
+
+print("Hello World, this is Sunkanmi Otitolaye with HNGi7 ID HNG-03049 using Python for stage 2 task.leosuky99@gmail.com")

bb778ad1f71dbb2b1e0bb98bad9de231babeb117
Mfonabasi <thompsonmfonabasi@gmail.com>
2020-06-05 08:45:43 +0100

    Made changes to internscript and made changes to the following this name, thid id, this language

---
+++ scripts/MfonabasiThompson.js
@@ -0,0 +1,14 @@
+function sayHello(fullname, id, language) {
+ 
+     
+     this.fullname = 'Mfonabasi  Thompson';
+     this.id = 'HNG-04400';
+     this.language = 'Java Script';
+  console.log(
+   `Hello World, this is ${fullname} with HNGi7 ${id} using ${language}`
+  );
+    
+}
+sayHello();
+
+

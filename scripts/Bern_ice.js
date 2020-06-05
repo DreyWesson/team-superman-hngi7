f2243556a9729fbf1e6995428453511aa539f3c7
Bernice El. Omiunu <53863709+Beel234@users.noreply.github.com>
2020-06-05 23:46:25 +0100

    Updated Bern_ice

--- scripts/Bern_ice.js
+++ scripts/Bern_ice.js
@@ -1,4 +1,4 @@
-const HNG_task = (fullname, id, language, email) => {
+const hngTask = (fullname, id, language, email) => {
   console.log(`Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}`);
   }
-  HNG_task("Bernice Omiunu", "HNG-02649", "Javascript","bel4luv4@gmail.com");
+  hngTask("Bernice Omiunu", "HNG-02649", "Javascript","bel4luv4@gmail.com");

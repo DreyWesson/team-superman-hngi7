690bb90edf2249dbb9ac645e5790aa1c0e89f2f2
oluwaseyifunmitan Muyiwa <49420652+Seyifunmitan@users.noreply.github.com>
2020-06-05 14:44:48 +0100

    Update my script after failing the script-test
    Update my script after failing the script-test
    change language to JavaScript instead of Javascript

--- scripts/seyifunmitan.js
+++ scripts/seyifunmitan.js
@@ -3,4 +3,4 @@ function intro(name, id, language, email) {
   console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`)
 }
 
-intro('Ademujimi Oluwaseyi', 'HNG-00685' , 'Javascript', 'oluwaseyifunmitan5@gmail.com')
+intro('Ademujimi Oluwaseyi', 'HNG-00685' , 'JavaScript', 'oluwaseyifunmitan5@gmail.com')

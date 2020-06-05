2ae25af4d255e6b0f1e689b1bbdf975cfc568f94
Francis Uthman kolapo <44780222+iamfrankola@users.noreply.github.com>
2020-06-05 22:49:05 +0100

    Update iamfrankola.js

--- scripts/iamfrankola.js
+++ scripts/iamfrankola.js
@@ -3,4 +3,4 @@ var id = 'HNG-03952';
 var language = 'JavaScript';
 var email = 'iamfrankola@mail.com';
 7
-console.log(`Hello World, this is ${name} with HNG ID ${id} and email ${email}  using ${language} for stage 2 task.`);
+console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and using ${language} for stage 2 task.${email}`)

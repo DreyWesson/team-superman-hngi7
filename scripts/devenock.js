5485670f1230db973e3ea1cde029c57346e1aaa8
Trend20 <enockomondi305@gmail.com>
2020-06-05 13:35:59 -0400

    devenock

--- scripts/devenock.js
+++ scripts/devenock.js
@@ -1,17 +1,19 @@
-const myDetails = (myName, myId, myStackLanguage) =>{
+const myDetails = (myName, myId, myStackLanguage,email) =>{
     this.myName = myName;
     this.myId = myId;
     this.myStackLanguage;
+    this.email = email;
 }
 
-const greetings = (myName,myId, myStackLanguage) =>{
-   console.log(`Hello World, this is ${myName} with HNGi7 ID ${myId} using ${myStackLanguage} for stage 2 task `);
+const greetings = (myName,myId, myStackLanguage,email) =>{
+   console.log(`Hello World, this is ${myName} with HNGi7 ID ${myId} using ${myStackLanguage} for stage 2 task. ${email} `);
 }
 
-const data = new myDetails(userName, userId, userlanguage);
-greetings(data.userName, data.userId, data.userLanguage);
+const data = new myDetails(userName, userId, userlanguage,email);
+greetings(data.userName, data.userId, data.userLanguage, data.email);
 
 
 const  userName='Enock Omondi Otuoma';
-const  userId="HNG-03371";
-const  userLanguage="JavaScript";
+const  userId='HNG-03371';
+const  userLanguage='JavaScript';
+const email = 'enockomondi305@gmail.com'

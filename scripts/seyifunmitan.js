333c68f1a4cf83c41ef7049504b4b24c2ff94930
oluwaseyifunmitan Muyiwa <49420652+Seyifunmitan@users.noreply.github.com>
2020-06-05 14:46:51 +0100

    Updated script
    update the script to correct the error that causes failed script

--- scripts/seyifunmitan.js
+++ scripts/seyifunmitan.js
@@ -1,6 +1,6 @@
 
-function intro(name, id, language, email) {
+function intro(name='Ademujimi Oluwaseyi', id='HNG-00685', language= 'JavaScript', email='oluwaseyifunmitan5@gmail.com') {
   console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`)
 }
 
-intro('Ademujimi Oluwaseyi', 'HNG-00685' , 'JavaScript', 'oluwaseyifunmitan5@gmail.com')
+intro()

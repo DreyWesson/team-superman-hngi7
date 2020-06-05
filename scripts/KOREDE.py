2b0643947d0785f19ef4f01054899df922b51892
generalkorex <35337945+generalkorex@users.noreply.github.com>
2020-06-05 10:34:05 +0100

    Update KOREDE.py

--- scripts/KOREDE.py
+++ scripts/KOREDE.py
@@ -1,1 +1,1 @@
-print("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task" %("Sulaiman Korede", "HNG-01969", " Python"))
+print("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task Email %s" %("Sulaiman Korede", "HNG-01969", " Python","sharpboymekorex@gmail.com"))

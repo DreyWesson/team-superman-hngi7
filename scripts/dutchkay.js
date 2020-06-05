672041bd93a5e5cce5b21a6c84e298135e82ae57
dutchkay1 <ductchkay7@gmail.com>
2020-06-05 18:06:12 +0100

    new changes have been made

--- scripts/dutchkay.js
+++ scripts/dutchkay.js
@@ -2,9 +2,9 @@ function dutchKay() {
 	let myName = "Chukwuebuka Okafor";
 	let hngId = "HNG-00102";
 	let language = "JavaScript";
-	let email = "ductchkay7@gmail.com";
+	let myEmail = "ductchkay7@gmail.com";
 	
-	console.log(`Hello World, this is ${myName} with HNGi7 ID ${hngId} using ${language} for stage 2 task.${email}`);
+	console.log(`Hello World, this is ${myName} with HNGi7 ID ${hngId} using ${language} for stage 2 task.${myEmail}`);
 }
 
 dutchKay();

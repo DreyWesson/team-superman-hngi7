22120df9a45cac6727cda22acdaf295246e2c5e5
Adbeel Omiunu <45834662+Adbe-El@users.noreply.github.com>
2020-06-05 20:25:10 +0100

    Adbe-el.js

--- scripts/Adbe-El.js
+++ scripts/Adbe-El.js
@@ -4,7 +4,7 @@ const language = "JavaScript";
 const email = "adbeelomiunu@gmail.com";
 
 let sayHelloWorld = (name, id, lang, email) => {
-console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + lang + " for stage 2 task. " + email);
+console.log(`Hello world, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task.${email}`);
 };
 
 sayHelloWorld(name, Id, language, email);

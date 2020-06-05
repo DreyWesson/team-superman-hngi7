dbe1334fe77b324363fe5681af9bdf5db4a4dd0b
Ashimi <princeadebowale3@gmail.com>
2020-06-06 00:56:54 +0100

    added ashimi.php

---
+++ scripts/ashimi.php
@@ -0,0 +1,8 @@
+<?php
+$full_name = "Abdulwahab Ashimi";
+$id = "HNG-04636";
+$lang = "PHP";
+$email = "princeadebowale3@gmail.com";
+
+echo "Hello World, this is $full_name with HNGi7 ID $id using $lang for stage 2 task.$email";
+?>

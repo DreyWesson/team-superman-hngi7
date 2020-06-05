ce884eed67677fcdec2b63aa08c98fd537630111
Ayanful <emmanuelayanful98@gmail.com>
2020-06-05 12:31:31 +0000

    new scripts

---
+++ scripts/python_script_edited.py
@@ -0,0 +1,5 @@
+name = "Ayanful Emmanuel Kwame"
+hng_id = "HNG-03582"
+language = "Python"
+email = "ekayanful@st.ug.edu.gh"
+print(f'Hello World, this is {name} with HNGi7 ID {hng_id} using {language} for stage 2 task.{email}')

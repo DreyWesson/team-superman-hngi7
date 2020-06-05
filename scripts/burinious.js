82f471b0b56b37b8d647391bbfcca0a433cc59dd
Ayoyinka <akomolafeayoyinka@gmail.com>
2020-06-05 01:24:22 +0100

    This is my hello world task for stage 2

---
+++ scripts/burinious.js
@@ -0,0 +1,11 @@
+const name = "Akomolafe Ayoyinka";
+const id = "HNG-04465";
+const language = "JavaScript";
+const email = "akomolafeayoyinka@gmail.com";
+
+const sayHelloToHNG = (name, id, language, email) => {
+    console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`);
+};
+
+
+sayHelloToHNG (name, id, language, email);

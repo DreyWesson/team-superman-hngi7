7661676cb90fa8d26b1aabf1196ad5cf33c967ed
tosinblessing <tosinblessing.shittu@gmail.com>
2020-06-05 10:16:56 +0100

    Revert "tosin.php"
    This reverts commit 3255b781873385309fcba83c982211f78f8f2281.

--- tosinshittu.php
+++
@@ -1,8 +0,0 @@
-<?php
-$fullName = "Shittu Tosin";
-$internId = "HNG-06430";
-$language = "PHP";
-$email = "tosinblessing.shittu@gmail.com";
-
-echo "Hello World, this is $fullName with HNGi7 ID  $internId using $language for stage 2 task .$email";
-?>

abc6a1d4e595ab12c16c9df667050781b5ae5212
sparkidea25 <michaeldaralola123@gmail.com>
2020-06-05 03:57:39 +0100

    micheal

--- scripts/gelgit.php
+++ scripts/gelgit.php
@@ -2,7 +2,7 @@
 $fname = "Micheal Daralola Oketunde";
 $ID = "HNG-04388";
 $lang = "PHP";
-$email = "michaeldaralola123@gmail.com"
+$email = "michaeldaralola123@gmail.com";
 
 
 echo "Hello World, this is $fname with HNGi7 ID $ID using $lang for stage 2 task.$email";

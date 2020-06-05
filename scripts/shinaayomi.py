3cab82b67929c3c21ebac779d11c19217781164d
shinaayomi <lateef718@gmail.com>
2020-06-05 18:02:40 +0100

    Update shinaayomi.py

--- scripts/shinaayomi.py
+++ scripts/shinaayomi.py
@@ -1,11 +1,8 @@
-def main_details(name, hng_id, email, language):
-	print("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " using "+ language +" for stage 2 task.{}".format(email))
-	
-
-name = "Adebisi Abdullateef" 
+name = "Adebisi Abdullateef"
 hng_id = "HNG-04601"
-language = "Python" 
-email = "lateef718@gmail.com"	
+programming_language = "Python"
+email = "lateef718@gmail.com"
 
-if __name__ == "__main__":
-	main_details(name, hng_id, email, language)
+print("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task.{}"
+.format(name, hng_id, programming_language, email))
+

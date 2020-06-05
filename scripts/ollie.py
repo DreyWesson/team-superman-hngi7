ea9c292c64bfd7a06723c9698bd0ad3cd28d123e
oladolapo <66223952+oladolapo@users.noreply.github.com>
2020-06-05 22:54:24 +0100

    Delete ollie.py

--- scripts/ollie.py
+++
@@ -1,5 +0,0 @@
-fullname = 'Ogunsipe Oladolapo"'
-ID = 'HNG-00590'
-email = 'dolapoogunsipe@gmail.com'
-language = 'Python' 
-print("Hello World, this is", fullname, "with HNGi7 ID", ID, "using", language, "for stage 2 task.{}".format(email))

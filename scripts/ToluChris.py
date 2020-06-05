3fbe8321e83aaad0fa75614e87c5d8b6d5545623
tolu0099 <tolu.chris@ymail.com>
2020-06-05 13:24:53 -0700

    Add files via upload

---
+++ scripts/ToluChris.py
@@ -0,0 +1,15 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> myname="Olatunbosun Tolulope"
+>>> ID= HNG-05559
+SyntaxError: invalid token
+>>> ID="HNG-05559"
+>>> language="python"
+>>> email="tolu.chris@ymail.com"
+>>> print("Hello World, this is " +myname+ " with HNGi7 ID of " +ID+ " using " +language+ " for stage 2 task " +email+" ")
+Hello World, this is Olatunbosun Tolulope with HNGi7 ID of HNG-05559 using python for stage 2 task tolu.chris@ymail.com 
+>>> 
+KeyboardInterrupt
+>>> print("Hello World, this is " +myname+ " with HNGi7 ID of " +ID+ " using " +language+ " for stage 2 task of email  " +email+" ")
+Hello World, this is Olatunbosun Tolulope with HNGi7 ID of HNG-05559 using python for stage 2 task of email  tolu.chris@ymail.com 
+>>> 

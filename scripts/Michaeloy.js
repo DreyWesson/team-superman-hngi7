88baa047a851ea7c905314af2ed179626440ac24
tosinblessing <52457129+tosinblessing@users.noreply.github.com>
2020-06-05 08:15:54 +0100

    Revert "tosinshittu.php"
    This reverts commit cd53673b901fa220b49d0f6e50d3f592ac149bfd.

--- scripts/Michaeloy.js
+++ scripts/Michaeloy.js
@@ -1,11 +1,4 @@
+const myProfile = (name, id, lang, email) => 
+`"Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task.${email} "`;
 
-let name = "Michael John";
-let id = "HNG-00424";
-let lang = "JavaScript";
-let email = "johnmikeloy@gmail.com";
-
-function myInfo(){
-    console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task.${email}`);
-}
-
-myInfo();
+console.log(myProfile("Michael John", "HNG-00424", "JavaScript", "johnmikeloy@gmail.com"));

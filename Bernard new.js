5e015c76ad142d3de5f5a2038dc816201194bd6a
sosualfred <sosualfred@gmail.com>
2020-06-06 08:28:13 +0000

    fix merge

--- Bernard new.js
+++
@@ -1,7 +0,0 @@
- function firsttask (fullname, HNGid, language, email)
- {
-     console.log(
-         `Hello World, this is ${fullname} with HNGi7 ID ${HNGid} using ${language} for stage 2 task.${email}`
-         );
- }
- firsttask ("Obinna Bernard Ofoegbu", "HNG-01203", "JavaScript", "obinnaofoegbu71@gmail.com");

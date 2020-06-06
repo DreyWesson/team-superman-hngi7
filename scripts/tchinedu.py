473e7883e3b5be6fcb672299b0a6b853656bfcf8
tchinedu <51605328+tchinedu@users.noreply.github.com>
2020-06-06 14:08:11 +0100

    Create tchinedu.py

---
+++ scripts/tchinedu.py
@@ -0,0 +1,10 @@
+#HNG tasks 3
+
+name = 'Chinedu Oguegbu'
+id = 'HNG-00303'
+language = 'Python'
+email = 'chineduoguegbu@gmail.com'
+
+output = f'Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task.{email}'
+
+print(output)

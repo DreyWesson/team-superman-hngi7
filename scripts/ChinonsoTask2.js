e888ce16af50676f7271f76bc9089d93049508c5
Chinonso Stanislaus Ngwu <38362902+MrSCAN@users.noreply.github.com>
2020-06-06 09:07:20 +0100

    Fixed the issues
     please help me check this code, I don't know what else to do

--- scripts/ChinonsoTask2.js
+++ scripts/ChinonsoTask2.js
@@ -5,20 +5,21 @@ const myInfo = {
     return this.firstName + " " + this.lastName;
   },
   hngId: "HNG-02298",
-  language: "Javascript",
+  language: "JavaScript",
   email: "ngwucs@gmail.com",
 };
 
-console.log(
-  "Hello World, this is " +
-    myInfo.fullName() +
-    " " +
-    "with HNGi7 ID " +
-    myInfo.hngId +
-    " " +
-    "using" +
-    " " +
-    myInfo.language +
-    " " +
-    " for stage 2 task."+myInfo.email
-);
+function myTask() {
+  console.log(
+    "Hello World, this is " +
+      myInfo.fullName() +
+      " with HNGi7 ID " +
+      myInfo.hngId +
+      " using " +
+      myInfo.language +
+      " for stage 2 task." +
+      myInfo.email
+  );
+}
+
+myTask();

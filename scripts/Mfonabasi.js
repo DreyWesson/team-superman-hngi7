c07bbdcd654c3ebcec83fa9144c6197f4079744d
Sosu Alfred <sosualfred@gmail.com>
2020-06-06 21:51:04 +0000

    Update Mfonabasi.js

--- scripts/Mfonabasi.js
+++ scripts/Mfonabasi.js
@@ -6,6 +6,6 @@ let language
 name = "Mfonabasi Thompson";
 id = "HNG-04400";
 email = "mfonabasithompson@gmail.com";
-language = "Javascript";
+language = "JavaScript";
 
 alert("Hello World, this is ", name, "with HNGi7", id, "and email" ,email, "using", language );

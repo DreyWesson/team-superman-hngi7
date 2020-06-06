1bbb9855491faf45d308d948e4fa81a57eac0dd1
yempie <62244332+yempie@users.noreply.github.com>
2020-06-06 21:22:33 +0100

    Update YempieCorinne.php

--- scripts/YempieCorinne.php
+++ scripts/YempieCorinne.php
@@ -5,6 +5,6 @@ $ID="HNG-06785";
 $language="PHP";
 $email="yempiecorinne@gmail.com";
 
-echo "Hello World, this is $full_name with HNGi7 ID $ID using $language for stage 2 task. My email:$email";
+echo "Hello World, this is $full_name with HNGi7 ID $ID using $language for stage 2 task.$email";
 
 ?>

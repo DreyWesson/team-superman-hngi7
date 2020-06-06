115a23571297e914a04037eb82ded7c170defcf7
Bamidele Osakwe <55025203+gallantme@users.noreply.github.com>
2020-06-06 22:07:33 +0100

    Add files via upload

---
+++ scripts/bamidele.js
@@ -0,0 +1,8 @@
+let hngIntern = {
+    name: "Osakwe Bamidele",
+    id: "HNG-05592",
+    language: "Javascript",
+    Email: "bamideleo25@gmail.com"
+};
+
+console.log(`Hello World, this is ${hngIntern.name} with HNGi7 ID ${hngIntern.id} using ${hngIntern.language} for stage 2 task.${hngIntern.Email}`);

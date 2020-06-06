3d93d6889e217991ad34f4d2e9a19c85fe7553e1
Blessing Roland <30534732+Rolyteck@users.noreply.github.com>
2020-06-06 16:57:50 +0100

    Stage 2 task

---
+++ scripts/rolyteck.php
@@ -0,0 +1,11 @@
+<?php
+$name = "Roland Chukwudi Blessing ";
+
+$hng_id = "HNG-02628";
+
+$language = "PHP";
+
+$email = "Rolyconsult@gmail.com";
+
+
+echo "Hello World, this is {$name} with HNGi7 ID {$hng_id} using {$language} for stage 2 task.{$email}";

2bd646d5195c9b2ac4215b2261252e1d1f503d53
Sosu Alfred <sosualfred@gmail.com>
2020-06-06 15:34:00 +0000

    Create Abiola Olamide Oluwayomi.js

---
+++ scripts/Abiola Olamide Oluwayomi.js
@@ -0,0 +1,3 @@
+function introduce(name="Abiola Olamide Oluwayomi", ID="HNG-06618",lang="javascript", email="horllamideh95@gmail.com"){
+console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} using ${lang} for stage 2 task.${email}`)}
+introduce()

831add72bdaef710594e87f359f333c668bed8c5
Elljay <32459954+hayomide@users.noreply.github.com>
2020-06-06 19:37:21 +0100

    Delete Ayobami_task2.py

--- Ayobami_task2.py
+++
@@ -1,6 +0,0 @@
-Name = "Elijah Ayobami Bello"
-ID = "HNG-00563"
-Language = "Python"
-Email = "bellijaah@gmail.com"
-
-print ("Hello World, this is "+Name+" with HNGi7 ID "+ID+ " using "+Language+" for stage 2 task. "+Email)

faf45fcd8ed54c4ef4983d163620ededd0c69a96
xemoy <57658067+xemoy@users.noreply.github.com>
2020-06-06 23:16:18 +0100

    Update and rename scripts/Olamide.js to Oluwayomi.js

---
+++ Oluwayomi.js
@@ -0,0 +1,7 @@
+function introduce(name="Abiola Oluwayomi", ID="HNG-00618",lang="javascript", email="horllamideh95@gmail.com"){
+console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} using ${lang} for stage 2 task.${email}`)}
+introduce()
+
+
+
+

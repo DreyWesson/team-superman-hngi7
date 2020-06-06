160990d0f1cab793a5d109fc4d874ac974228983
Kachi123 <kachi.erugo@codex.com.ng>
2020-06-06 14:21:17 +0100

    fix script problem

---
+++ scripts/onyedikachi123.js
@@ -0,0 +1,5 @@
+let fullName = 'Onyedikach Shedrach Erugo';
+let ID = 'HNG-03614';
+let language =  'Javascript';
+
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task`);

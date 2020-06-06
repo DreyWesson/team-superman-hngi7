f6845328a3d98baeda87e4dafbc20cdf1751e977
Elljay <32459954+hayomide@users.noreply.github.com>
2020-06-06 17:45:03 +0100

    Ayobami_task2
    Updated task

---
+++ scripts/Bami_task2.py
@@ -0,0 +1,5 @@
+fullname="Elijah Ayobami Bello"
+id="HNG-00563"
+email="bellijaah@gmail.com"
+language="python"
+print("Hello World, this is "+fullname+" with HNGi7 ID "+id+" and email "+email+ " using "+language+" for stage 2 task")

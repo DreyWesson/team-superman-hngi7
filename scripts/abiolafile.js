9655d21981f0e7366cb4dc5c19107d3f3b5e2d24
Sosu Alfred <sosualfred@gmail.com>
2020-06-06 22:34:58 +0000

    Update and rename abiola.js to abiolafile.js

---
+++ scripts/abiolafile.js
@@ -0,0 +1,9 @@
+var name = "Abiola Olamide";
+var id = "HNG-06618";
+var lang= "JavaScript";
+var email = "horllamideh95@@gmail.com"
+
+function mytask(name,id,lang,email){
+    console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + lang + " for stage 2 task." + email);
+}
+mytask(name,id,lang,email);

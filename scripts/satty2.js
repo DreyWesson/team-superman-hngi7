3e43ddbfc960fa202ee7eab5a51b5454813b5cc3
sattycool <bemyguest9ite@gmail.com>
2020-06-06 07:26:35 -0700

    javascript

---
+++ scripts/satty2.js
@@ -0,0 +1,16 @@
+const outPut =function(){
+
+    let myName = "ibrahim seidu";
+
+    let hngId = "HNG-04975";
+
+    let EmailInfo = "bemyguest9ite@gmail.com";
+
+    let languageUsed = "javaScript";
+
+    let information = "Hello World, this is" + myName +
+
+ " with HNGi7 ID " + hngId + " using " + languageUsed + " for stage 2 task. " + EmailInfo
+     console.log(information)
+}
+    outPut()

151db807e29af8b59bd6487c49bfbf6f3c60c84b
Blessedpsalm <43576157+Blessedpsalm@users.noreply.github.com>
2020-06-06 04:06:01 +0100

    Update blessedpsalm.py

--- scripts/blessedpsalm.py
+++ scripts/blessedpsalm.py
@@ -4,7 +4,10 @@ Created on Thu Jun  4 16:00:26 2020
 
 @author: bdp
 """
-(Name, Language, ID, Email) = ('Samuel Ogunsola', 'Python, HTML and CSS', 'HNG-00800','sammy4excellence97@gmail.com') 
+Name = 'Samuel Ogunsola'
+Language = 'Python'
+ID =  'HNG-00800'
+Email = 'sammy4excellence97@gmail.com' 
 
 def profile_format(Name, Language, ID, Email):
     return  ('Hello World, this is ' + (Name) + ' with HNGi7 ID ' + (ID) + ' using ' + (Language) + ' for stage 2 task. ' + (Email))

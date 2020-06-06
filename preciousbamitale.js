191220d1b4c1dddff5a2027af2ade7f9bcd89012
sosualfred <sosualfred@gmail.com>
2020-06-06 15:09:31 +0000

    resolve conflict merge delete precisoul...file

--- preciousbamitale.js
+++
@@ -1,7 +0,0 @@
-
-
-const p_name = 'Precious Bamitale';
-const HNGi_id = 'HNG-06792';
-const lang = 'javascript';
-
-console.log('Hello World, this is ${p_name} with HNGi7 ID ${HNGi_id} using ${lang} for stage 2 task')

33c1e4f86cf61df04040bc9857001ad2fc27d51e
Elljay <32459954+hayomide@users.noreply.github.com>
2020-06-06 15:26:54 +0100

    Add files via upload

--- scripts/Bami_task2.py
+++ scripts/Bami_task2.py
@@ -1,14 +1,8 @@
-HNGi7_text = 'Hello World!'
-HNGi7_ID = "HG-00563"
-language = 'Python'
-Full_name = {
-    "first_name": "Elijah",
-    "middle_name": "Ayobami",
-    "last_name": "Bello",
-}
+name = ' Elijah Ayomide Bello'
+Id = ' HNG-00563'
+language = ' Python'
+email =  ' bellijaah@gmail.com'
 
-x = Full_name["first_name"]
-y = Full_name["middle_name"]
-z = Full_name["last_name"]    
+print ('Hello World ' + 'this is' + name + ' with HNGi7 ID' + Id + ' using' + language + ' for stage 2 task.' + email)
 
-print(HNGi7_text +'\t This is \t' + x + '\t' + y + '\t' + z + '\t with HNGI7 ID ->>\t' + HNGi7_ID + '\t using->>\t' + language + '\t for stage2 task')
+

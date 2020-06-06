f1ac62ce0a54d6cb5f0c0dd15d47bc1309df1628
Sosu Alfred <sosualfred@gmail.com>
2020-06-06 22:13:41 +0000

    Update lawalluqman.py

--- scripts/lawalluqman.py
+++ scripts/lawalluqman.py
@@ -1,8 +1,1 @@
-# A function to accept 4 string arguments and return an expected string 
-# containing the HNGi7 Intern profile
-
-def say_hello(full_name, id:int, language):
-    return (f"Hello World, this is {full_name} with HNGi7 ID {id} and email ID {id} using {language} for stage 2 task")
-
-
-print(say_hello("lawal luqman", "HNG-2322", "luqmanlajani@gmail.com" "python"))
+print("Hello World, this is lawal luqman with HNGi7 ID HNG-02322 using Python Language for stage 2 task. luqmanlajani@gmail.com")

7e057aaa861a11564e023fd7780e660882329727
Oluwasegun Ajayi <32372535+sakarious@users.noreply.github.com>
2020-06-06 13:53:16 -0700

    real script

--- scripts/mooseize.php
+++ scripts/mooseize.php
@@ -5,7 +5,7 @@ $language = "PHP";
 $email = "ajayimooseize@gmail.com";
 
 
-$stage_two='Hello World, this is '.$name.' with HNGi7 Internship ID '.$id.' using '.$language.' for stage 2 task '.$email.'';
+$stage_two = "Hello World, this is $name with HNGi7 Internship ID $id using $language for stage 2 task.$email";
 
 echo $stage_two;
 

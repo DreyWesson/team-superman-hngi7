d56dca2f46879b89d1de9f0d8d7e695f73ff4b5a
berniescribblings <65433039+berniescribblings@users.noreply.github.com>
2020-06-06 11:12:39 +0100

    Update bernard.js

--- scripts/bernard.js
+++ scripts/bernard.js
@@ -1,7 +1,7 @@
-function firsttask (fullname, HNGid, language, email)
+function firsttask(Fullname, HNGid, Language, Email)
 {
     console.log(
-        'Hello World, this is $ {fullname} with HNGi7 ID ${ HNGid} using ${language} for stage 2 task.${email}'
+        `Hello World, This is ${fullname} with HNGi7 ID`${ HNGid} using ${language} for stage 2 task.${email}`
         );
 }
-firsttask ("Obinna Bernard Ofoegbu", "HNG-01203", "JavaScript", "obinnaofoegbu71@gmail.com");
+firsttask("Obinna Bernard Ofoegbu", "HNG-01203", "JavaScript", "obinnaofoegbu71@gmail.com");

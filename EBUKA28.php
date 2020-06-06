f77db60eaadc281b90046e559d77c9f644634bff
ebuka28 <chideraokoli28@gmail.com>
2020-06-06 10:28:48 -0600

    ebuka28-HNG-02101

---
+++ EBUKA28.php
@@ -0,0 +1,8 @@
+<?php 
+$Name= "Okoli Ebuka";
+$ID="HNG-02101";
+$Language="PHP";
+$Email="chideraokoli28@gmail.com";
+
+echo "Hello World, this is $Name with HNGi7 ID $ID using $Language for stage 2 task.$Email";
+?>

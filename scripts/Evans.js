24c0f167bf55c4fbaa0b0359f6ab304c74f2f461
BotanicalCoder <57745557+BotanicalCoder@users.noreply.github.com>
2020-06-06 09:13:57 +0100

    Corrected script

--- scripts/Evans.js
+++ scripts/Evans.js
@@ -7,6 +7,6 @@ var myHngDetails = {
 
 var { name, id, language, email } = myHngDetails;
 function myHngTask() {
-  return `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`;
+  console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`);
 }
-console.log(myHngTask(name, id, language,email));
+myHngTask(name, id, language,email);

f817049d6f33d9dabb6bcdae5048a16a5306d60b
Karimous6644 <55147254+Karimous6644@users.noreply.github.com>
2020-06-06 21:08:38 +0100

    Add files via upload

---
+++ scripts/KARIMOUS.py
@@ -0,0 +1,5 @@
+def introduction(name, ID, lang, email):
+    return f"Hello World, this is {name} with HNGi7 ID {ID} using {lang} for stage 2 task.{email}"
+
+
+print(introduction(name="Abdulkarim Abdulwahab", ID="HNG-06459", lang="Python", email="adebayo6644@gamil.com"))

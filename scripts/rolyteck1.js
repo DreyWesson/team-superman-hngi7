d6d3550eb7373a9202a5fae0790fc7d8d0c88705
Blessing Roland <30534732+Rolyteck@users.noreply.github.com>
2020-06-06 17:34:45 +0100

    HNG Stage 2

---
+++ scripts/rolyteck1.js
@@ -0,0 +1,9 @@
+
+var email = 'Rolyconsult@gmail.com';
+var fullname = 'Roland Blessing Chukwudi';
+var hngid = 'HNG-02628';
+var lan = 'javascript'
+
+var result = `Hello World, this is ${fullname} with HNGi7 ID ${hngid} using ${lan} for stage 2 task. email ${email}`;
+
+console.log(result)

901fe566b9c2bc35b768c32f131b4ae7ee381786
Sosu Alfred <sosualfred@gmail.com>
2020-06-06 22:19:19 +0000

    Update Abiola Olamide Oluwayomi.js

--- scripts/Abiola Olamide Oluwayomi.js
+++ scripts/Abiola Olamide Oluwayomi.js
@@ -1,6 +1,12 @@
-var namere = "Abiola Olamide";
-var ID = "HNG-06618";
-var languse = "JavaScript";
-var email = "horllamideh95@@gmail.com";
+const name = 'Abiola Olamide';
+const id = 'HNG-06618';
+const language = 'JavaScript';
+const email = 'horllamideh95@@gmail.com';
+
+const introduce = (name, id, language, email) => {
   
-console.log(`Hello World, this is ${namere} with HNGi7 ID ${ID} using ${languse} for stage 2 task.${email}`);
+ console.log(`Hello world, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`);
+
+};
+
+introduce (name, id, language, email);

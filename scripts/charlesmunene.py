c92d884de49e421232a5026425271b8ec3a40a0c
charles Munene <charlesmunenek@gmail.com>
2020-06-06 22:32:29 +0300

    first commit for task 1

---
+++ scripts/charlesmunene.py
@@ -0,0 +1,6 @@
+fullName = 'charles Munene'
+ID = 'HNG-02507'
+language = 'Python'
+myEmail ='charlesmunenek@gmail.com'
+print("Hello World, this is" +' '+ fullName +" "+ "with HNGi7 ID" +" "+ ID +" "+ "using" +' '+ language +" "+ "for stage 2 task."+""+myEmail)
+    

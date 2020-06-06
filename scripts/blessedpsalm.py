24cfdd86c1b1d490b30a18331cbf2af19827f84d
Blessedpsalm <43576157+Blessedpsalm@users.noreply.github.com>
2020-06-06 10:05:33 +0100

    Update blessedpsalm.py

--- scripts/blessedpsalm.py
+++ scripts/blessedpsalm.py
@@ -8,8 +8,7 @@ Name = 'Samuel Ogunsola'
 Language = 'Python'
 ID =  'HNG-00800'
 Email = 'sammy4excellence97@gmail.com' 
+print ('Hello World, this is ' + (Name) + ' with HNGi7 ID ' + (ID) + ' using ' + (Language) + ' for stage 2 task. ' + (Email))
+# def profile_format(Name, Language, ID, Email):
+#     return  ('Hello World, this is ' + (Name) + ' with HNGi7 ID ' + (ID) + ' using ' + (Language) + ' for stage 2 task. ' + (Email))
 
-def profile_format(Name, Language, ID, Email):
-    return  ('Hello World, this is ' + (Name) + ' with HNGi7 ID ' + (ID) + ' using ' + (Language) + ' for stage 2 task. ' + (Email))
-
-print (profile_format('Samuel Ogunsola', 'Python', 'HNG-00800','sammy4excellence97@gmail.com'))

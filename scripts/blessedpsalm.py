5eda6e8e0a6e99db2d5f5f4ed0e6e69eefb50fa2
Blessedpsalm <43576157+Blessedpsalm@users.noreply.github.com>
2020-06-06 23:34:43 +0100

    Update blessedpsalm.py

--- scripts/blessedpsalm.py
+++ scripts/blessedpsalm.py
@@ -1,14 +1,9 @@
-# -*- coding: utf-8 -*-
-# """
-# Created on Thu Jun  4 16:00:26 2020
-
-# @author: bdp
-# """
 Name = 'Samuel Ogunsola'
 Language = 'Python'
 ID =  'HNG-00800'
 Email = 'sammy4excellence97@gmail.com' 
-print ("Hello World, this is " + Name + " with HNGi7 ID " + ID + " using " + Language + " for stage 2 task. " + Email)
-# def profile_format(Name, Language, ID, Email):
-#     return  ('Hello World, this is ' + (Name) + ' with HNGi7 ID ' + (ID) + ' using ' + (Language) + ' for stage 2 task. ' + (Email))
 
+def profile_format():
+    print("Hello World, this is" , Name +" with HNGi7" + " ID", ID +" using" , Language + " for stage 2 task." +Email)
+
+profile_format()

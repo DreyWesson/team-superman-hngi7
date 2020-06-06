9fbf95db36b46c5afb85eeafd254684bdeba598b
Hanson <hanson2510@gmail.com>
2020-06-06 22:06:57 +0100

    deleted failed script

--- scripts/hanson0018.js
+++
@@ -1,4 +0,0 @@
-function hanson (name,hngid,langauge,email){
-    console.log ("Hello World, this is " + " " + name + " " + "with HNGi7 ID" + " " + hngid + " " + "using" + language + " " + "for Stage 2 task."+ email)
-}
-hanson ("Hanson Nnadi", "HNG-00018", "JavaScript", "hanson2510@gmail.com");

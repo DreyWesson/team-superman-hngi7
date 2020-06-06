6608e62346a3b7c57910b06d2665d1f5cf5dd7e0
xemoy <57658067+xemoy@users.noreply.github.com>
2020-06-05 22:44:52 -1200

    create olamide.js
    This is a JAVASCRIPT file

--- scripts/JegedeDavid.js
+++ scripts/JegedeDavid.js
@@ -1,3 +1,13 @@
-console.log("Hello World, this is Jegede David with HNGi7 ID HNG-05938 using JavaScript for stage 2 task");
 
+function hngdavid() {
 
+    let name = `Jegede David `;
+    let id = `HNG-05938`;
+    let lang = `JavaScript`;
+    let email = `davidstick766@gmail.com`;
+
+    let message = `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task.${email}`;
+    console.log(message);
+}
+
+hngdavid();

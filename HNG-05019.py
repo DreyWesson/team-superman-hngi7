3588bd70f526932b11729a186bb75bf0113d1732
Micheal Nwigwe <66284919+icecolddeveloper@users.noreply.github.com>
2020-06-06 14:19:49 +0100

    ice.py
    here is my new commit

---
+++ HNG-05019.py
@@ -0,0 +1,8 @@
+full_name = 'Nwigwe Michael Ifeanyi'
+hng_id = 'HNG-05019'
+language = 'Python'
+email = 'rubzyg@gmail.com'
+
+output = f'Hello World, this is {full_name} with HNGi7 ID {hng_id} using {language} for stage 2 task.{email}'
+
+print(output)

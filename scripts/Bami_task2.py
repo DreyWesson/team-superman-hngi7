94eced224679a41377811d3c94639f9cc1685341
Elljay <32459954+hayomide@users.noreply.github.com>
2020-06-06 21:36:37 +0100

    Ayobami_stage 2 task

---
+++ scripts/Bami_task2.py
@@ -0,0 +1,6 @@
+# below is my HNGi7 task 2 
+def HNGi7_task(name, HNGID, Language, Email):
+    return ("Hello World, this is " + (name) +" with HNGi7 ID " +(HNGID)+ " using "+ (Language) + " for stage 2 task." +(Email))
+
+
+print(HNGi7_task("Elijah Ayobami Bello", "HNG-00563", "Python", "bellijaah@gmail.com"))

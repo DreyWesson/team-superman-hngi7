afcd0cc2ca3cfd04a7df671c8df3f9b6a83b30c1
victor200-tech <55100684+victor200-tech@users.noreply.github.com>
2020-06-06 18:38:57 +0100

    Update segun33.js

--- scripts/segun33.js
+++ scripts/segun33.js
@@ -1,7 +1,7 @@
-var name = "Ogunekun Segun";
-var id =  "HNG-03092";
-var lang = "JavaScript";
-var email = "ogunekun@gmail.com"
+var name ="Ogunekun Segun";
+var id = "HNG-03092";
+var lang ="JavaScript";
+var email ="ogunekun@gmail.com"
 
 function mytask(name,id,lang,email){
     console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + lang + " for stage 2 task." + email);

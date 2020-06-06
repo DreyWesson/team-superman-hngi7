056f9163e72fddb6325205bc7c781654dd73f911
Onyedikachi123 <36213804+Onyedikachi123@users.noreply.github.com>
2020-06-06 18:31:30 +0100

    my new file

---
+++ scripts/Kachihng.js
@@ -0,0 +1,7 @@
+let fullName = 'Onyedikachi Shedrach Erugo';
+let ID = 'HNG-03614';
+let language =  'JavaScript';
+
+let email = 'onyedikachierugo@gmail.com'
+
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task.${email}`);

2c572081c290e873432249d17714ad7b70ec1796
chrisunday2013 <potentialsunny47@gmail.com>
2020-06-06 23:51:50 +0100

    javascript

---
+++ scripts/ibrahim.js
@@ -0,0 +1,18 @@
+const information = function(){
+
+  let myName = "Ibrahim seidu";
+
+  let hngId = "HNG-04975";
+
+  let emailAddress= "bemyguest9ite@gmail.com";
+
+  let languageScript = "JavaScript";
+
+  let result = "Hello World, this is " + myName + " with HNGi7 ID " + hngId + " using " + languageScript + " for stage 2 task."+ emailAddress
+   
+  console.log(result)
+}
+
+information()
+
+

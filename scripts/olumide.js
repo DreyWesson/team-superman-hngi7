990974305356bb723becb319970b4ffecaee4c04
Olumide <olu.awosanya13@gmail.com>
2020-06-06 11:55:24 +0100

    my script

---
+++ scripts/olumide.js
@@ -0,0 +1,5 @@
+// hngi7 script for task 2 
+
+
+var msg = "Hello World. This is [Awosanya Olumide Oloruntobi] with HNGi7 ID [HNG-06492] using [Javascript] for stage 2 task and my email is [olu.awosanya13@gmail.com].";
+console.log(msg);

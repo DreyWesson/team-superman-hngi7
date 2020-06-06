c2a1260861071002aef038fd1ea7784d357b854f
Bolu Tejumola <tejumolabolu@gmail.com>
2020-06-06 14:35:29 +0100

    Submission for Hng-2542

---
+++ scripts/Hng-2542.js
@@ -0,0 +1,24 @@
+function Hng_task2() {
+    const name = "Tejumola Bolu";
+    const id = "HNG-2542";
+    const language = "JavaScript";
+    const email = "tejumolabolu@gmail.com"
+    console.log(
+      "Hello World, this is" +
+        " " +
+        name +
+        " " +
+      "with HNGi7 ID" +
+        " " +
+        id +
+        " " +
+        "using" +
+        " " +
+        language +
+        " " +
+      "for stage 2 task." +
+       " " +
+      email
+    );
+  }
+  Hng_task2();

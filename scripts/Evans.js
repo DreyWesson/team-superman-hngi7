6f54782a554f1856db90ec1a15d3616a291229a0
BotanicalCoder <57745557+BotanicalCoder@users.noreply.github.com>
2020-06-06 09:35:46 +0100

    Refactored code

--- scripts/Evans.js
+++ scripts/Evans.js
@@ -1,12 +1,5 @@
-var myHngDetails = {
-  name: "Okocha Chibueze Evans",
-  id: "HNG-02521",
-  language: "JavaScript",
-  email: "eokocha0@gmail.com"
-};
 
-var { name, id, language, email } = myHngDetails;
-function myHngTask() {
-  console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`);
+function myHngTask(name="Okocha Chibueze Evans", id="HNG-02521", language="JavaScript",email="eokocha0@gmail.com") {
+  return `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`;
 }
-myHngTask(name, id, language,email);
+console.log(myHngTask(name, id, language,email));

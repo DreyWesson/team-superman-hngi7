088153798c4c2fc49f59b40e876dbf8d261d925c
BotanicalCoder <57745557+BotanicalCoder@users.noreply.github.com>
2020-06-06 10:02:12 +0100

    Changes

--- scripts/Evans.js
+++ scripts/Evans.js
@@ -1,5 +1,3 @@
-function myHngTask(name, id, language,email
-) {
+function myHngTask(name="Okocha Chibueze Evans", id="HNG-02521", language="JavaScript",email="eokocha0@gmail.com") {
 console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.${email}`);}
-myHngTask(
-name="Okocha Chibueze Evans", id="HNG-02521", language="JavaScript",email="eokocha0@gmail.com");
+myHngTask();

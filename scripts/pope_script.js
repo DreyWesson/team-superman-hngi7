fe6a3a257e20eb5159d77e429aaa02e5b3a2c05b
ignatiusobiorah <iobiorah23@gmail.com>
2020-06-05 17:09:26 -0700

    added pope_script.js

---
+++ scripts/pope_script.js
@@ -0,0 +1,6 @@
+let full_name  = "Ignatius Obiorah";
+let my_id = "HNG-04906";
+let email = "iobiorah23@gmail.com";
+let language = "JavaScript";
+
+document.write("Hello World, this is "+ full_name +" with HNGi7 ID "+ my_id +" using "+ language +" for stage 2 task email "+ email);

94c3fc5c2c97a72b3a637bc0f3c8b21e8762d405
xemoy <57658067+xemoy@users.noreply.github.com>
2020-06-07 00:04:40 +0100

    Update Oluwayomi.js

--- Oluwayomi.js
+++ Oluwayomi.js
@@ -1,4 +1,4 @@
-function introduce(name="Abiola Oluwayomi", ID="HNG-00618",lang="javascript", email="horllamideh95@gmail.com"){
+function introduce(name="Abiola Oluwayomi", ID="HNG-06618",lang="javascript", email="horllamideh95@gmail.com"){
 console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} using ${lang} for stage 2 task.${email}`)}
 introduce()
 

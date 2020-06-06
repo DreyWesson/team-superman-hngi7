64e132e1547a7311f5cf03e288c940684a90c9ee
Sosu Alfred <sosualfred@gmail.com>
2020-06-06 22:12:01 +0000

    Update Abiola Olamide Oluwayomi.js

--- scripts/Abiola Olamide Oluwayomi.js
+++ scripts/Abiola Olamide Oluwayomi.js
@@ -1,6 +1,6 @@
-var name="Abiola Olamide";
-var ID="HNG-06618";
-var lang="JavaScript";
-var email="horllamideh95@@gmail.com";
+var namere = "Abiola Olamide";
+var ID = "HNG-06618";
+var languse = "JavaScript";
+var email = "horllamideh95@@gmail.com";
   
-console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} using ${lang} for stage 2 task.${email}`);
+console.log(`Hello World, this is ${namere} with HNGi7 ID ${ID} using ${languse} for stage 2 task.${email}`);

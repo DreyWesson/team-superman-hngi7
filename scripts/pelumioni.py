fa851894b7dc862da116ad974605609f5602cdf0
lumpee <onioluwapelumi2014@gmail.com>
2020-06-06 16:28:06 +0100

    new update

--- scripts/pelumioni.py
+++ scripts/pelumioni.py
@@ -1,2 +1,2 @@
 print("Hello World, this is {0} with HNGi7  ID {1} using {2} for stage 2 task.{3}".format(
-    "oluwapelumi oni", "HNG-04612", "python", "onioluwapelumi2014@gmail.com"))
+    "Oluwapelumi Oni", "HNG-04612", "python", "onioluwapelumi2014@gmail.com"))

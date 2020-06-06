80ce88e73131577aeb1894e30bd22f72312145a7
Godson <adeshina.godwin@gmail.com>
2020-06-06 19:42:14 +0100

    Godson task 2 edited, please merge
    HNGi7 Team-Superman task 2

---
+++ scripts/godsonedit.js
@@ -0,0 +1,11 @@
+let fullname = 'Ogunyomi Adeshina';
+let hngid = 'HNG-02569';
+let language = 'Javascript';
+let email = 'adeshina.godwin@gmail.com';
+
+function myIntroduction (fullname, hngid, language, email) {
+    console.log ('Hello World, this is ' + fullname + ' with HNGi7 ID ' + hngid + ' using ' + language + ' for stage 2 task.'+email)
+}
+   
+
+myIntroduction(fullname, hngid, language, email);

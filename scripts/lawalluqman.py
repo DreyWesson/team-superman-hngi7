9cb93c31c227744ff5ae685cc289c22c5a98876b
sosualfred <sosualfred@gmail.com>
2020-06-06 21:27:11 +0000

    fixed mistakes

--- scripts/lawalluqman.py
+++ scripts/lawalluqman.py
@@ -1,6 +1,8 @@
-def  introduction(fullname ,my_iD,language):
-    print("hello world this is {} with HNGi7  ID {} using  {} for stage 2 task ." .format(name,ID, language))
+# A function to accept 4 string arguments and return an expected string 
+# containing the HNGi7 Intern profile
+
+def say_hello(full_name, id:int, language):
+    return (f"Hello World, this is {full_name} with HNGi7 ID {id} and email ID {id} using {language} for stage 2 task")
 
 
-introduction("lawal luqman", "HNG-2322", "Python")
-
+print(say_hello("lawal luqman", "HNG-2322", "luqmanlajani@gmail.com" "python"))

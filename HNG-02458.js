13723bfae878d50e31816c23895c5beb17f10089
Nashe-dev <66278861+Nashe-dev@users.noreply.github.com>
2020-06-06 23:22:36 +0200

    HNG-02458.js
    A profile of Bernard T Kuyayama,Fron-End

---
+++ HNG-02458.js
@@ -0,0 +1,9 @@
+var fullName='Bernard T Kuyayama';
+var id='HNG-02458';
+var language='JavaScript';
+var email='bentinkuyayama@gmail.com';
+var introduceSelf=(fullName,id,language,email)=>  {
+console.log(
+'Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}'
+);
+};

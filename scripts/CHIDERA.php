69c0e5402fd9781b33730bfe4810002aaed07440
Sosu Alfred <sosualfred@gmail.com>
2020-06-06 13:49:52 +0000

    Update CHIDERA.php

--- scripts/CHIDERA.php
+++ scripts/CHIDERA.php
@@ -4,4 +4,6 @@
  $language = "PHP";
  $email = "chideraokoli28@gmail.com";
 
- echo "Hello World, this is ".$name." with HNGi7 ID ".$id." using ".$language." for stage 2 task. ".$email;
+ echo "Hello World, this is ".$name." with HNGi7 ID ".$id." using ".$language." for stage 2 task.".$email;
+
+?>

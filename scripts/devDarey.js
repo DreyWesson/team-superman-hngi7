1bf8fd622f80ea344fa15d293439aa0ab9cb1028
unknown <ojeksdarey@gmail.com>
2020-06-06 13:35:35 +0100

    HNG Task two

---
+++ scripts/devDarey.js
@@ -0,0 +1,6 @@
+const name = 'Ojekunle Damilare'
+const id = 'HNG-06810'
+const email = 'ojeksdarey@gmail.com'
+const language = 'javascript'
+const output = `HelloWorld,this is ${name} with ${id} and ${email} using ${language} for stage 2 task`
+console.log(output)

7bb895a1db37d87327b53fb23c3d3280625b9c3e
Micheal Nwigwe <66284919+icecolddeveloper@users.noreply.github.com>
2020-06-06 15:45:46 +0100

    here is my new code
    updated file

--- scripts/HNG-05019.py
+++ scripts/HNG-05019.py
@@ -1,8 +1,3 @@
-full_name = 'Nwigwe Michael Ifeanyi'
-hng_id = 'HNG-05019'
-language = 'Python'
-email = 'rubzyg@gmail.com'
+print('Hello World, this is Nwigwe Michael Ifeanyi with HNGi7 ID HNG-05019 using python for stage 2 '
+      'task.rubzyg@gmail.com')
 
-output = f'Hello World, this is {full_name} with HNGi7 ID {hng_id} using {language} for stage 2 task.{email}'
-
-print(output)

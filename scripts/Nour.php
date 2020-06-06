dd7a7e722691ecdf65f4720de840785e7ede77b4
Nura Shariff <35644678+shariffnour@users.noreply.github.com>
2020-06-06 19:41:11 +0100

    Created Nour.php

---
+++ scripts/Nour.php
@@ -0,0 +1,9 @@
+<?php
+
+$myName = "Nura Musa Shariff";
+$myHngId = "HNG-04748";
+$lang = "PHP";
+$email ="shariffnura@gmail.com";
+
+echo "Hello World, this is ".$myName. " with HNGi7 ID ".$myHngId. " using " .$lang. " for stage 2 task. " .$email. ".";
+?>

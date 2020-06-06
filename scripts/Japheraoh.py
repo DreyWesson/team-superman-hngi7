798d32fd63927120dcf44d577e2fa2ac6c1017b5
Olaku Japheth <olakujapheth@gmail.com>
2020-06-06 01:36:30 +0100

    Japheraoh modified

--- scripts/Japheraoh.py
+++ scripts/Japheraoh.py
@@ -1,1 +1,1 @@
-Print ("Hello World, this is Olaku Japheth with HNGi7 ID HNG-05733 using Python for stage 2 task.olakujapheth@gmail.com")
+print("Hello World, this is Olaku Japheth with HNGi7 ID HNG-05733 using Python for stage 2 task.olakujapheth@gmail.com")

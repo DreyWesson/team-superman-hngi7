a6b590e3e1f9e7ba5ed418986200d725e0a0bf98
Jonathan463 <godsonjonathan39@gmail.com>
2020-06-05 22:17:53 -1200

    Update godson_jonathan.js

--- scripts/godson_jonathan.js
+++ scripts/godson_jonathan.js
@@ -1,10 +1,10 @@
-let fullname = 'godson Jonathan';
+let fullname = 'Godson Jonathan';
 let hngid = 'HNG-05389';
 let language = 'JavaScript';
 let email = 'godsonjonathan39@gmail.com';
 
 function myDetails(fullname, hngid, language, email) 
 {
-  console.log('Hello World, this is' +''+ +fullname+ +''+ 'with HNGi7 ID' +''+ +hngid+ +''+ 'using' +''+ +language+ +''+ 'for stage 2 task.' +''+ +email)
+  console.log('Hello World, this is ${fullname} with HNGi7 ID ${hngid} using ${language} for stage 2 task.${email}');
 }
-myDetails('Godson Jonathan', 'HNG-05389', 'JavaScript', 'godsonjonathan39@gmail.com')
+myDetails('Godson Jonathan', 'HNG-05389', 'JavaScript', 'godsonjonathan39@gmail.com');

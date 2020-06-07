a960bc15eca102fd974372ccd018738009ac0bab
ebuka28 <chideraokoli28@gmail.com>
2020-06-07 00:24:21 -0600

    ebuka28-HNG-02101 script

---
+++ scripts/chidera.php
@@ -0,0 +1,7 @@
+<?php
+ $name = "Okoli Ebuka";
+ $id = "HNG-02101";
+ $language = "php";
+ $email = "chideraokoli28@gmail.com";
+  echo "Hello World, this is .$name. with HNGi7 ID .$id. using .$language. for stage 2 task.$email";
+  ?>

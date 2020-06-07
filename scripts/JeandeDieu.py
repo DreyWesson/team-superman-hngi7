69055a2c4512bb2c0cbf79e516f2ef9f83b3783c
Manishimwe Aime Jayd <aimejayd2012@gmail.com>
2020-06-07 20:20:06 +0530

    Promotion script to Task 2

--- scripts/JeandeDieu.py
+++ scripts/JeandeDieu.py
@@ -1,2 +1,2 @@
-message = "Hello World, This is JEAN DE DIEU MANISHIMWE With HNGi7 ID HNG-03605 using python for stage 2 task.aimejayd2016@gmail.com"
-print(message)
+MyMessage = "Hello World, This is JEAN DE DIEU MANISHIMWE With HNGi7 ID HNG-03605 using python for stage 2 task.aimejayd2016@gmail.com"
+print(MyMessage)

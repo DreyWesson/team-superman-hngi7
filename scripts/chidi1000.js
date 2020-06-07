3be76be50fa0aefd094f9e19ab9fc885541ec64c
xemoy <57658067+xemoy@users.noreply.github.com>
2020-06-06 14:27:40 -1200

    delete
    delete

--- scripts/chidi1000.js
+++
@@ -1,6 +0,0 @@
-function chidi1000(name="Mgbekwute Remigius Chidiebere", myID="HNG-02727", myEmail="mgbekwutechidiebere@gmail.com", scriptLanguage="Javascript"){
-
-    console.log(`Hello World, this is ${name} with HNiG7 ID ${myID} using ${scriptLanguage} for stage 2 task.${myEmail}`
-    );
-}
-chidi1000();

769b3031eca058d7022544306109eccf3aca876f
Sosu Alfred <sosualfred@gmail.com>
2020-06-07 20:26:38 +0000

    Update gpee-tech.js

--- gpee-tech.js
+++ gpee-tech.js
@@ -1,9 +1,8 @@
 const fullName = "God'spower Etuk";
 const id = "HNG-02259";
-let lang = "Javascript";
+let lang = "JavaScript";
 let email = "godspoweretuk38@gmail.com";
 
 console.log(
-    "Hello World, This is " + fullName + ", "+"with HNGi7 ID as " + id + " using "+ lang +" for stage 2 task."
-    "Hello World, this is " + fullName + " with HNGi7 ID " + id + " using "+ lang +" for stage 2 task"+ email
+    "Hello World, this is " + fullName + " with HNGi7 ID " + id + " using "+ lang +" for stage 2 task."+ email
     );

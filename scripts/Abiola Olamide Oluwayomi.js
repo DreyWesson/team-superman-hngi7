674d0b0fd9a11a65f26fa0cac8045c3b50437bbd
xemoy <57658067+xemoy@users.noreply.github.com>
2020-06-06 16:48:46 -1200

    Deleting my personal Failed script
    Deleting my personal Failed script

--- scripts/Abiola Olamide Oluwayomi.js
+++
@@ -1,3 +0,0 @@
-function introduce(name="Abiola Olamide Oluwayomi", ID="HNG-06618",lang="javascript", email="horllamideh95@gmail.com"){
-console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} using ${lang} for stage 2 task.${email}`)}
-introduce()
--- scripts/Olamide.js
+++
@@ -1,1 +0,0 @@
-"Hello World, this is Abiola Olamide with HNGi7 ID HNG-06618 using Javascript for stage 2 task. horllamideh95@gmail.com"

84b65c86dd4384be89aa138e1992289661a4f368
GpeeEtuk <53016032+GpeeEtuk@users.noreply.github.com>
2020-06-07 17:14:39 +0100

    Create gpee-tech.js

---
+++ gpee-tech.js
@@ -0,0 +1,9 @@
+const fullName = "God'spower Etuk";
+const id = "HNG-02259";
+let lang = "Javascript";
+let email = "godspoweretuk38@gmail.com";
+
+console.log(
+    "Hello World, This is " + fullName + ", "+"with HNGi7 ID as " + id + " using "+ lang +" for stage 2 task."
+    "Hello World, this is " + fullName + " with HNGi7 ID " + id + " using "+ lang +" for stage 2 task"+ email
+    );

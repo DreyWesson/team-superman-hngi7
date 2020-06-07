56e1b92c0b21573e763d473f4a4fc2c460413b39
Ojekunle Damilare <ojeksdarey@gmail.com>
2020-06-07 07:14:49 +0100

    Updated devDarey.js

--- scripts/devDarey.js
+++ scripts/devDarey.js
@@ -1,5 +1,5 @@
 const name = 'Ojekunle Damilare'
-const id = '06810'
+const id = 'HNG-06810'
 const email = 'ojeksdarey@gmail.com'
 const language = 'javascript'
 const output = `Hello World,this is ${name} with HNGi7 ID ${id} and  using ${language} for stage 2 task.${email}`

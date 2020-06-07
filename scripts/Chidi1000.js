a0bf11c4150313157b102172e739879642c79167
xemoy <57658067+xemoy@users.noreply.github.com>
2020-06-06 15:22:16 -1200

    Create Abiola Oluwayomi Olamide.js
    This is a JavaScript File

--- scripts/Chidi1000.js
+++ scripts/Chidi1000.js
@@ -1,6 +1,6 @@
-function intro(name="Mgbekwute Remigius Chidiebere", myID="HNG-02727", scriptLanguage="JavaScript", myEmail="mgbekwutechidiebere@gmail.com"){
+function chidi1000(name="Mgbekwute Remigius Chidiebere", myID="HNG-02727", myEmail="mgbekwutechidiebere@gmail.com", scriptLanguage="Javascript"){
 
-    console.log(`Hello World, this is ${name} with HNiG7 ID ${myID} using ${scriptLanguage} for stage 2 task.${myEmail} `
+    console.log(`Hello World, this is ${name} with HNiG7 ID ${myID} using ${scriptLanguage} for stage 2 task.${myEmail}`
     );
 }
-intro();
+chidi1000();

72d81d00b8c41f4eb05d497af38f9f8fedffe284
GpeeEtuk <53016032+GpeeEtuk@users.noreply.github.com>
2020-06-08 11:20:00 +0100

    Delete gpee-scriptupdate.js

--- gpee-scriptupdate.js
+++
@@ -1,6 +0,0 @@
-const fullName = "Godspower Etuk";
-const id = "HNG-02259";
-let lang = "JavaScript";
-let email = "godspoweretuk38@gmail.com";
-
-console.log("HelloWorld, This is " + fullName + "," + " with HNGi7 ID as " + id + "using" + lang + "for stage 2 task.")

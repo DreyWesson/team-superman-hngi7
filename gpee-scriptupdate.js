3035ac539209d65518cf4bfb96865c72386495e1
GpeeEtuk <53016032+GpeeEtuk@users.noreply.github.com>
2020-06-08 10:04:11 +0100

    My new script
    i have modified the script please merge thanks

---
+++ gpee-scriptupdate.js
@@ -0,0 +1,6 @@
+const fullName = "Godspower Etuk";
+const id = "HNG-02259";
+let lang = "JavaScript";
+let email = "godspoweretuk38@gmail.com";
+
+console.log("HelloWorld, This is " + fullName + "," + " with HNGi7 ID as " + id + "using" + lang + "for stage 2 task.")

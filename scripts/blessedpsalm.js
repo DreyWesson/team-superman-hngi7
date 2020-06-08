4cd8d039c6ae5af72a68f70482bfd22217b10e31
Blessedpsalm <43576157+Blessedpsalm@users.noreply.github.com>
2020-06-08 07:59:58 +0100

    Create blessedpsalm.js

---
+++ scripts/blessedpsalm.js
@@ -0,0 +1,11 @@
+const info = {
+    name: "Samuel Ogunsola",
+    email: "sammy4excellence97@gmail.com",
+    id: "HNG-00800",
+    language: "Javascript",
+
+}
+
+const { name, id, email, language } = info;
+const displayText = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task email ${email}`;
+console.log(displayText);
